687b3f2888a870081cf1a757984630c3fddd5055
Henry Oswald <henry.oswald@sharelatex.com>
2014-08-04 12:18:31 +0100

    removed external libs for i18n which we no longer need

--- services/web/public/js/libs/ng-i18next/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  define(["libs/ng-i18next/provider", "libs/ng-i18next/directive/directive", "libs/ng-i18next/filter/filter"], function() {
-
-  	console.log("hello")
-
-  });
-
-}).call(this);
