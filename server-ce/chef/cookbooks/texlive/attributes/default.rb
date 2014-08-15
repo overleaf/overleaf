bdf4fbb59ac9a0860828fbbad2a0dc7a7316b4d2
James Allen <james@sharelatex.com>
2014-08-15 13:34:57 +0100

    Set up dev environment with Vagrant

--- server-ce/chef/cookbooks/texlive/attributes/default.rb
+++ server-ce/chef/cookbooks/texlive/attributes/default.rb
@@ -1,2 +1,2 @@
 default[:texlive][:schema]  = "small"
-default[:texlive][:bin_dir] = "/usr/local/texlive/2013/bin/x86_64-linux"
+default[:texlive][:bin_dir] = "/usr/local/texlive/2014/bin/x86_64-linux"
