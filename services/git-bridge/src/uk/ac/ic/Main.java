2df4f1b0e0448fdec6f2c0fd5ee7db41c64a8e8c
Winston Li <winston.li12@imperial.ac.uk>
2014-11-01 16:42:00 +0000

    Added JGit library and its JavaDocs.

--- services/git-bridge/src/uk/ac/ic/Main.java
+++ services/git-bridge/src/uk/ac/ic/Main.java
@@ -1,12 +1,17 @@
 package uk.ac.ic;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.*;
+
+import java.io.IOException;
+
 /**
  * Created by Roxy on 01/11/14.
  */
 public class Main {
 
     public static void main(String[] args) {
-
+        
     }
 
 }
