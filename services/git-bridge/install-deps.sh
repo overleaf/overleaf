332d776ad175e29119c455393670a9a55e08b8bc
Winston Li <winston.li12@imperial.ac.uk>
2014-12-05 21:10:07 +0000

    Fixed install-deps.sh.

--- services/git-bridge/install-deps.sh
+++ services/git-bridge/install-deps.sh
@@ -3,7 +3,7 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 apt-get update
-apt-get install ant
-apt-get install openjdk-7-jdk
-update-alternatives --set java /usr/lib/java-7-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/java-7-openjdk-amd64/jre/bin/javac
+apt-get install -y ant
+apt-get install -y openjdk-7-jdk
+update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/javac
