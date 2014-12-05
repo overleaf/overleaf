605107aa7402cb24bcc8f38e477c8fd165c750c0
Winston Li <winston.li12@imperial.ac.uk>
2014-12-05 21:00:20 +0000

    Made scripts executable.

--- services/git-bridge/install.sh
+++ services/git-bridge/install.sh
@@ -2,6 +2,7 @@ if [ "$(id -u)" != "0" ]; then
 	echo "You must be root to install" 1>&2
 	exit 1
 fi
+command -v ant >/dev/null 2>&1 && command -v /usr/lib/jvm/java-7-openjdk-amd64/bin/javac >/dev/null 2>&1 || ./install-deps.sh
 ant all &&\
 cp ./bin/writelatex-git-bridge.jar /usr/local/sbin/ &&\
 cp ./bin/wlgb /etc/init.d/ &&\
