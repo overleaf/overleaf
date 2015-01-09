2310aa124fcf3297c1b2bb1d456253a8b1116f87
Winston Li <winston.li12@imperial.ac.uk>
2015-01-09 07:52:49 +0000

    Fixed typo and changed to only copy config file if one does not already exist.

--- services/git-bridge/install.sh
+++ services/git-bridge/install.sh
@@ -1,14 +1,3 @@
-command -v ant >/dev/null 2>&1 && command -v /usr/lib/jvm/java-7-openjdk-amd64/bin/javac >/dev/null 2>&1 || installDeps
-mvn package &&\
-sudo mv ./target/writelatex-git-bridge-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/local/sbin/writelatex-git-bridge.jar &&\
-sudo cp ./bin/wlgb /etc/init.d/ &&\
-sudo mkdir -p /var/log/wlgb &&\
-sudo mkdir -p /etc/wlgb &&\
-if [ -f /etc/wlgb/config.json ]; then
-    sudo cp ./bin/config.json /etc/wlgb/
-fi &&\
-sudo /usr/sbin/update-rc.d -f wlgb defaults
-
 installDeps() {
 	sudo apt-get update
 	sudo apt-get install -y maven2
@@ -17,3 +6,14 @@ installDeps() {
 	sudo update-alternatives --set javac /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/javac
 	return 1
 }
+
+command -v ant >/dev/null 2>&1 && command -v /usr/lib/jvm/java-7-openjdk-amd64/bin/javac >/dev/null 2>&1 || installDeps
+sudo mvn package &&\
+sudo mv ./target/writelatex-git-bridge-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/local/sbin/writelatex-git-bridge.jar &&\
+sudo cp ./bin/wlgb /etc/init.d/ &&\
+sudo mkdir -p /var/log/wlgb &&\
+sudo mkdir -p /etc/wlgb &&\
+if [ ! -f /etc/wlgb/config.json ]; then
+    sudo cp ./bin/config.json /etc/wlgb/
+fi &&\
+sudo /usr/sbin/update-rc.d -f wlgb defaults
