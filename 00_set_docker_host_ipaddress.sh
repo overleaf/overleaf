d347ee087d9b79f0d3bee329099bfa7faf289d17
James Allen <james@sharelatex.com>
2015-02-09 16:27:44 +0000

    Add missing file

---
+++ 00_set_docker_host_ipaddress.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# See the bottom of http://stackoverflow.com/questions/24319662/from-inside-of-a-docker-container-how-do-i-connect-to-the-localhost-of-the-mach
+echo "`route -n | awk '/UG[ \t]/{print $2}'` dockerhost" >> /etc/hosts
