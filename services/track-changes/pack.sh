64545d65407eb804c8743928fcd9a7db06430161
Brian - Work <bjg@network-theory.co.uk>
2016-01-27 15:48:36 +0000

    improve packing script

--- services/track-changes/pack.sh
+++ services/track-changes/pack.sh
@@ -1,7 +1,21 @@
-while docs=$(curl "localhost:3015/doc/list?limit=1000"); do
+#!/bin/bash
+
+# find all the docHistories with unpacked ops and pack them
+
+HOST=${1:-"localhost:3015"}
+T=${2:-10}
+
+echo packing all docHistory on $HOST with delay of $T
+for n in $(seq 5 -1 1) ; do
+    echo starting in $n seconds
+    sleep 1
+done
+
+while docs=$(curl "$HOST/doc/list?limit=1000"); do
     if [ -z "$docs" ] ; then break ; fi
     for d in $docs ; do 
 	echo "packing $d"
-	curl -X POST "localhost:3015/doc/$d/pack"
+	curl -X POST "$HOST/doc/$d/pack"
+	sleep $T
     done
 done
