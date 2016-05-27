c524be6180efa1b7e123b6f9b7f9489ecb91ad80
Henry Oswald <henry.oswald@sharelatex.com>
2016-05-27 12:55:18 +0100

    run as www-data

--- runit/spelling-sharelatex.sh
+++ runit/spelling-sharelatex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SHARELATEX_CONFIG=/etc/sharelatex/settings.coffee
-exec /sbin/setuser sharelatex /usr/bin/node /var/www/sharelatex/spelling/app.js >> /var/log/sharelatex/spelling.log 2>&1
+exec /sbin/setuser www-data /usr/bin/node /var/www/sharelatex/spelling/app.js >> /var/log/sharelatex/spelling.log 2>&1
