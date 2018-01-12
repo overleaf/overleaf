1f35b6dec36030d2df3d86b15ea4a75230982c2e
Michael Walker <mike@barrucadu.co.uk>
2018-01-12 16:07:37 +0000

    Raise an error if files exceed the max file size

---
+++ services/git-bridge/src/main/java/uk/ac/ic/wlgitbridge/bridge/util/CastUtil.java
@@ -0,0 +1,18 @@
+package uk.ac.ic.wlgitbridge.bridge.util;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Created by winston on 01/07/2017.
+ */
+public class CastUtil {
+
+    public static int assumeInt(long l) {
+        Preconditions.checkArgument(
+                l <= (long) Integer.MAX_VALUE
+                        && l >= (long) Integer.MIN_VALUE,
+                l + " cannot fit inside an int");
+        return (int) l;
+    }
+
+}
