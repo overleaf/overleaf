647cf24bb6bde5696ee0424fceb0d35d396eb39a
Michael Walker <mike@barrucadu.co.uk>
2018-02-07 10:39:26 +0000

    Use a List<String> rather than hard-coding newlines

--- services/git-bridge/src/main/java/uk/ac/ic/wlgitbridge/snapshot/base/MissingRepositoryException.java
+++ services/git-bridge/src/main/java/uk/ac/ic/wlgitbridge/snapshot/base/MissingRepositoryException.java
@@ -4,27 +4,35 @@ import com.google.gson.JsonElement;
 import uk.ac.ic.wlgitbridge.git.exception.SnapshotAPIException;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MissingRepositoryException extends SnapshotAPIException {
 
-    public static final String GENERIC_REASON =
-        "This Overleaf project currently has no git access.\n" +
-        "\n" +
-        "If this problem persists, please contact us.";
+    public static final List<String> GENERIC_REASON = Arrays.asList(
+        "This Overleaf project currently has no git access.",
+        "",
+        "If this problem persists, please contact us."
+    );
 
-    public static final String EXPORTED_TO_V2 =
-        "This Overleaf project has been moved to Overleaf v2, and git access is temporarily unsupported.\n" +
-        "\n" +
-        "See https://www.overleaf.com/help/342 for more information.";
+    public static final List<String> EXPORTED_TO_V2 = Arrays.asList(
+        "This Overleaf project has been moved to Overleaf v2, and git access is temporarily unsupported.",
+        "",
+        "See https://www.overleaf.com/help/342 for more information."
+    );
 
-    private String message = "";
+    private List<String> descriptionLines;
 
     public MissingRepositoryException() {
+        descriptionLines = new ArrayList<String>();
     }
 
     public MissingRepositoryException(String message) {
-        this.message = message;
+        this.descriptionLines = Arrays.asList(message);
+    }
+
+    public MissingRepositoryException(List<String> descriptionLines) {
+        this.descriptionLines = descriptionLines;
     }
 
     @Override
@@ -32,12 +40,12 @@ public class MissingRepositoryException extends SnapshotAPIException {
 
     @Override
     public String getMessage() {
-        return message;
+        return String.join("\n", this.descriptionLines);
     }
 
     @Override
     public List<String> getDescriptionLines() {
-        return Arrays.asList(getMessage());
+        return this.descriptionLines;
     }
 
 }
