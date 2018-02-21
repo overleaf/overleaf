027ab1250aee3dc4981d3b34ffa013565b135ab4
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-02-21 14:55:08 +0000

    Configure CM with latex parser

--- services/web/public/es/rich-text.js
+++ services/web/public/es/rich-text.js
@@ -1,11 +1,19 @@
 import CodeMirror, { Doc } from 'codemirror'
 
+import LatexParser from './ide/editor/codemirror/parser'
+
 export function init (rootEl) {
-  return CodeMirror(rootEl)
+  CodeMirror.defineMode('latex', () => new LatexParser())
+  CodeMirror.defineMIME('application/x-tex', 'latex')
+  CodeMirror.defineMIME('application/x-latex', 'latex')
+
+  return CodeMirror(rootEl, {
+    mode: 'latex'
+  })
 }
 
 export function openDoc (cm, content) {
-  const newDoc = Doc(content)
+  const newDoc = Doc(content, 'latex')
   cm.swapDoc(newDoc)
   return newDoc
 }
