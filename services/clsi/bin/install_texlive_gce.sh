3c2172434bd9abf009b916fe43dead41efc74e97
Henry Oswald <henry.oswald@sharelatex.com>
2018-03-08 15:38:17 +0000

    have entrypoint kickoff download off texlive images
    install script exits without error if auth fails.

---
+++ services/clsi/bin/install_texlive_gce.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+METADATA=http://metadata.google.internal./computeMetadata/v1
+SVC_ACCT=$METADATA/instance/service-accounts/default
+ACCESS_TOKEN=$(curl -s -H 'Metadata-Flavor: Google' $SVC_ACCT/token | cut -d'"' -f 4)
+if [ -z "$ACCESS_TOKEN" ]; then
+	echo "No acccess token to download texlive-full images from google container, continuing without downloading. This is likely not a google cloud enviroment."
+	exit 0
+fi
+docker login -u '_token' -p $ACCESS_TOKEN https://gcr.io
+docker pull --all-tags gcr.io/henry-terraform-admin/texlive-full #TODO NEED TO MAKE THIS AN ENV VAR
+echo "Finished downloading texlive-full images"
+
+
--- services/clsi/entrypoint.sh
+++ services/clsi/entrypoint.sh
@@ -2,4 +2,5 @@
 
 echo "Changing permissions of /var/run/docker.sock for sibling containers"
 chown root:docker /var/run/docker.sock
+./bin/install_texlive_gce.sh
 exec runuser -u app "$@"
