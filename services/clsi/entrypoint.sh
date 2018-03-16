6d21ab734a67790a12ab70a3553ccaacc9d37467
Henry Oswald <henry.oswald@sharelatex.com>
2018-03-16 17:10:56 +0000

    try copying synctex betwen directories

--- services/clsi/entrypoint.sh
+++ services/clsi/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 echo "Changing permissions of /var/run/docker.sock for sibling containers"
+cp /var/clsi/bin/synctex /var/clsi/bin/synctex-mount/synctex
+
 chown root:docker /var/run/docker.sock
+chown app:app /app/compiles
+
 ./bin/install_texlive_gce.sh
 exec runuser -u app "$@"
