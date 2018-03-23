46f2ff051c7d9eb3b6adc5514dd96000d46ad71b
Henry Oswald <henry.oswald@sharelatex.com>
2018-03-23 17:29:21 +0000

    cd into tmp

--- services/filestore/install_deps.sh
+++ services/filestore/install_deps.sh
@@ -3,8 +3,8 @@ apt-get update
 
 apt-get install vim imagemagick optipng --yes
 
-wget https://s3.amazonaws.com/sl-public-dev-assets/ghostscript-9.15.tar.gz -O /tmp/ghostscript-9.15.tar.gz
-ls -al /tmp
+wget -q https://s3.amazonaws.com/sl-public-dev-assets/ghostscript-9.15.tar.gz -O /tmp/ghostscript-9.15.tar.gz
+cd /tmp
 tar -xvf /tmp/ghostscript-9.15.tar.gz
 cd /tmp/ghostscript-9.15 && ./configure && make && make install
 
