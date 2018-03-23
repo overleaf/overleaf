801adf8e9ea728fc9bc153cf3080fa5ed8cc1249
Henry Oswald <henry.oswald@sharelatex.com>
2018-03-23 17:06:31 +0000

    install gs manually

--- services/filestore/install_deps.sh
+++ services/filestore/install_deps.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-apt-get install poppler-utils vim ghostscript imagemagick optipng --yes
+apt-get update
+
+apt-get install vim imagemagick optipng --yes
+
+wget https://s3.amazonaws.com/sl-public-dev-assets/ghostscript-9.15.tar.gz /tmp/ghostscript-9.15.tar.gz
+tar -xvf /tmp/ghostscript-9.15.tar.gz
+cd /tmp/ghostscript-9.15 && ./configure && make && make install
+
 npm rebuild
 chown -R node:node /app/uploads
