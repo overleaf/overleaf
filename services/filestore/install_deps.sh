6657b6c1dc9f6ac11fbd2c57fc7b9c951f4379d7
Henry Oswald <henry.oswald@sharelatex.com>
2018-05-21 11:13:52 +0100

    make all needed directories in install deps

--- services/filestore/install_deps.sh
+++ services/filestore/install_deps.sh
@@ -7,8 +7,9 @@ wget -q https://s3.amazonaws.com/sl-public-dev-assets/ghostscript-9.15.tar.gz -O
 cd /tmp
 tar -xvf /tmp/ghostscript-9.15.tar.gz
 cd /tmp/ghostscript-9.15 && ./configure && make && make install
-
 npm rebuild
+mkdir /app/user_files/ /app/uploads/ /app/template_files/
+chown -R node:node /app/user_files
 chown -R node:node /app/uploads
-
-echo gs --version
+chown -R node:node /app/template_files
+ls -al /app
