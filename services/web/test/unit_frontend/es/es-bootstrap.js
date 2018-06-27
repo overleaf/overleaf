3a90a2d004b083fee036857c3fab7b16c35ee85c
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-06-27 11:28:29 +0100

    Add custom assertion method to chai for comparing CM Pos objects

---
+++ services/web/test/unit_frontend/es/es-bootstrap.js
@@ -0,0 +1,19 @@
+/* global chai */
+
+/**
+ * Add chai assertion for comparing CodeMirror Pos objects.
+ * A deep comparison will fail because CodeMirror inserts additional properties
+ * that we want to ignore.
+ */
+chai.Assertion.addMethod('equalPos', function (expectedPos) {
+  const { line: actualLine, ch: actualCh } = this._obj
+  const { line: expectedLine, ch: expectedCh } = expectedPos
+
+  this.assert(
+    actualLine === expectedLine && actualCh === expectedCh,
+    `expected #{exp} to equal #{act}`,
+    `expected #{exp} to not equal #{act}`,
+    `Pos({ line: ${expectedLine}, ch: ${expectedCh} })`,
+    `Pos({ line: ${actualLine}, ch: ${actualCh} })`
+  )
+})
