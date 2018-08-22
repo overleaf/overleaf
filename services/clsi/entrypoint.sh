b28d2103578687d32e467e684a9e37c0d0e135f9
Henry Oswald <henry.oswald@sharelatex.com>
2018-08-23 00:10:06 +0100

    try changing bin to be owned by node

--- services/clsi/entrypoint.sh
+++ services/clsi/entrypoint.sh
@@ -17,6 +17,7 @@ chown -R node:node /app/bin/synctex
 mkdir -p /app/test/acceptance/fixtures/tmp/
 chown -R node:node /app
 
+chown -R node:node /app/bin
 
 ./bin/install_texlive_gce.sh
 exec runuser -u node "$@"
