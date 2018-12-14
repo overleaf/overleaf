46904bfe7391f6878aa32b5a48c65fb23f200c62
Shane Kilkelly <shane@kilkelly.me>
2018-12-14 16:13:58 +0000

    Set timeouts on http requests to 3 minutes, up from the default of 20 seconds.

--- services/git-bridge/src/main/java/uk/ac/ic/wlgitbridge/snapshot/base/Request.java
+++ services/git-bridge/src/main/java/uk/ac/ic/wlgitbridge/snapshot/base/Request.java
@@ -130,6 +130,7 @@ public abstract class Request<T extends Result> {
             HttpRequest request = Instance.httpRequestFactory.buildGetRequest(
                     new GenericUrl(url)
             );
+            setTimeouts(request);
             request(request);
         } catch (IOException e) {
             e.printStackTrace();
@@ -157,6 +158,13 @@ public abstract class Request<T extends Result> {
     private void request(HttpRequest request) throws IOException {
         onBeforeRequest(request);
         future = request.executeAsync();
+    }
+
+    private void setTimeouts(HttpRequest request) {
+        // timeouts are 20s by default
+        int threeMinutesInMs = 1000 * 60 * 3;
+        request.setConnectTimeout(threeMinutesInMs);
+        request.setReadTimeout(threeMinutesInMs);
     }
 
 }
