e78487922e746cc571193714ae2d6498c48cdb0b
Shane Kilkelly <shane@kilkelly.me>
2019-02-14 10:49:32 +0000

    Merge pull request #1513 from sharelatex/sk-migrate-read-and-write-token-prefix-post-deploy
    Script to re-activate token access for select projects
    
    GitOrigin-RevId: df379f49ec840948ec1afc0864b35b5b5746ebc1

---
+++ services/web/scripts/project-tokens/reactivate_projects.js
@@ -0,0 +1,39 @@
+const ProjectDetailsHandler = require('../../app/js/Features/Project/ProjectDetailsHandler')
+const Async = require('async')
+
+const projectIds = [
+  // put ids here
+]
+
+Async.eachLimit(
+  projectIds,
+  5,
+  (projectId, cb) => {
+    ProjectDetailsHandler.setPublicAccessLevel(projectId, 'tokenBased', err => {
+      if (err) {
+        return cb(err)
+      }
+      console.log(
+        `>> Set public-access-level to tokenBased for project ${projectId}`
+      )
+      ProjectDetailsHandler.ensureTokensArePresent(projectId, (err, tokens) => {
+        if (err) {
+          return cb(err)
+        }
+        console.log(
+          `>> Re-generated tokens for project ${projectId}, ${JSON.stringify(
+            tokens
+          )}`
+        )
+        cb()
+      })
+    })
+  },
+  err => {
+    if (err) {
+      throw err
+    }
+    console.log('>> Done')
+    process.exit(0)
+  }
+)
