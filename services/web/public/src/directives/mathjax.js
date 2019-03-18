41f75b5936447b5a684583e71c285c83935e43a5
Alasdair Smith <alasdairsmith100@gmail.com>
2019-03-18 10:24:54 +0000

    Merge pull request #1543 from sharelatex/as-fix-potential-angular-xss
    Fix potential Angular XSS issues
    
    GitOrigin-RevId: a18a609a25b29e910cb78e28a37398417cfe4842

--- services/web/public/src/directives/mathjax.js
+++ services/web/public/src/directives/mathjax.js
@@ -1,70 +1,37 @@
-/* eslint-disable
-    max-len,
-    no-undef,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS103: Rewrite code to no longer use __guard__
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-define(['base'], App =>
-  App.directive('mathjax', () => ({
-    link(scope, element, attrs) {
-      if (attrs.delimiter !== 'no-single-dollar') {
-        const inlineMathConfig = __guard__(
-          __guard__(
-            typeof MathJax !== 'undefined' && MathJax !== null
-              ? MathJax.Hub
-              : undefined,
-            x1 => x1.config
-          ),
-          x => x.tex2jax.inlineMath
-        )
-        const alreadyConfigured = _.find(
-          inlineMathConfig,
-          c => c[0] === '$' && c[1] === '$'
-        )
+/* global MathJax, _ */
 
-        if (alreadyConfigured == null) {
-          __guard__(
-            typeof MathJax !== 'undefined' && MathJax !== null
-              ? MathJax.Hub
-              : undefined,
-            x2 =>
-              x2.Config({
-                tex2jax: {
-                  inlineMath: inlineMathConfig.concat([['$', '$']])
-                }
-              })
+define(['base'], function(App) {
+  return App.directive('mathjax', function($compile) {
+    return {
+      link(scope, element, attrs) {
+        if (!(MathJax && MathJax.Hub)) return
+
+        const mathJaxContents = element.html()
+        const nonBindableEl = $compile('<span ng-non-bindable></span>')({})
+        element.html('').append(nonBindableEl)
+        nonBindableEl.html(mathJaxContents)
+
+        if (attrs.delimiter !== 'no-single-dollar') {
+          const inlineMathConfig =
+            MathJax.Hub.config && MathJax.Hub.config.tex2jax.inlineMath
+          const alreadyConfigured = _.find(
+            inlineMathConfig,
+            c => c[0] === '$' && c[1] === '$'
           )
-        }
-      }
 
-      return setTimeout(
-        () =>
-          __guard__(
-            typeof MathJax !== 'undefined' && MathJax !== null
-              ? MathJax.Hub
-              : undefined,
-            x3 =>
-              x3.Queue([
-                'Typeset',
-                typeof MathJax !== 'undefined' && MathJax !== null
-                  ? MathJax.Hub
-                  : undefined,
-                element.get(0)
-              ])
-          ),
-        0
-      )
+          if (!alreadyConfigured) {
+            MathJax.Hub.Config({
+              tex2jax: {
+                inlineMath: inlineMathConfig.concat([['$', '$']])
+              }
+            })
+          }
+        }
+
+        setTimeout(() => {
+          MathJax.Hub.Queue(['Typeset', MathJax.Hub, element.get(0)])
+        }, 0)
+      }
     }
-  })))
-function __guard__(value, transform) {
-  return typeof value !== 'undefined' && value !== null
-    ? transform(value)
-    : undefined
-}
+  })
+})
