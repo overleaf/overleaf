27251bc1ac29f8698d4b117c5fc84d04a39989b1
Timothée Alby <timothee.alby@gmail.com>
2019-03-20 10:18:06 -0500

    Merge pull request #1646 from sharelatex/ho-fail-lint
    Remove `set -e` from build process and wait for each previous task
    
    GitOrigin-RevId: 4ece4b70c729e8de654adae0053fdb28f0f86799

--- services/web/install_deps.sh
+++ services/web/install_deps.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e
-
 WEBPACK_ENV=production make minify &
 make --no-print-directory format & 
 make --no-print-directory lint & 
 npm install git+https://github.com/sharelatex/translations-sharelatex.git#master &
-wait -n
+wait -n && wait -n && wait -n && wait -n 
 
 chmod -R 0755 /app/public
 chown -R node:node /app/public
