494f749e72ff0628f720c18fbaf05bcd0656272f
Henry Oswald <henry.oswald@sharelatex.com>
2019-03-21 11:02:12 +0000

    Merge pull request #1651 from sharelatex/ho-check-exit-codes
    Check exit codes of parallel build tasks
    
    GitOrigin-RevId: fb242722d8a886c64b61876cfdcbece9bfa987e6

--- services/web/install_deps.sh
+++ services/web/install_deps.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-WEBPACK_ENV=production make minify
-make --no-print-directory format
-make --no-print-directory lint
-npm install git+https://github.com/sharelatex/translations-sharelatex.git#master
+make --no-print-directory format & FORMAT=$!
+make --no-print-directory lint & LINT=$!
+npm install git+https://github.com/sharelatex/translations-sharelatex.git#master & TRANSLATIONS=$!
+WEBPACK_ENV=production make minify & MINIFY=$!
+
+echo "Waiting for lint, format, translations and minify to finish"
+
+wait $LINT && echo "Lint complete" || exit 1
+wait $FORMAT && echo "Format complete" || exit 1
+wait $TRANSLATIONS && echo "Translations install complete" || exit 1
+wait $MINIFY && echo "Minifiy complete" || exit 1
 
 chmod -R 0755 /app/public
 chown -R node:node /app/public
