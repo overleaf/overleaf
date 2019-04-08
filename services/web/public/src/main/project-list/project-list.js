e6943e8e71f2ba45d359ec44ee1b41f9f9d0b24f
Paulo Jorge Reis <paulojreis@gmail.com>
2019-04-08 11:00:36 +0100

    Merge pull request #1667 from sharelatex/pr-add-post-integration-survey-link
    Add survey link in the projects list sidebar.
    
    GitOrigin-RevId: 085f969336e81d546c51043c3ab9dc63585f97fb

--- services/web/public/src/main/project-list/project-list.js
+++ services/web/public/src/main/project-list/project-list.js
@@ -34,6 +34,36 @@ define(['base'], function(App) {
     $scope.nUntagged = 0
     $scope.reverse = true
     $scope.searchText = { value: '' }
+    $scope.shouldShowSurveyLink = false
+
+    const surveyStartDate = new Date(2019, 3, 2)
+
+    if (
+      localStorage('dismissed-survey') === true ||
+      new Date() < surveyStartDate
+    ) {
+      $scope.shouldShowSurveyLink = false
+    } else {
+      const _v2LaunchDate = new Date(2019, 0, 8)
+      let _nRecentProjects = 0
+
+      for (let project of $scope.projects) {
+        if (
+          project.accessLevel === 'owner' &&
+          new Date(project.lastUpdated) > _v2LaunchDate
+        ) {
+          if (++_nRecentProjects > 1) {
+            $scope.shouldShowSurveyLink = true
+            break
+          }
+        }
+      }
+    }
+
+    $scope.dismissSurvey = () => {
+      localStorage('dismissed-survey', true)
+      $scope.shouldShowSurveyLink = false
+    }
 
     $timeout(() => recalculateProjectListHeight(), 10)
 
