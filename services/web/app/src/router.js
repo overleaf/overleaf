84de1af960403e328189fb9f20ff2ac272d39f15
Shane Kilkelly <shane@kilkelly.me>
2019-08-30 09:37:50 +0100

    Merge pull request #2108 from overleaf/sk-add-rate-limit-to-project-page
    Add a rate limit to the `/project` page
    
    GitOrigin-RevId: ef77f092c94c6a88fc73354cee6b8572b84d3d3a

--- services/web/app/src/router.js
+++ services/web/app/src/router.js
@@ -262,6 +262,11 @@ function initialize(webRouter, privateApiRouter, publicApiRouter) {
   webRouter.get(
     '/project',
     AuthenticationController.requireLogin(),
+    RateLimiterMiddleware.rateLimit({
+      endpointName: 'open-project',
+      maxRequests: 20,
+      timeInterval: 60
+    }),
     ProjectController.projectListPage
   )
   webRouter.post(
