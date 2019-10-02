c7a7887d6718990a29e764d28cb44a23104b8a3f
Brian Gough <brian.gough@overleaf.com>
2019-10-02 16:47:36 +0100

    remove decaff comments

--- services/spelling/test/unit/js/ASpellWorkerTests.js
+++ services/spelling/test/unit/js/ASpellWorkerTests.js
@@ -2,13 +2,6 @@
     handle-callback-err,
     no-undef
 */
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const sinon = require('sinon')
 const chai = require('chai')
 const { expect } = chai
