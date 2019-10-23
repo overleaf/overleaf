3be4883649b654b212b6500b8f5c694272741599
Ersun Warncke <ersun.warncke@overleaf.com>
2019-10-23 14:06:31 -0400

    block sso registration for institutional sso domains
    GitOrigin-RevId: f1ac63cf47bb41bd3f3603e38a50ed43c9f03650

--- services/web/app/src/Features/Institutions/InstitutionsAPI.js
+++ services/web/app/src/Features/Institutions/InstitutionsAPI.js
@@ -17,24 +17,6 @@ const settings = require('settings-sharelatex')
 const request = require('request')
 const { promisifyAll } = require('../../util/promises')
 const NotificationsBuilder = require('../Notifications/NotificationsBuilder')
-const V1Api = require('../V1/V1Api')
-
-function getInstitutionViaDomain(domain) {
-  return new Promise(function(resolve, reject) {
-    V1Api.request(
-      {
-        timeout: 20 * 1000,
-        uri: `api/v1/sharelatex/university_saml?hostname=${domain}`
-      },
-      function(error, response, body) {
-        if (error) {
-          reject(error)
-        }
-        resolve(body)
-      }
-    )
-  })
-}
 
 const InstitutionsAPI = {
   getInstitutionAffiliations(institutionId, callback) {
@@ -50,8 +32,6 @@ const InstitutionsAPI = {
       (error, body) => callback(error, body || [])
     )
   },
-
-  getInstitutionViaDomain,
 
   getInstitutionLicences(institutionId, startDate, endDate, lag, callback) {
     if (callback == null) {
