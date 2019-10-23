da7ef926681559934c291b97817235c462417056
Miguel Serrano <mserranom@users.noreply.github.com>
2019-10-23 10:48:35 +0200

    Added modules/modules-*.js to sync with public repo (#2278)
    GitOrigin-RevId: 86cc5a7f9129fa774faa63b7a52cd5569dda7a5a

---
+++ services/web/modules/modules-ide.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+const MODULES_PATH = path.join(__dirname, './')
+
+const entryPoints = []
+if (fs.existsSync(MODULES_PATH)) {
+  fs.readdirSync(MODULES_PATH).reduce((acc, module) => {
+    const entryPath = path.join(
+      MODULES_PATH,
+      module,
+      '/public/src/ide/index.js'
+    )
+    if (fs.existsSync(entryPath)) {
+      acc.push(entryPath)
+    }
+    return acc
+  }, entryPoints)
+}
+
+module.exports = function() {
+  return {
+    code: `define(['${entryPoints.join("', '")}'], function() {})`
+  }
+}
