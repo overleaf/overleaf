0aad6f2bc8405012ba438be9d4cc6369c55aaecc
Henry Oswald <henry.oswald@sharelatex.com>
2019-11-19 16:46:37 +0000

    Merge pull request #2386 from overleaf/ho-disable-view-precompile
    fix eslint issues
    
    GitOrigin-RevId: 90b5a0b81117768ef646c40f55d032426a3f78f8

--- services/web/app/src/infrastructure/Server.js
+++ services/web/app/src/infrastructure/Server.js
@@ -29,7 +29,6 @@ const RedirectManager = require('./RedirectManager')
 const ProxyManager = require('./ProxyManager')
 const translations = require('translations-sharelatex').setup(Settings.i18n)
 const Modules = require('./Modules')
-const Views = require('./Views')
 
 const ErrorController = require('../Features/Errors/ErrorController')
 const HttpErrorController = require('../Features/Errors/HttpErrorController')
@@ -169,7 +168,6 @@ expressLocals(webRouter, privateApiRouter, publicApiRouter)
 if (app.get('env') === 'production') {
   logger.info('Production Enviroment')
   app.enable('view cache')
-  // Views.precompileViews(app) // precompile templates
 }
 
 webRouter.use(function(req, res, next) {
