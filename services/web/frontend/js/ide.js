810aadde10d382935fe032eeaa4602433242020f
Eric Mc Sween <eric.mcsween@overleaf.com>
2020-01-06 10:35:12 -0500

    Merge pull request #2486 from overleaf/hb-fix-dropbox-wording
    Replace Sharelatex with Overleaf in Dropbox modals
    
    GitOrigin-RevId: 954963eaf2fa6136f6002b495d7003769a33b66a

--- services/web/frontend/js/ide.js
+++ services/web/frontend/js/ide.js
@@ -228,7 +228,7 @@ define([
           'Project Renamed or Deleted',
           `\
 This project has either been renamed or deleted by an external data source such as Dropbox.
-We don't want to delete your data on ShareLaTeX, so this project still contains your history and collaborators.
+We don't want to delete your data on Overleaf, so this project still contains your history and collaborators.
 If the project has been renamed please look in your project list for a new project under the new name.\
 `
         )
