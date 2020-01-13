c7ee5c6f5839b2a6730712d6f98bfb567b3799a0
decaffeinate <timothee.alby@gmail.com>
2020-01-13 20:01:13 +0100

    decaffeinate: Run post-processing cleanups on NotificationsControllerTest.coffee and 1 other file

--- services/notifications/test/unit/coffee/NotificationsTests.js
+++ services/notifications/test/unit/coffee/NotificationsTests.js
@@ -1,3 +1,12 @@
+/* eslint-disable
+    camelcase,
+    handle-callback-err,
+    no-dupe-keys,
+    no-return-assign,
+    no-unused-vars,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
