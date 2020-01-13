b667b9ad1caff34e0f1a4e0dc46bb0a38310add0
decaffeinate <timothee.alby@gmail.com>
2020-01-13 20:00:40 +0100

    decaffeinate: Run post-processing cleanups on HealthCheckController.coffee and 2 other files

--- services/notifications/app/coffee/HealthCheckController.js
+++ services/notifications/app/coffee/HealthCheckController.js
@@ -1,3 +1,10 @@
+/* eslint-disable
+    camelcase,
+    no-dupe-keys,
+    standard/no-callback-literal,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
