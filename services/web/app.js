d420c1e99b94db1975a9cbd14cee6fd645d77fcb
Brian Gough <briangough@users.noreply.github.com>
2020-01-13 13:13:18 +0000

    Merge pull request #2507 from overleaf/bg-graceful-shutdown
    graceful shutdown for batch imports
    
    GitOrigin-RevId: 37178109204868a5991b5d89a5dd3be4fc3b7c73

--- services/web/app.js
+++ services/web/app.js
@@ -48,4 +48,10 @@ if (!module.parent) {
   })
 }
 
+// handle SIGTERM for graceful shutdown in kubernetes
+process.on('SIGTERM', function(signal) {
+  logger.warn({ signal: signal }, 'received signal, shutting down')
+  Settings.shuttingDown = true
+})
+
 module.exports = Server.server
