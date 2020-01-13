36a899fd2fd3c5f148544dacd028c86e17deddd8
Tim Alby <timothee.alby@gmail.com>
2020-01-13 20:01:23 +0100

    prettier: convert individual decaffeinated files to Prettier format

--- services/notifications/config/settings.defaults.js
+++ services/notifications/config/settings.defaults.js
@@ -1,13 +1,15 @@
-let Settings;
-module.exports = (Settings = {
-	internal: {
-		notifications: {
-			port: 3042,
-			host: process.env["LISTEN_ADDRESS"] || "localhost"
-		}
-	},
+let Settings
+module.exports = Settings = {
+  internal: {
+    notifications: {
+      port: 3042,
+      host: process.env.LISTEN_ADDRESS || 'localhost'
+    }
+  },
 
-	mongo: {
-		url: process.env['MONGO_CONNECTION_STRING'] || `mongodb://${process.env["MONGO_HOST"] || "localhost"}/sharelatex`
-	}
-});
+  mongo: {
+    url:
+      process.env.MONGO_CONNECTION_STRING ||
+      `mongodb://${process.env.MONGO_HOST || 'localhost'}/sharelatex`
+  }
+}
