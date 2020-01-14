9e36de9da079e26afe2d3ef177b8662838a9d512
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-01-14 17:10:07 +0000

    Merge pull request #2520 from overleaf/revert-2488-as-import-pr-708
    Revert "[Imported] Vendor web fonts, (#708)"
    
    GitOrigin-RevId: a9276168b880326fda51ba3d172d6cbfbdf74f6a

--- services/web/webpack.config.prod.js
+++ services/web/webpack.config.prod.js
@@ -5,9 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const base = require('./webpack.config')
 
-// Use "smart" merge: attempts to combine loaders targeting the same file type,
-// overriding the base config
-module.exports = merge.smart(base, {
+module.exports = merge(base, {
   mode: 'production',
 
   // Enable a full source map. Generates a comment linking to the source map
@@ -16,26 +14,6 @@ module.exports = merge.smart(base, {
   output: {
     // Override filename to include hash for immutable caching
     filename: 'js/[name]-[chunkhash].js'
-  },
-
-  module: {
-    rules: [
-      {
-        // Override base font loading to add hash to filename so that we can
-        // use "immutable" caching
-        test: /\.(woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              publicPath: '/fonts/',
-              name: '[name]-[hash].[ext]'
-            }
-          }
-        ]
-      }
-    ]
   },
 
   optimization: {
