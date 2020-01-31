c1d81dc1ea5c58424d184321d9c8b341e5caf372
Jakob Ackermann <das7pad@outlook.com>
2020-01-31 15:45:26 +0100

    [misc] install a maintained ghostscript version and delete package lists
    also do not install vim and bail out in case any command exited with a
     non zero exit code.

--- services/filestore/install_deps.sh
+++ services/filestore/install_deps.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
+
+set -ex
+
 apt-get update
 
-apt-get install vim imagemagick optipng --yes
+apt-get install ghostscript imagemagick optipng --yes
 
-wget -q https://s3.amazonaws.com/sl-public-dev-assets/ghostscript-9.15.tar.gz -O /tmp/ghostscript-9.15.tar.gz
-cd /tmp
-tar -xvf /tmp/ghostscript-9.15.tar.gz
-cd /tmp/ghostscript-9.15 && ./configure && make && make install
-npm rebuild
+rm -rf /var/lib/apt/lists/*
+
 mkdir /app/user_files/ /app/uploads/ /app/template_files/
 chown -R node:node /app/user_files
 chown -R node:node /app/uploads
 chown -R node:node /app/template_files
-ls -al /app
