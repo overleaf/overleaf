f326d632ab74223bd666a9cb793e6b8b4b8bea06
Ersun Warncke <ersun.warncke@gmail.com>
2020-02-05 08:53:31 -0400

    Revert "Revert "optimize rootDoc_id validation""
    This reverts commit 13e4b22daa99b096cf2a9625212a855be59b1fdc.
    
    GitOrigin-RevId: f015bc2bb54e98d1271bc2417647638aa80ba843

--- services/web/test/unit/src/Compile/ClsiManagerTests.js
+++ services/web/test/unit/src/Compile/ClsiManagerTests.js
@@ -24,6 +24,9 @@ describe('ClsiManager', function() {
     this.DocumentUpdaterHandler = {
       getProjectDocsIfMatch: sinon.stub().callsArgWith(2, null, null)
     }
+    this.ProjectRootDocManager = {
+      setRootDocAutomatically: sinon.stub().yields()
+    }
     this.logger = {
       log: sinon.stub(),
       error: sinon.stub(),
@@ -63,6 +66,7 @@ describe('ClsiManager', function() {
         },
         '../Project/ProjectEntityHandler': this.ProjectEntityHandler,
         '../Project/ProjectGetter': this.ProjectGetter,
+        '../Project/ProjectRootDocManager': this.ProjectRootDocManager,
         '../DocumentUpdater/DocumentUpdaterHandler': this
           .DocumentUpdaterHandler,
         './ClsiCookieManager': () => this.ClsiCookieManager,
