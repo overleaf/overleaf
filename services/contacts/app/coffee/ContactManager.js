0b7e05732e2a246a3127451263647518a8532e5b
decaffeinate <s@sd.ai>
2020-02-17 07:37:55 +0000

    decaffeinate: Run post-processing cleanups on ContactManager.coffee and 3 other files

--- services/contacts/app/coffee/ContactManager.js
+++ services/contacts/app/coffee/ContactManager.js
@@ -1,3 +1,9 @@
+/* eslint-disable
+    camelcase,
+    handle-callback-err,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
