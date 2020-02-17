8f6dfedc6cee66e94fa819acb695a58959d8b5c8
Simon Detheridge <s@sd.ai>
2020-02-17 07:38:00 +0000

    prettier: convert app/js decaffeinated files to Prettier format

--- services/contacts/app/js/ContactManager.js
+++ services/contacts/app/js/ContactManager.js
@@ -10,51 +10,69 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-let ContactManager;
-const {db, ObjectId} = require("./mongojs");
-const logger = require('logger-sharelatex');
-const metrics = require('metrics-sharelatex');
+let ContactManager
+const { db, ObjectId } = require('./mongojs')
+const logger = require('logger-sharelatex')
+const metrics = require('metrics-sharelatex')
 
-module.exports = (ContactManager = {
-	touchContact(user_id, contact_id, callback) {
-		if (callback == null) { callback = function(error) {}; }
-		try {
-			user_id = ObjectId(user_id.toString());
-		} catch (error1) {
-			const error = error1;
-			return callback(error);
-		}
+module.exports = ContactManager = {
+  touchContact(user_id, contact_id, callback) {
+    if (callback == null) {
+      callback = function(error) {}
+    }
+    try {
+      user_id = ObjectId(user_id.toString())
+    } catch (error1) {
+      const error = error1
+      return callback(error)
+    }
 
-		const update = { $set: {}, $inc: {} };
-		update.$inc[`contacts.${contact_id}.n`] = 1;
-		update.$set[`contacts.${contact_id}.ts`] = new Date();
+    const update = { $set: {}, $inc: {} }
+    update.$inc[`contacts.${contact_id}.n`] = 1
+    update.$set[`contacts.${contact_id}.ts`] = new Date()
 
-		return db.contacts.update({
-			user_id
-		}, update, {
-			upsert: true
-		}, callback);
-	},
-	
-	getContacts(user_id, callback) {
-		if (callback == null) { callback = function(error) {}; }
-		try {
-			user_id = ObjectId(user_id.toString());
-		} catch (error1) {
-			const error = error1;
-			return callback(error);
-		}
-		
-		return db.contacts.findOne({
-			user_id
-		}, function(error, user) {
-			if (error != null) { return callback(error); }
-			return callback(null, user != null ? user.contacts : undefined);
-		});
-	}
-});
+    return db.contacts.update(
+      {
+        user_id
+      },
+      update,
+      {
+        upsert: true
+      },
+      callback
+    )
+  },
 
-[
-	'touchContact',
-	'getContacts',
-].map(method => metrics.timeAsyncMethod(ContactManager, method, 'mongo.ContactManager', logger));
+  getContacts(user_id, callback) {
+    if (callback == null) {
+      callback = function(error) {}
+    }
+    try {
+      user_id = ObjectId(user_id.toString())
+    } catch (error1) {
+      const error = error1
+      return callback(error)
+    }
+
+    return db.contacts.findOne(
+      {
+        user_id
+      },
+      function(error, user) {
+        if (error != null) {
+          return callback(error)
+        }
+        return callback(null, user != null ? user.contacts : undefined)
+      }
+    )
+  }
+}
+
+;['touchContact', 'getContacts'].map(method =>
+  metrics.timeAsyncMethod(
+    ContactManager,
+    method,
+    'mongo.ContactManager',
+    logger
+  )
+)
--- services/contacts/app/js/Errors.js
+++ services/contacts/app/js/Errors.js
@@ -4,15 +4,13 @@
 */
 // TODO: This file was created by bulk-decaffeinate.
 // Fix any style issues and re-enable lint.
-let Errors;
+let Errors
 var NotFoundError = function(message) {
-	const error = new Error(message);
-	error.name = "NotFoundError";
-	error.__proto__ = NotFoundError.prototype;
-	return error;
-};
-NotFoundError.prototype.__proto__ = Error.prototype;
+  const error = new Error(message)
+  error.name = 'NotFoundError'
+  error.__proto__ = NotFoundError.prototype
+  return error
+}
+NotFoundError.prototype.__proto__ = Error.prototype
 
-module.exports = (Errors =
-	{NotFoundError});
-
+module.exports = Errors = { NotFoundError }
