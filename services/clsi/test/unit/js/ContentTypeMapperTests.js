a62d8186e997be4d07fb288d8148cdc779133897
mserranom <mserranom@gmail.com>
2020-02-19 12:15:37 +0100

    prettier: convert test/unit decaffeinated files to Prettier format

--- services/clsi/test/unit/js/ContentTypeMapperTests.js
+++ services/clsi/test/unit/js/ContentTypeMapperTests.js
@@ -10,73 +10,72 @@
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const SandboxedModule = require('sandboxed-module');
-const sinon = require('sinon');
-require('chai').should();
-const modulePath = require('path').join(__dirname, '../../../app/js/ContentTypeMapper');
+const SandboxedModule = require('sandboxed-module')
+const sinon = require('sinon')
+require('chai').should()
+const modulePath = require('path').join(
+  __dirname,
+  '../../../app/js/ContentTypeMapper'
+)
 
 describe('ContentTypeMapper', function() {
+  beforeEach(function() {
+    return (this.ContentTypeMapper = SandboxedModule.require(modulePath))
+  })
 
-	beforeEach(function() {
-		return this.ContentTypeMapper = SandboxedModule.require(modulePath);
-	});
+  return describe('map', function() {
+    it('should map .txt to text/plain', function() {
+      const content_type = this.ContentTypeMapper.map('example.txt')
+      return content_type.should.equal('text/plain')
+    })
 
-	return describe('map', function() {
+    it('should map .csv to text/csv', function() {
+      const content_type = this.ContentTypeMapper.map('example.csv')
+      return content_type.should.equal('text/csv')
+    })
 
-		it('should map .txt to text/plain', function() {
-			const content_type = this.ContentTypeMapper.map('example.txt');
-			return content_type.should.equal('text/plain');
-		});
+    it('should map .pdf to application/pdf', function() {
+      const content_type = this.ContentTypeMapper.map('example.pdf')
+      return content_type.should.equal('application/pdf')
+    })
 
-		it('should map .csv to text/csv', function() {
-			const content_type = this.ContentTypeMapper.map('example.csv');
-			return content_type.should.equal('text/csv');
-		});
+    it('should fall back to octet-stream', function() {
+      const content_type = this.ContentTypeMapper.map('example.unknown')
+      return content_type.should.equal('application/octet-stream')
+    })
 
-		it('should map .pdf to application/pdf', function() {
-			const content_type = this.ContentTypeMapper.map('example.pdf');
-			return content_type.should.equal('application/pdf');
-		});
+    describe('coercing web files to plain text', function() {
+      it('should map .js to plain text', function() {
+        const content_type = this.ContentTypeMapper.map('example.js')
+        return content_type.should.equal('text/plain')
+      })
 
-		it('should fall back to octet-stream', function() {
-			const content_type = this.ContentTypeMapper.map('example.unknown');
-			return content_type.should.equal('application/octet-stream');
-		});
+      it('should map .html to plain text', function() {
+        const content_type = this.ContentTypeMapper.map('example.html')
+        return content_type.should.equal('text/plain')
+      })
 
-		describe('coercing web files to plain text', function() {
+      return it('should map .css to plain text', function() {
+        const content_type = this.ContentTypeMapper.map('example.css')
+        return content_type.should.equal('text/plain')
+      })
+    })
 
-			it('should map .js to plain text', function() {
-				const content_type = this.ContentTypeMapper.map('example.js');
-				return content_type.should.equal('text/plain');
-			});
+    return describe('image files', function() {
+      it('should map .png to image/png', function() {
+        const content_type = this.ContentTypeMapper.map('example.png')
+        return content_type.should.equal('image/png')
+      })
 
-			it('should map .html to plain text', function() {
-				const content_type = this.ContentTypeMapper.map('example.html');
-				return content_type.should.equal('text/plain');
-			});
+      it('should map .jpeg to image/jpeg', function() {
+        const content_type = this.ContentTypeMapper.map('example.jpeg')
+        return content_type.should.equal('image/jpeg')
+      })
 
-			return it('should map .css to plain text', function() {
-				const content_type = this.ContentTypeMapper.map('example.css');
-				return content_type.should.equal('text/plain');
-			});
-		});
-
-		return describe('image files', function() {
-
-			it('should map .png to image/png', function() {
-				const content_type = this.ContentTypeMapper.map('example.png');
-				return content_type.should.equal('image/png');
-			});
-
-			it('should map .jpeg to image/jpeg', function() {
-				const content_type = this.ContentTypeMapper.map('example.jpeg');
-				return content_type.should.equal('image/jpeg');
-			});
-				
-			return it('should map .svg to text/plain to protect against XSS (SVG can execute JS)', function() {
-				const content_type = this.ContentTypeMapper.map('example.svg');
-				return content_type.should.equal('text/plain');
-			});
-		});
-	});
-});
+      return it('should map .svg to text/plain to protect against XSS (SVG can execute JS)', function() {
+        const content_type = this.ContentTypeMapper.map('example.svg')
+        return content_type.should.equal('text/plain')
+      })
+    })
+  })
+})
--- services/clsi/test/unit/js/DraftModeManagerTests.js
+++ services/clsi/test/unit/js/DraftModeManagerTests.js
@@ -8,75 +8,79 @@
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const SandboxedModule = require('sandboxed-module');
-const sinon = require('sinon');
-require('chai').should();
-const modulePath = require('path').join(__dirname, '../../../app/js/DraftModeManager');
+const SandboxedModule = require('sandboxed-module')
+const sinon = require('sinon')
+require('chai').should()
+const modulePath = require('path').join(
+  __dirname,
+  '../../../app/js/DraftModeManager'
+)
 
 describe('DraftModeManager', function() {
-	beforeEach(function() {
-		return this.DraftModeManager = SandboxedModule.require(modulePath, { requires: {
-			"fs": (this.fs = {}),
-			"logger-sharelatex": (this.logger = {log() {}})
-		}
-	});});
-		
-	describe("_injectDraftOption", function() {
-		it("should add draft option into documentclass with existing options", function() {
-			return this.DraftModeManager
-				._injectDraftOption(`\
-\\documentclass[a4paper,foo=bar]{article}\
-`)
-				.should.equal(`\
-\\documentclass[draft,a4paper,foo=bar]{article}\
-`);
-		});
+  beforeEach(function() {
+    return (this.DraftModeManager = SandboxedModule.require(modulePath, {
+      requires: {
+        fs: (this.fs = {}),
+        'logger-sharelatex': (this.logger = { log() {} })
+      }
+    }))
+  })
 
-		return it("should add draft option into documentclass with no options", function() {
-			return this.DraftModeManager
-				._injectDraftOption(`\
-\\documentclass{article}\
+  describe('_injectDraftOption', function() {
+    it('should add draft option into documentclass with existing options', function() {
+      return this.DraftModeManager._injectDraftOption(`\
+\\documentclass[a4paper,foo=bar]{article}\
+`).should.equal(`\
+\\documentclass[draft,a4paper,foo=bar]{article}\
 `)
-				.should.equal(`\
+    })
+
+    return it('should add draft option into documentclass with no options', function() {
+      return this.DraftModeManager._injectDraftOption(`\
+\\documentclass{article}\
+`).should.equal(`\
 \\documentclass[draft]{article}\
-`);
-		});
-	});
-	
-	return describe("injectDraftMode", function() {
-		beforeEach(function() {
-			this.filename = "/mock/filename.tex";
-			this.callback = sinon.stub();
-			const content = `\
+`)
+    })
+  })
+
+  return describe('injectDraftMode', function() {
+    beforeEach(function() {
+      this.filename = '/mock/filename.tex'
+      this.callback = sinon.stub()
+      const content = `\
 \\documentclass{article}
 \\begin{document}
 Hello world
 \\end{document}\
-`;
-			this.fs.readFile = sinon.stub().callsArgWith(2, null, content);
-			this.fs.writeFile = sinon.stub().callsArg(2);
-			return this.DraftModeManager.injectDraftMode(this.filename, this.callback);
-		});
-	
-		it("should read the file", function() {
-			return this.fs.readFile
-				.calledWith(this.filename, "utf8")
-				.should.equal(true);
-		});
-		
-		it("should write the modified file", function() {
-			return this.fs.writeFile
-				.calledWith(this.filename, `\
+`
+      this.fs.readFile = sinon.stub().callsArgWith(2, null, content)
+      this.fs.writeFile = sinon.stub().callsArg(2)
+      return this.DraftModeManager.injectDraftMode(this.filename, this.callback)
+    })
+
+    it('should read the file', function() {
+      return this.fs.readFile
+        .calledWith(this.filename, 'utf8')
+        .should.equal(true)
+    })
+
+    it('should write the modified file', function() {
+      return this.fs.writeFile
+        .calledWith(
+          this.filename,
+          `\
 \\documentclass[draft]{article}
 \\begin{document}
 Hello world
 \\end{document}\
-`)
-				.should.equal(true);
-		});
-		
-		return it("should call the callback", function() {
-			return this.callback.called.should.equal(true);
-		});
-	});
-});
+`
+        )
+        .should.equal(true)
+    })
+
+    return it('should call the callback', function() {
+      return this.callback.called.should.equal(true)
+    })
+  })
+})
