516102e6fb691d6d65cc6c533b6749a0bf08feb0
Jakob Ackermann <das7pad@outlook.com>
2020-02-28 12:26:46 +0100

    [misc] test/acceptance: do not hard code fake credentials

--- services/filestore/test/acceptance/js/FilestoreTests.js
+++ services/filestore/test/acceptance/js/FilestoreTests.js
@@ -415,8 +415,8 @@ describe('Filestore', function() {
 
             const s3ClientSettings = {
               credentials: {
-                accessKeyId: 'fake',
-                secretAccessKey: 'fake'
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
               },
               endpoint: process.env.AWS_S3_ENDPOINT,
               sslEnabled: false,
