76af2e4319bb391d22e925a2fd84337a76661f86
Hugh O'Brien <hugh.obrien@overleaf.com>
2020-05-09 16:03:29 +0100

    linting
    GitOrigin-RevId: c177ac287aa98c16fa6c89d865e12a4a08779e0b

--- services/web/frontend/js/ide/chat/services/chatMessages.js
+++ services/web/frontend/js/ide/chat/services/chatMessages.js
@@ -15,7 +15,7 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-define(['../../../base'], (App) =>
+define(['../../../base'], App =>
   App.factory('chatMessages', function($http, ide) {
     const MESSAGES_URL = `/project/${ide.project_id}/messages`
     const MESSAGE_LIMIT = 50
