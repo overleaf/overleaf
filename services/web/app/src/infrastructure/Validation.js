3d411b98cf856757a4596950e7a2270d039946df
Eric Mc Sween <eric.mcsween@overleaf.com>
2020-05-20 10:18:08 -0400

    Merge pull request #2838 from overleaf/msm-fix-npm-warnings
    Fix npm warnings
    
    GitOrigin-RevId: afc57deb7c4d07d7e86c9c9964fe76ace7b46901

--- services/web/app/src/infrastructure/Validation.js
+++ services/web/app/src/infrastructure/Validation.js
@@ -1,7 +1,25 @@
 const { Joi: CelebrateJoi, celebrate, errors } = require('celebrate')
-const JoiObjectId = require('joi-mongodb-objectid')
+const { ObjectId } = require('./mongojs')
 
-const Joi = CelebrateJoi.extend(JoiObjectId)
+const objectIdValidator = {
+  name: 'objectId',
+  language: {
+    invalid: 'needs to be a valid ObjectId'
+  },
+  pre(value, state, options) {
+    if (!ObjectId.isValid(value)) {
+      return this.createError('objectId.invalid', { value }, state, options)
+    }
+
+    if (options.convert) {
+      return new ObjectId(value)
+    }
+
+    return value
+  }
+}
+
+const Joi = CelebrateJoi.extend(objectIdValidator)
 const errorMiddleware = errors()
 
 module.exports = { Joi, validate, errorMiddleware }
