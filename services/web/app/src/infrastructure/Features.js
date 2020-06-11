7d98a822081687aeff7a5ec7447d813dea44f5ad
Jessica Lawshe <jessica.lawshe@overleaf.com>
2020-06-11 10:24:11 -0500

    Merge pull request #2891 from overleaf/jel-remove-unused-feature-flags
    Remove unused redirect-sl feature flag and sharelatex-redirects module
    
    GitOrigin-RevId: a13ae586db3b7ab4440f2e941947be3a28d97741

--- services/web/app/src/infrastructure/Features.js
+++ services/web/app/src/infrastructure/Features.js
@@ -1,15 +1,4 @@
-/* eslint-disable
-    max-len,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-let Features
+const _ = require('lodash')
 const Settings = require('settings-sharelatex')
 const fs = require('fs')
 
@@ -25,12 +14,12 @@ const trackChangesModuleAvailable = fs.existsSync(
   `${__dirname}/../../../modules/track-changes`
 )
 
-module.exports = Features = {
+const Features = {
   externalAuthenticationSystemUsed() {
     return (
-      Settings.ldap != null ||
-      Settings.saml != null ||
-      (Settings.overleaf != null ? Settings.overleaf.oauth : undefined) != null
+      !!Settings.ldap ||
+      !!Settings.saml ||
+      !!_.get(Settings, ['overleaf', 'oauth'])
     )
   },
 
@@ -39,18 +28,15 @@ module.exports = Features = {
       case 'homepage':
         return Settings.enableHomepage
       case 'registration':
-        return (
-          !Features.externalAuthenticationSystemUsed() ||
-          Settings.overleaf != null
-        )
+        return !Features.externalAuthenticationSystemUsed() || Settings.overleaf
       case 'github-sync':
         return Settings.enableGithubSync
       case 'git-bridge':
         return Settings.enableGitBridge
       case 'custom-togglers':
-        return Settings.overleaf != null
+        return !!Settings.overleaf
       case 'oauth':
-        return Settings.oauth != null
+        return !!Settings.oauth
       case 'templates-server-pro':
         return Settings.overleaf == null
       case 'affiliations':
@@ -58,17 +44,15 @@ module.exports = Features = {
         // Checking both properties is needed for the time being to allow
         // enabling the feature in web-api and disabling in Server Pro
         // see https://github.com/overleaf/web-internal/pull/2127
-        return Settings.apis.v1 && !!Settings.apis.v1.url
-      case 'redirect-sl':
-        return Settings.redirectToV2 != null
+        return Settings.apis && Settings.apis.v1 && !!Settings.apis.v1.url
       case 'overleaf-integration':
-        return Settings.overleaf != null
+        return !!Settings.overleaf
       case 'references':
-        return Settings.apis.references.url != null
+        return !!_.get(Settings, ['apis', 'references', 'url'])
       case 'saml':
         return Settings.enableSaml
       case 'link-url':
-        return Settings.apis.linkedUrlProxy && Settings.apis.linkedUrlProxy.url
+        return _.get(Settings, ['apis', 'linkedUrlProxy', 'url'])
       case 'public-registration':
         return publicRegistrationModuleAvailable
       case 'support':
@@ -80,3 +64,5 @@ module.exports = Features = {
     }
   }
 }
+
+module.exports = Features
