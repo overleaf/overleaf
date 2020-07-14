2556fded1eb34ac5030f0c351ecb19853a04a40f
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-07-14 14:24:25 +0100

    Merge pull request #2998 from overleaf/as-beta-page-updates
    Update beta program page to include feedback survey link
    
    GitOrigin-RevId: 549cd2be01b8f64d952d0347c8c102d0d2efae24

--- services/web/app/src/Features/BetaProgram/BetaProgramController.js
+++ services/web/app/src/Features/BetaProgram/BetaProgramController.js
@@ -1,60 +1,47 @@
-/* eslint-disable
-    camelcase,
-    no-unused-vars,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-let BetaProgramController
 const BetaProgramHandler = require('./BetaProgramHandler')
 const UserGetter = require('../User/UserGetter')
 const Settings = require('settings-sharelatex')
 const logger = require('logger-sharelatex')
 const AuthenticationController = require('../Authentication/AuthenticationController')
 
-module.exports = BetaProgramController = {
+const BetaProgramController = {
   optIn(req, res, next) {
-    const user_id = AuthenticationController.getLoggedInUserId(req)
-    logger.log({ user_id }, 'user opting in to beta program')
-    if (user_id == null) {
+    const userId = AuthenticationController.getLoggedInUserId(req)
+    logger.log({ userId }, 'user opting in to beta program')
+    if (userId == null) {
       return next(new Error('no user id in session'))
     }
-    return BetaProgramHandler.optIn(user_id, function(err) {
+    BetaProgramHandler.optIn(userId, function(err) {
       if (err) {
         return next(err)
       }
-      return res.redirect('/beta/participate')
+      res.redirect('/beta/participate')
     })
   },
 
   optOut(req, res, next) {
-    const user_id = AuthenticationController.getLoggedInUserId(req)
-    logger.log({ user_id }, 'user opting out of beta program')
-    if (user_id == null) {
+    const userId = AuthenticationController.getLoggedInUserId(req)
+    logger.log({ userId }, 'user opting out of beta program')
+    if (userId == null) {
       return next(new Error('no user id in session'))
     }
-    return BetaProgramHandler.optOut(user_id, function(err) {
+    BetaProgramHandler.optOut(userId, function(err) {
       if (err) {
         return next(err)
       }
-      return res.redirect('/beta/participate')
+      res.redirect('/beta/participate')
     })
   },
 
   optInPage(req, res, next) {
-    const user_id = AuthenticationController.getLoggedInUserId(req)
-    logger.log({ user_id }, 'showing beta participation page for user')
-    return UserGetter.getUser(user_id, function(err, user) {
+    const userId = AuthenticationController.getLoggedInUserId(req)
+    logger.log({ user_id: userId }, 'showing beta participation page for user')
+    UserGetter.getUser(userId, function(err, user) {
       if (err) {
-        logger.warn({ err, user_id }, 'error fetching user')
+        logger.warn({ err, userId }, 'error fetching user')
         return next(err)
       }
-      return res.render('beta_program/opt_in', {
+      res.render('beta_program/opt_in', {
         title: 'sharelatex_beta_program',
         user,
         languages: Settings.languages
@@ -62,3 +49,5 @@ module.exports = BetaProgramController = {
     })
   }
 }
+
+module.exports = BetaProgramController
--- services/web/test/unit/src/BetaProgram/BetaProgramControllerTests.js
+++ services/web/test/unit/src/BetaProgram/BetaProgramControllerTests.js
@@ -1,26 +1,12 @@
-/* eslint-disable
-    max-len,
-    mocha/no-identical-title,
-    no-return-assign,
-    no-unused-vars,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const should = require('chai').should()
+require('chai').should()
 const SandboxedModule = require('sandboxed-module')
-const assert = require('assert')
 const path = require('path')
 const sinon = require('sinon')
+
 const modulePath = path.join(
   __dirname,
   '../../../../app/src/Features/BetaProgram/BetaProgramController'
 )
-const { expect } = require('chai')
 
 describe('BetaProgramController', function() {
   beforeEach(function() {
@@ -67,130 +53,113 @@ describe('BetaProgramController', function() {
       redirect: sinon.stub(),
       render: sinon.stub()
     }
-    return (this.next = sinon.stub())
+    this.next = sinon.stub()
   })
 
   describe('optIn', function() {
     beforeEach(function() {
-      return this.BetaProgramHandler.optIn.callsArgWith(1, null)
+      this.BetaProgramHandler.optIn.callsArgWith(1, null)
     })
 
     it("should redirect to '/beta/participate'", function() {
       this.BetaProgramController.optIn(this.req, this.res, this.next)
       this.res.redirect.callCount.should.equal(1)
-      return this.res.redirect.firstCall.args[0].should.equal(
-        '/beta/participate'
-      )
+      this.res.redirect.firstCall.args[0].should.equal('/beta/participate')
     })
 
     it('should not call next with an error', function() {
       this.BetaProgramController.optIn(this.req, this.res, this.next)
-      return this.next.callCount.should.equal(0)
-    })
-
-    it('should not call next with an error', function() {
-      this.BetaProgramController.optIn(this.req, this.res, this.next)
-      return this.next.callCount.should.equal(0)
+      this.next.callCount.should.equal(0)
     })
 
     it('should call BetaProgramHandler.optIn', function() {
       this.BetaProgramController.optIn(this.req, this.res, this.next)
-      return this.BetaProgramHandler.optIn.callCount.should.equal(1)
+      this.BetaProgramHandler.optIn.callCount.should.equal(1)
     })
 
     describe('when BetaProgramHandler.opIn produces an error', function() {
       beforeEach(function() {
-        return this.BetaProgramHandler.optIn.callsArgWith(1, new Error('woops'))
+        this.BetaProgramHandler.optIn.callsArgWith(1, new Error('woops'))
       })
 
       it("should not redirect to '/beta/participate'", function() {
         this.BetaProgramController.optIn(this.req, this.res, this.next)
-        return this.res.redirect.callCount.should.equal(0)
+        this.res.redirect.callCount.should.equal(0)
       })
 
       it('should produce an error', function() {
         this.BetaProgramController.optIn(this.req, this.res, this.next)
         this.next.callCount.should.equal(1)
-        return this.next.firstCall.args[0].should.be.instanceof(Error)
+        this.next.firstCall.args[0].should.be.instanceof(Error)
       })
     })
   })
 
   describe('optOut', function() {
     beforeEach(function() {
-      return this.BetaProgramHandler.optOut.callsArgWith(1, null)
+      this.BetaProgramHandler.optOut.callsArgWith(1, null)
     })
 
     it("should redirect to '/beta/participate'", function() {
       this.BetaProgramController.optOut(this.req, this.res, this.next)
       this.res.redirect.callCount.should.equal(1)
-      return this.res.redirect.firstCall.args[0].should.equal(
-        '/beta/participate'
-      )
+      this.res.redirect.firstCall.args[0].should.equal('/beta/participate')
     })
 
     it('should not call next with an error', function() {
       this.BetaProgramController.optOut(this.req, this.res, this.next)
-      return this.next.callCount.should.equal(0)
-    })
-
-    it('should not call next with an error', function() {
-      this.BetaProgramController.optOut(this.req, this.res, this.next)
-      return this.next.callCount.should.equal(0)
+      this.next.callCount.should.equal(0)
     })
 
     it('should call BetaProgramHandler.optOut', function() {
       this.BetaProgramController.optOut(this.req, this.res, this.next)
-      return this.BetaProgramHandler.optOut.callCount.should.equal(1)
+      this.BetaProgramHandler.optOut.callCount.should.equal(1)
     })
 
     describe('when BetaProgramHandler.optOut produces an error', function() {
       beforeEach(function() {
-        return this.BetaProgramHandler.optOut.callsArgWith(
-          1,
-          new Error('woops')
-        )
+        this.BetaProgramHandler.optOut.callsArgWith(1, new Error('woops'))
       })
 
       it("should not redirect to '/beta/participate'", function() {
         this.BetaProgramController.optOut(this.req, this.res, this.next)
-        return this.res.redirect.callCount.should.equal(0)
+        this.res.redirect.callCount.should.equal(0)
       })
 
       it('should produce an error', function() {
         this.BetaProgramController.optOut(this.req, this.res, this.next)
         this.next.callCount.should.equal(1)
-        return this.next.firstCall.args[0].should.be.instanceof(Error)
+        this.next.firstCall.args[0].should.be.instanceof(Error)
       })
     })
   })
 
   describe('optInPage', function() {
     beforeEach(function() {
-      return this.UserGetter.getUser.callsArgWith(1, null, this.user)
+      this.UserGetter.getUser.callsArgWith(1, null, this.user)
     })
 
     it('should render the opt-in page', function() {
       this.BetaProgramController.optInPage(this.req, this.res, this.next)
       this.res.render.callCount.should.equal(1)
       const { args } = this.res.render.firstCall
-      return args[0].should.equal('beta_program/opt_in')
+      args[0].should.equal('beta_program/opt_in')
     })
 
     describe('when UserGetter.getUser produces an error', function() {
       beforeEach(function() {
-        return this.UserGetter.getUser.callsArgWith(1, new Error('woops'))
+        this.UserGetter.getUser.callsArgWith(1, new Error('woops'))
       })
 
       it('should not render the opt-in page', function() {
         this.BetaProgramController.optInPage(this.req, this.res, this.next)
-        return this.res.render.callCount.should.equal(0)
+        this.res.render.callCount.should.equal(0)
       })
 
       it('should produce an error', function() {
         this.BetaProgramController.optInPage(this.req, this.res, this.next)
         this.next.callCount.should.equal(1)
-        return this.next.firstCall.args[0].should.be.instanceof(Error)
+        this.next.firstCall.args[0].should.be.instanceof(Error)
       })
     })
   })
