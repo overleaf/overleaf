fd88819eec3aa78096555336f26aa9edf541ebb2
Jakob Ackermann <jakob.ackermann@overleaf.com>
2020-08-20 14:03:03 +0100

    [Router] _handleError: ol_context.doc_id does not exist, drop overwrite

--- services/real-time/app/js/Router.js
+++ services/real-time/app/js/Router.js
@@ -27,7 +27,7 @@ let Router
 module.exports = Router = {
   _handleError(callback, error, client, method, attrs) {
     attrs = attrs || {}
-    for (const key of ['project_id', 'doc_id', 'user_id']) {
+    for (const key of ['project_id', 'user_id']) {
       attrs[key] = client.ol_context[key]
     }
     attrs.client_id = client.id
