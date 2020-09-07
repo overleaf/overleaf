f6083a2d5109d24424f50252c71ec08ce4aeb485
Jakob Ackermann <jakob.ackermann@overleaf.com>
2020-09-07 09:31:50 +0100

    [misc] mongodb: refactor the process of setting up the db construct
    Co-Authored-By: John Lees-Miller <jdleesmiller@gmail.com>

--- services/chat/app/js/mongodb.js
+++ services/chat/app/js/mongodb.js
@@ -3,17 +3,21 @@ const { MongoClient, ObjectId } = require('mongodb')
 
 const clientPromise = MongoClient.connect(Settings.mongo.url)
 
+let setupDbPromise
 async function waitForDb() {
-  await clientPromise
+  if (!setupDbPromise) {
+    setupDbPromise = setupDb()
+  }
+  await setupDbPromise
 }
 
 const db = {}
-waitForDb().then(async function () {
+async function setupDb() {
   const internalDb = (await clientPromise).db()
 
   db.messages = internalDb.collection('messages')
   db.rooms = internalDb.collection('rooms')
-})
+}
 
 module.exports = {
   db,
