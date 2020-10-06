22046266e183126dceab62e63a1801675dde596c
Jessica Lawshe <jessica.lawshe@overleaf.com>
2020-10-06 08:50:24 -0500

    Merge pull request #3253 from overleaf/jel-email-app-name
    Use settings.appName in email
    
    GitOrigin-RevId: fee83af096be37b502331ee59a29f1093eb25fe9

--- services/web/app/src/Features/Email/Layouts/BaseWithHeaderEmailLayout.js
+++ services/web/app/src/Features/Email/Layouts/BaseWithHeaderEmailLayout.js
@@ -348,7 +348,7 @@ module.exports = _.template(`\
 							<table class="row collapse" style="border-collapse: collapse; border-spacing: 0; display: table; padding: 0; position: relative; text-align: left; vertical-align: top; width: 100%;"><tbody><tr style="padding: 0; text-align: left; vertical-align: top;">
 								<th class="small-12 large-12 columns first last" style="Margin: 0 auto; color: #5D6879; font-family: Helvetica, Arial, sans-serif; font-size: 16px; font-weight: normal; line-height: 1.3; margin: 0 auto; padding: 0; padding-bottom: 0; padding-left: 0; padding-right: 0; text-align: left; width: 588px;"><table style="border-collapse: collapse; border-spacing: 0; padding: 0; text-align: left; vertical-align: top; width: 100%;"><tr style="padding: 0; text-align: left; vertical-align: top;"><th style="Margin: 0; color: #5D6879; font-family: Helvetica, Arial, sans-serif; font-size: 16px; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left;">
 								<h1 style="Margin: 0; Margin-bottom: px; color: #FFFFFF; font-family: Georgia, serif; font-size: 30px; font-weight: normal; line-height: 1.3; margin: 0; margin-bottom: px; padding: 0; text-align: left; word-wrap: normal;">
-									Overleaf
+									${settings.appName}
 								</h1>
 								</th>
 								<th class="expander" style="Margin: 0; color: #5D6879; font-family: Helvetica, Arial, sans-serif; font-size: 16px; font-weight: normal; line-height: 1.3; margin: 0; padding: 0 !important; text-align: left; visibility: hidden; width: 0;"></th></tr></table></th>
