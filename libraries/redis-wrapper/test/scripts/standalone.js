7210b3dfedd081f505ff3ae58a098bc23edda45c
Jakob Ackermann <jakob.ackermann@overleaf.com>
2020-11-09 16:21:05 +0000

    [misc] decaffeinate tests: decaffeinate standalone script

--- libraries/redis-wrapper/test/scripts/standalone.js
+++ libraries/redis-wrapper/test/scripts/standalone.js
@@ -1,13 +1,16 @@
-# execute this script with a redis container running to test the health check
-# starting and stopping redis with this script running is a good test
+// execute this script with a redis container running to test the health check
+// starting and stopping redis with this script running is a good test
 
-redis = require "../.."
+const redis = require('../../')
 
-rclient = redis.createClient({host:"localhost",port:"6379"})
-setInterval () ->
-    rclient.healthCheck (err) ->
-        if err?
-            console.log "HEALTH CHECK FAILED", err
-        else
-            console.log "HEALTH CHECK OK"
-, 1000
+const rclient = redis.createClient({ host: 'localhost', port: '6379' })
+
+setInterval(() => {
+  rclient.healthCheck((err) => {
+    if (err) {
+      console.log('HEALTH CHECK FAILED', err)
+    } else {
+      console.log('HEALTH CHECK OK')
+    }
+  })
+}, 1000)
