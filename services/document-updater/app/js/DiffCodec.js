b74e7f6feb4d8bfcaca301abdf2d715204199f24
Eric Mc Sween <eric.mcsween@overleaf.com>
2020-12-07 15:25:52 -0500

    Decaf cleanup: unnecessary returns

--- services/document-updater/app/js/DiffCodec.js
+++ services/document-updater/app/js/DiffCodec.js
@@ -9,7 +9,6 @@
 // Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
@@ -51,6 +50,6 @@ module.exports = DiffCodec = {
         throw 'Unknown type'
       }
     }
-    return callback(null, ops)
+    callback(null, ops)
   }
 }
