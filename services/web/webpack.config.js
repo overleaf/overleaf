37f083809aaf980bf1bb6211fb9580976bba8baf
Alf Eaton <75253002+aeaton-overleaf@users.noreply.github.com>
2021-01-18 16:57:48 +0000

    Merge pull request #3578 from overleaf/ae-revert-transpile
    Revert "Add @overleaf/o-error to transpiled modules"
    
    GitOrigin-RevId: 111a8503f3e32be0b67296ec3a8c62453bc77675

--- services/web/webpack.config.js
+++ services/web/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
         // Only compile application files (npm and vendored dependencies are in
         // ES5 already)
         exclude: [
-          /node_modules\/(?!@overleaf\/o-error\/)/,
+          /node_modules/,
           path.resolve(__dirname, 'frontend/js/vendor')
         ],
         use: [
