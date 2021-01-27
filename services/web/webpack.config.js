8a6136efa7bef37a0bcf9dd9ae3214eba48e0181
Miguel Serrano <mserranom@users.noreply.github.com>
2021-01-27 10:53:51 +0100

    Merge pull request #3593 from overleaf/as-transpile-react-dnd
    Fix react-dnd crashing the editor on IE11
    
    GitOrigin-RevId: 2ab3725ef03a105de551dbc4a5b51538d03be2df

--- services/web/webpack.config.js
+++ services/web/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
         // Only compile application files (npm and vendored dependencies are in
         // ES5 already)
         exclude: [
-          /node_modules/,
+          /node_modules\/(?!react-dnd\/)/,
           path.resolve(__dirname, 'frontend/js/vendor')
         ],
         use: [
