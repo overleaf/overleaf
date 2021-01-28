979a2b0be5c76b619cbe48575bea18d4a38bc267
Eric Mc Sween <eric.mcsween@overleaf.com>
2021-01-28 08:40:18 -0500

    Merge pull request #3607 from overleaf/em-bull-completed
    Keep a fixed number of completed jobs in Bull
    
    GitOrigin-RevId: c4a1cdf9ce811be7624b46c876fca0ac1d4253c6

--- services/web/app/src/infrastructure/Queues.js
+++ services/web/app/src/infrastructure/Queues.js
@@ -3,6 +3,11 @@ const Settings = require('settings-sharelatex')
 
 const analyticsQueues = {}
 
+// Bull will keep a fixed number of the most recently completed jobs. This is
+// useful to inspect recently completed jobs. The bull prometheus exporter also
+// uses the completed job records to report on job duration.
+const MAX_COMPLETED_JOBS_RETAINED = 10000
+
 function initialize() {
   if (Settings.analytics.enabled) {
     analyticsQueues.events = createQueue('analytics-events')
@@ -14,7 +19,7 @@ function createQueue(queueName, defaultJobOptions) {
   return new Queue(queueName, {
     redis: Settings.redis.queues,
     defaultJobOptions: {
-      removeOnComplete: true,
+      removeOnComplete: MAX_COMPLETED_JOBS_RETAINED,
       attempts: 11,
       backoff: {
         type: 'exponential',
