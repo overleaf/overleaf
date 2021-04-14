507548d6615f9363d8047815b1172bbd0c0bb42a
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-04-14 12:54:46 +0200

    [migrations] fix index creation (#871)

--- server-ce/migrations/12_add_deletedFiles_indexes.js
+++ server-ce/migrations/12_add_deletedFiles_indexes.js
@@ -4,22 +4,21 @@ const Settings = require('settings-sharelatex')
 const mongojs = require('mongojs')
 const db = mongojs(Settings.mongo.url, ['deletedFiles'])
 
-const INDEX_FILTER = { 'projectId_1': 1 }
+const INDEX_NAME = 'projectId_1'
+const INDEX_KEYS = { projectId: 1 }
 const INDEX_OPTIONS = {
-  key: {
-    projectId: 1
-  },
+  name: INDEX_NAME,
   background: 1
 }
 
 exports.migrate = (client, done) => {
   db.deletedFiles.ensureIndex(
-    INDEX_FILTER,
+    INDEX_KEYS,
     INDEX_OPTIONS,
     done
   )
 }
 
 exports.rollback = (client, done) => {
-  db.deletedFiles.dropIndex(INDEX_FILTER, done)
+  db.deletedFiles.dropIndex(INDEX_NAME, done)
 }
