8c984a77c484eb35a88b291adbaa38beeb139ca2
decaffeinate <alf.eaton@overleaf.com>
2021-05-19 16:50:45 +0100

    decaffeinate: Convert settings.test.coffee to JS

--- services/clsi/test/acceptance/scripts/settings.test.js
+++ services/clsi/test/acceptance/scripts/settings.test.js
@@ -1,47 +1,59 @@
-Path = require "path"
+const Path = require("path");
 
-module.exports =
-	# Options are passed to Sequelize.
-	# See http://sequelizejs.com/documentation#usage-options for details
-	mysql:
-		clsi:
-			database: "clsi"
-			username: "clsi"
-			password: null
-			dialect: "sqlite"
+module.exports = {
+	// Options are passed to Sequelize.
+	// See http://sequelizejs.com/documentation#usage-options for details
+	mysql: {
+		clsi: {
+			database: "clsi",
+			username: "clsi",
+			password: null,
+			dialect: "sqlite",
 			storage: Path.resolve("db.sqlite")
+		}
+	},
 
-	path:
-		compilesDir:  Path.resolve(__dirname + "/../../../compiles")
-		clsiCacheDir: Path.resolve(__dirname + "/../../../cache")
-		#synctexBaseDir: (project_id) -> Path.join(@compilesDir, project_id)
-		synctexBaseDir: () -> "/compile"
+	path: {
+		compilesDir:  Path.resolve(__dirname + "/../../../compiles"),
+		clsiCacheDir: Path.resolve(__dirname + "/../../../cache"),
+		//synctexBaseDir: (project_id) -> Path.join(@compilesDir, project_id)
+		synctexBaseDir() { return "/compile"; },
 		sandboxedCompilesHostDir: process.env['SANDBOXED_COMPILES_HOST_DIR']
+	},
 
-	clsi:
-		#strace: true
-		#archive_logs: true
-		commandRunner: "docker-runner-sharelatex"
-		latexmkCommandPrefix: ["/usr/bin/time", "-v"]         # on Linux
-		docker:
-			image: process.env.TEXLIVE_IMAGE || "texlive-full:2017.1-opt"
-			env:
-				PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/texlive/2017/bin/x86_64-linux/"
+	clsi: {
+		//strace: true
+		//archive_logs: true
+		commandRunner: "docker-runner-sharelatex",
+		latexmkCommandPrefix: ["/usr/bin/time", "-v"],         // on Linux
+		docker: {
+			image: process.env.TEXLIVE_IMAGE || "texlive-full:2017.1-opt",
+			env: {
+				PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/texlive/2017/bin/x86_64-linux/",
 				HOME: "/tmp"
-			modem:
+			},
+			modem: {
 				socketPath: false
+			},
 			user: process.env.SIBLING_CONTAINER_USER ||"111"
+		}
+	},
 
-	internal:
-		clsi:
-			port: 3013
-			load_port: 3044
+	internal: {
+		clsi: {
+			port: 3013,
+			load_port: 3044,
 			host: "localhost"
+		}
+	},
 
-	apis:
-		clsi:
+	apis: {
+		clsi: {
 			url: "http://localhost:3013"
+		}
+	},
 
-	smokeTest: false
-	project_cache_length_ms: 1000 * 60 * 60 * 24
+	smokeTest: false,
+	project_cache_length_ms: 1000 * 60 * 60 * 24,
 	parallelFileDownloads:1
+};
