614734c3e1216ff92c8f971aca09d3a57c03e4ea
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-05-27 11:30:12 +0200

    Merge pull request #4100 from overleaf/ab-sub-page-click-event
    Add event on subscription page upgrade button click
    
    GitOrigin-RevId: 964eb6ad2d6ee7eceb3aac54501a61601d4948ac

--- services/web/frontend/js/main/subscription/upgrade-subscription.js
+++ services/web/frontend/js/main/subscription/upgrade-subscription.js
@@ -5,6 +5,7 @@ export default App.controller(
   function ($scope, eventTracking) {
     $scope.upgradeSubscription = function () {
       eventTracking.send('subscription-funnel', 'subscription-page', 'upgrade')
+      eventTracking.sendMB('subscription-page-upgrade-button-click')
     }
   }
 )
