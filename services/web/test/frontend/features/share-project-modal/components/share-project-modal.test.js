cc6629693eff95afe089c21752de453e74f70fdc
Alf Eaton <alf.eaton@overleaf.com>
2021-05-28 10:04:32 +0100

    Merge pull request #4114 from overleaf/ae-fix-share-modal-test
    Fix share modal test by ensuring that scoped value is used
    
    GitOrigin-RevId: 618dd15bf1b21403cc9a64d3174707ee713bda86

--- services/web/test/frontend/features/share-project-modal/components/share-project-modal.test.js
+++ services/web/test/frontend/features/share-project-modal/components/share-project-modal.test.js
@@ -10,6 +10,7 @@ import {
   waitForElementToBeRemoved,
 } from '@testing-library/react'
 import fetchMock from 'fetch-mock'
+import { get } from 'lodash'
 import ShareProjectModal from '../../../../../frontend/js/features/share-project-modal/components/share-project-modal'
 import * as locationModule from '../../../../../frontend/js/features/share-project-modal/utils/location'
 
@@ -70,11 +71,16 @@ describe('<ShareProjectModal/>', function () {
   ]
 
   const ideWithProject = project => {
+    const scope = { project }
+
     return {
       $scope: {
-        $watch: () => () => {},
+        $watch: (path, callback) => {
+          callback(get(scope, path))
+          return () => null
+        },
         $applyAsync: () => {},
-        project,
+        ...scope,
       },
     }
   }
@@ -243,7 +249,7 @@ describe('<ShareProjectModal/>', function () {
     )
 
     await screen.findByText(
-      'To change access permissions, please ask the project owner'
+      'To add more collaborators or turn on link sharing, please ask the project owner'
     )
 
     expect(screen.queryByRole('button', { name: 'Turn off link sharing' })).to
