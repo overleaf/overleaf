ed3507977bae62f84b46b8eaf95f37faa68b36ec
Shane Kilkelly <shane.kilkelly@overleaf.com>
2021-06-16 17:13:16 +0100

    Switch to UseContainerSupport and MaxRAMPercentage

--- services/git-bridge/start.sh
+++ services/git-bridge/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /opt/envsubst < /envsubst_template.json > /conf/runtime.json
-exec java -Xms512m -Xmx3072m -jar /git-bridge.jar /conf/runtime.json
+exec java -XX:+UseContainerSupport -XX:MaxRAMPercentage=75.0 -jar /git-bridge.jar /conf/runtime.json
