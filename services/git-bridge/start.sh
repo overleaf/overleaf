f51b8139e96c570198cd89e747550f2a358ce27d
Simon Detheridge <s@sd.ai>
2021-06-23 12:43:00 +0100

    Allow overriding JVM args with an environment variable

--- services/git-bridge/start.sh
+++ services/git-bridge/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 /opt/envsubst < /envsubst_template.json > /conf/runtime.json
-exec java -XX:+UseContainerSupport -XX:MaxRAMPercentage=50.0 -Xlog:gc* -jar /git-bridge.jar /conf/runtime.json
+
+if [ "x$GIT_BRIDGE_JVM_ARGS" == "x" ]; then
+  GIT_BRIDGE_JVM_ARGS="-XX:+UseContainerSupport -XX:MaxRAMPercentage=50.0"
+fi
+
+exec java $GIT_BRIDGE_JVM_ARGS -jar /git-bridge.jar /conf/runtime.json
