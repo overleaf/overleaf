6b4854f34ec495a48bc9e15d349ffcc6329e1f17
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-07-12 17:24:29 +0100

    [misc] goodbye grunt

--- init_scripts/98_check_db_access.sh
+++ init_scripts/98_check_db_access.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Checking can connect to mongo and redis"
-cd /var/www/sharelatex && grunt check:redis
-cd /var/www/sharelatex && grunt check:mongo
+cd /var/www/sharelatex/web
+node scripts/server-ce/check-mongodb
+node scripts/server-ce/check-redis
 echo "All checks passed"
