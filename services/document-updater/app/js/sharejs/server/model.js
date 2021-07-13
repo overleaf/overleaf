e8697f7f976b9df03d62ae1b7eb05462dc2ce5b5
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-07-13 12:21:07 +0100

    [misc] temporary override a few new/changed eslint rules

--- services/document-updater/app/js/sharejs/server/model.js
+++ services/document-updater/app/js/sharejs/server/model.js
@@ -1,7 +1,6 @@
 /* eslint-disable
     no-console,
     no-return-assign,
-    standard/no-callback-literal,
 */
 // TODO: This file was created by bulk-decaffeinate.
 // Fix any style issues and re-enable lint.
