16876378c79fa58038e1ba7caa7bc48a3f0ad1f2
Shane Kilkelly <shane.kilkelly@overleaf.com>
2021-07-15 10:16:17 +0100

    Add init script to run migrations from web

---
+++ init_scripts/99_run_web_migrations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "${SHARELATEX_IS_SERVER_PRO:-null}" == "true" ]]; then
+  environment="server-pro"
+else
+  environment="server-ce"
+fi
+
+echo "Running migrations for $environment"
+cd /var/www/sharelatex/web
+npm run migrations -- migrate -t "$environment"
+echo "Finished migrations"
