d1c06d1878ceab2039fba49b971be5ca486cfc66
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-07-20 14:12:42 +0100

    [perf] trim down install_deps.sh -- install docker cli only

--- services/clsi/install_deps.sh
+++ services/clsi/install_deps.sh
@@ -1,4 +1,24 @@
-/bin/sh
-wget -qO- https://get.docker.com/ | sh
-apt-get install poppler-utils vim ghostscript --yes
-npm rebuild
+#!/bin/bash
+set -ex
+
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" \
+  > /etc/apt/sources.list.d/docker.list
+apt-get update
+
+apt-get install -y \
+  docker-ce-cli \
+  poppler-utils \
+  ghostscript \
+
+rm -rf /var/lib/apt/lists/*
