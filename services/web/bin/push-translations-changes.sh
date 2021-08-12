7dc6456dcae263b38f247335206d8c87de513610
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-08-12 17:54:36 +0200

    Merge pull request #4685 from overleaf/jpa-translations-sync
    [misc] update translations sync for monorepo
    
    GitOrigin-RevId: 794a9192212f7b63c040a08c42a1e9c6b3341609

--- services/web/bin/push-translations-changes.sh
+++ services/web/bin/push-translations-changes.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$BRANCH_NAME" ]]; then
-  BRANCH_NAME=master
-fi
+SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+WEB_DIR=$(dirname "$SCRIPT_DIR")
+
+cd "$WEB_DIR"
 
 if [[ `git status --porcelain=2 locales/` ]]; then
   git add locales/*
   git commit -m "auto update translation"
-  git push "$UPSTREAM_REPO" "HEAD:$BRANCH_NAME"
+  # Switch the cloudbuild clone from https to ssh authentication.
+  git remote set-url --push origin git@github.com:overleaf/internal.git
+  git push origin "HEAD:$BRANCH_NAME"
 else
   echo 'No changes'
 fi
---
+++ services/web/scripts/translations/Dockerfile
@@ -0,0 +1,7 @@
+FROM node:12.22.3
+
+WORKDIR /app/scripts/translations
+
+COPY . /app/scripts/translations/
+
+RUN npm ci --quiet
