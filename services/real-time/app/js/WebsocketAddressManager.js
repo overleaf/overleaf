6cece12369db8ddadcf06d5ed6bd201739053824
Brian Gough <briangough@users.noreply.github.com>
2021-09-15 09:10:25 +0100

    Merge pull request #5109 from overleaf/bg-fix-handshake-exception
    fix handshake exception
    
    GitOrigin-RevId: 917518ccac3f5fddf4029f1b1ec3e3806dde9a29

--- services/real-time/app/js/WebsocketAddressManager.js
+++ services/real-time/app/js/WebsocketAddressManager.js
@@ -11,17 +11,26 @@ module.exports = class WebsocketAddressManager {
   }
 
   getRemoteIp(clientHandshake) {
-    if (this.trust) {
+    if (!clientHandshake) {
+      return 'client-handshake-missing'
+    } else if (this.trust) {
       // create a dummy req object using the client handshake and
       // connection.remoteAddress for the proxy-addr module to parse
-      const addressPort = clientHandshake.address
-      const req = Object.create(clientHandshake, {
-        connection: {
-          value: { remoteAddress: addressPort && addressPort.address },
-        },
-      })
-      // return the address parsed from x-forwarded-for
-      return proxyaddr(req, this.trust)
+      try {
+        const addressPort = clientHandshake.address
+        const req = {
+          headers: {
+            'x-forwarded-for':
+              clientHandshake.headers &&
+              clientHandshake.headers['x-forwarded-for'],
+          },
+          connection: { remoteAddress: addressPort && addressPort.address },
+        }
+        // return the address parsed from x-forwarded-for
+        return proxyaddr(req, this.trust)
+      } catch (err) {
+        return 'client-handshake-invalid'
+      }
     } else {
       // return the address from the client handshake itself
       return clientHandshake.address && clientHandshake.address.address
