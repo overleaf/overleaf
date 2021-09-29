471bc70463d6d0f868037161c08890a5378a4f2a
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-09-29 11:04:10 +0200

    Merge pull request #5217 from overleaf/jpa-web-templates-de-ng
    [web] de-ng templates module (server-pro)
    
    GitOrigin-RevId: 8d2ffd6a109d61b11d6d7c1acb781b429da3891c

--- services/web/frontend/js/marketing.js
+++ services/web/frontend/js/marketing.js
@@ -5,6 +5,8 @@ import './features/form-helpers/hydrate-form'
 import './features/link-helpers/slow-link'
 import './features/contact-form'
 import './features/event-tracking'
+import './features/fallback-image'
 import './features/multi-submit'
 
 $('[data-ol-lang-selector-tooltip]').tooltip({ trigger: 'hover' })
+$('[data-toggle="tooltip"]').tooltip()
