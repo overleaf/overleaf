49adb3581106ed0b2ffe26e84fd238101d15f749
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-10-21 13:44:31 +0200

    Merge pull request #5515 from overleaf/jpa-new-cdn-caching
    [gdm] enable caching for the new CDN
    
    GitOrigin-RevId: 01128d2cc730198c726efb7b7bfd576a1fbd268e

--- services/web/frontend/js/cdn-load-test.js
+++ services/web/frontend/js/cdn-load-test.js
@@ -3,13 +3,13 @@ import { captureMessage } from './infrastructure/error-reporter'
 if (window.ExposedSettings.isOverleaf) {
   const cdnLoadTest = document.createElement('img')
   cdnLoadTest.addEventListener('error', function () {
-    captureMessage('CDN test image load error (cdn.overleaf.net)')
+    captureMessage('CDN test 2 image load error (cdn.overleaf.net)')
   })
   cdnLoadTest.src = 'https://cdn.overleaf.net/img/1p.gif'
 
   const cdnLoadTestOld = document.createElement('img')
   cdnLoadTestOld.addEventListener('error', function () {
-    captureMessage('CDN test image load error (cdn.overleaf.com)')
+    captureMessage('CDN test 2 image load error (cdn.overleaf.com)')
   })
   cdnLoadTestOld.src = 'https://cdn.overleaf.com/img/1p.gif'
 }
