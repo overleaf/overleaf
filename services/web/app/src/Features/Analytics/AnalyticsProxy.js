fdb5386b859e836b8df18cea02f6b664342919cc
Eric Mc Sween <eric.mcsween@overleaf.com>
2021-10-21 14:37:09 -0400

    Merge pull request #5547 from overleaf/em-ta-analytics-proxy-fix
    Fix how AnalyticsProxy generates the URL
    
    GitOrigin-RevId: 272e7ea92d1aa41f7390d39c590336a23dba3708

--- services/web/app/src/Features/Analytics/AnalyticsProxy.js
+++ services/web/app/src/Features/Analytics/AnalyticsProxy.js
@@ -1,7 +1,6 @@
 const settings = require('@overleaf/settings')
 const Errors = require('../Errors/Errors')
 const httpProxy = require('express-http-proxy')
-const { URL } = require('url')
 
 module.exports = {
   call(basePath) {
@@ -16,9 +15,9 @@ module.exports = {
 
     return httpProxy(settings.apis.analytics.url, {
       proxyReqPathResolver(req) {
-        const u = new URL(req.originalUrl, settings.siteUrl)
-        const requestPath = u.pathname + u.search
-        return `${basePath}${requestPath}`
+        // req.url is the part of the path that comes after the mount point in
+        // app.use()
+        return `${basePath}${req.url}`
       },
       proxyReqOptDecorator(proxyReqOpts, srcReq) {
         proxyReqOpts.headers = {} // unset all headers
