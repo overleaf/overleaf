e8c645bf2836540a577d885550808876af40f90b
Miguel Serrano <mserranom@users.noreply.github.com>
2021-12-07 10:18:48 +0100

    Using uuid for compilation logs (#6003)
    GitOrigin-RevId: 96646870da597881a6e8b26564be4dee26385bb8

--- services/web/frontend/js/features/pdf-preview/util/output-files.js
+++ services/web/frontend/js/features/pdf-preview/util/output-files.js
@@ -2,6 +2,7 @@ import getMeta from '../../../utils/meta'
 import HumanReadableLogs from '../../../ide/human-readable-logs/HumanReadableLogs'
 import BibLogParser from '../../../ide/log-parser/bib-log-parser'
 import { buildFileList } from './file-list'
+import { v4 as uuid } from 'uuid'
 
 const searchParams = new URLSearchParams(window.location.search)
 
@@ -68,7 +69,7 @@ export const handleOutputFiles = async (projectId, data) => {
           if (entry.file) {
             entry.file = normalizeFilePath(entry.file)
           }
-          entry.key = `${entry.file}:${entry.line}:${entry.column}:${entry.message}`
+          entry.key = uuid()
         }
         result.logEntries[key].push(...newEntries[key])
       }
