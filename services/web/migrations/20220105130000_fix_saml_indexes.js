a6625e872f7686b59b810f41d687c053ec8e6d31
Jakob Ackermann <jakob.ackermann@overleaf.com>
2022-01-10 12:36:40 +0000

    Merge pull request #6297 from overleaf/jpa-fix-index
    [web] migrations: fix_saml_indexes: add missing unique flag
    
    GitOrigin-RevId: 6a9d7ac58edcca27ec2b4bb7ab6e78d75053bbaf

--- services/web/migrations/20220105130000_fix_saml_indexes.js
+++ services/web/migrations/20220105130000_fix_saml_indexes.js
@@ -7,7 +7,12 @@ exports.tags = ['saas']
 exports.migrate = async ({ db }) => {
   // Fix-up the previous SAML migrations that were operating on collections with
   //  typos in their names.
-  await Helpers.addIndexesToCollection(db.users, usersIndexes)
+  await Helpers.addIndexesToCollection(
+    db.users,
+    usersIndexes.map(index => {
+      return Object.assign({}, index, { unique: true })
+    })
+  )
   await Helpers.addIndexesToCollection(db.samlLogs, samlLogsIndexes)
 }
 
