b7a5614c95fee3e1d73679ad178d8fb1a005d033
Alf Eaton <alf.eaton@overleaf.com>
2022-03-18 10:13:26 +0000

    Add PDF Preview error boundary components to Storybook (#7065)
    GitOrigin-RevId: cc9ec43040b7c43a70b76e8174e92cee32445162

---
+++ services/web/frontend/stories/pdf-preview-error-boundary.stories.js
@@ -0,0 +1,22 @@
+import ErrorBoundaryFallback from '../js/features/pdf-preview/components/error-boundary-fallback'
+import { setupContext } from './fixtures/context'
+import { withContextRoot } from './utils/with-context-root'
+
+export default {
+  title: 'PDF Preview / Error Boundary',
+  component: ErrorBoundaryFallback,
+}
+
+setupContext()
+
+export const PreviewErrorBoundary = () => {
+  return withContextRoot(<ErrorBoundaryFallback type="preview" />)
+}
+
+export const PdfErrorBoundary = () => {
+  return withContextRoot(<ErrorBoundaryFallback type="pdf" />)
+}
+
+export const LogsErrorBoundary = () => {
+  return withContextRoot(<ErrorBoundaryFallback type="logs" />)
+}
