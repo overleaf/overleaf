ac3eb878d63bb101c3befce38403a82ff32bc51c
Timothée Alby <timothee.alby@gmail.com>
2022-03-23 10:20:30 +0100

    Merge pull request #7172 from overleaf/ta-pdf-viewer-backdrop-hide
    Fix Logo Backdrop in PDF Viewer
    
    GitOrigin-RevId: ae9eb98dc8a51a882354b6ddea095df2bed51346

--- services/web/frontend/js/features/pdf-preview/components/pdf-preview-pane.js
+++ services/web/frontend/js/features/pdf-preview/components/pdf-preview-pane.js
@@ -1,17 +1,23 @@
 import { memo, Suspense } from 'react'
+import classNames from 'classnames'
 import PdfLogsViewer from './pdf-logs-viewer'
 import PdfViewer from './pdf-viewer'
 import LoadingSpinner from '../../../shared/components/loading-spinner'
 import PdfHybridPreviewToolbar from './pdf-preview-hybrid-toolbar'
 import PdfPreviewToolbar from './pdf-preview-toolbar'
+import { useCompileContext } from '../../../shared/context/compile-context'
 
 const newPreviewToolbar = new URLSearchParams(window.location.search).has(
   'new_preview_toolbar'
 )
 
 function PdfPreviewPane() {
+  const { pdfUrl } = useCompileContext()
+  const classes = classNames('pdf', 'full-size', {
+    'pdf-empty': !pdfUrl,
+  })
   return (
-    <div className="pdf full-size">
+    <div className={classes}>
       {newPreviewToolbar ? <PdfPreviewToolbar /> : <PdfHybridPreviewToolbar />}
       <Suspense fallback={<LoadingPreview />}>
         <div className="pdf-viewer">
