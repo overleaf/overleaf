2056a85199ba6a3400c91b848d7d7a6360fd6e7d
Thomas <thomas-@users.noreply.github.com>
2022-04-04 08:40:21 +0200

    Merge pull request #7325 from overleaf/tm-new-monthly-pricing-mar2022
    Update monthly plan pricing (March 2022)
    
    GitOrigin-RevId: a8d3bea22e22358077a1a82636225926ffc59d53

--- services/web/frontend/js/main/plans.js
+++ services/web/frontend/js/main/plans.js
@@ -18,15 +18,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '$80',
         },
         personal: {
-          monthly: '$10',
+          monthly: '$13',
           annual: '$120',
         },
         collaborator: {
-          monthly: '$15',
+          monthly: '$19',
           annual: '$180',
         },
         professional: {
-          monthly: '$30',
+          monthly: '$38',
           annual: '$360',
         },
       },
@@ -38,15 +38,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '€70',
         },
         personal: {
-          monthly: '€9',
+          monthly: '€12',
           annual: '€108',
         },
         collaborator: {
-          monthly: '€14',
+          monthly: '€18',
           annual: '€168',
         },
         professional: {
-          monthly: '€28',
+          monthly: '€35',
           annual: '€336',
         },
       },
@@ -58,15 +58,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '£60',
         },
         personal: {
-          monthly: '£8',
+          monthly: '£10',
           annual: '£96',
         },
         collaborator: {
-          monthly: '£12',
+          monthly: '£15',
           annual: '£144',
         },
         professional: {
-          monthly: '£24',
+          monthly: '£30',
           annual: '£288',
         },
       },
@@ -78,15 +78,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '600 kr',
         },
         personal: {
-          monthly: '73 kr',
+          monthly: '92 kr',
           annual: '876 kr',
         },
         collaborator: {
-          monthly: '110 kr',
+          monthly: '138 kr',
           annual: '1320 kr',
         },
         professional: {
-          monthly: '220 kr',
+          monthly: '275 kr',
           annual: '2640 kr',
         },
       },
@@ -97,15 +97,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '$90',
         },
         personal: {
-          monthly: '$11',
+          monthly: '$14',
           annual: '$132',
         },
         collaborator: {
-          monthly: '$17',
+          monthly: '$22',
           annual: '$204',
         },
         professional: {
-          monthly: '$34',
+          monthly: '$43',
           annual: '$408',
         },
       },
@@ -117,15 +117,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '600 kr',
         },
         personal: {
-          monthly: '73 kr',
+          monthly: '92 kr',
           annual: '876 kr',
         },
         collaborator: {
-          monthly: '110 kr',
+          monthly: '138 kr',
           annual: '1320 kr',
         },
         professional: {
-          monthly: '220 kr',
+          monthly: '275 kr',
           annual: '2640 kr',
         },
       },
@@ -137,15 +137,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '500 kr',
         },
         personal: {
-          monthly: '60 kr',
+          monthly: '75 kr',
           annual: '720 kr',
         },
         collaborator: {
-          monthly: '90 kr',
+          monthly: '113 kr',
           annual: '1080 kr',
         },
         professional: {
-          monthly: '180 kr',
+          monthly: '225 kr',
           annual: '2160 kr',
         },
       },
@@ -157,15 +157,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '$100',
         },
         personal: {
-          monthly: '$12',
+          monthly: '$15',
           annual: '$144',
         },
         collaborator: {
-          monthly: '$18',
+          monthly: '$23',
           annual: '$216',
         },
         professional: {
-          monthly: '$35',
+          monthly: '$44',
           annual: '$420',
         },
       },
@@ -177,15 +177,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '$100',
         },
         personal: {
-          monthly: '$12',
+          monthly: '$15',
           annual: '$144',
         },
         collaborator: {
-          monthly: '$18',
+          monthly: '$23',
           annual: '$216',
         },
         professional: {
-          monthly: '$35',
+          monthly: '$44',
           annual: '$420',
         },
       },
@@ -197,15 +197,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: 'Fr 80',
         },
         personal: {
-          monthly: 'Fr 10',
+          monthly: 'Fr 13',
           annual: 'Fr 120',
         },
         collaborator: {
-          monthly: 'Fr 15',
+          monthly: 'Fr 19',
           annual: 'Fr 180',
         },
         professional: {
-          monthly: 'Fr 30',
+          monthly: 'Fr 38',
           annual: 'Fr 360',
         },
       },
@@ -217,15 +217,15 @@ App.factory('MultiCurrencyPricing', function () {
           annual: '$120',
         },
         personal: {
-          monthly: '$13',
+          monthly: '$17',
           annual: '$156',
         },
         collaborator: {
-          monthly: '$20',
+          monthly: '$25',
           annual: '$240',
         },
         professional: {
-          monthly: '$40',
+          monthly: '$50',
           annual: '$480',
         },
       },
