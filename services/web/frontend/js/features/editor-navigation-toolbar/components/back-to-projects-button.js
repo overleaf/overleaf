fcdd02fac12398541da20133a06db2be7065e593
Alf Eaton <alf.eaton@overleaf.com>
2022-05-09 12:01:36 +0100

    Merge pull request #7849 from overleaf/ae-menu-bar
    Change "back to your projects" icon to "home"
    
    GitOrigin-RevId: 2620b7efbaa5af38af365596c008b79961b00247

--- services/web/frontend/js/features/editor-navigation-toolbar/components/back-to-projects-button.js
+++ services/web/frontend/js/features/editor-navigation-toolbar/components/back-to-projects-button.js
@@ -1,16 +1,23 @@
 import Icon from '../../../shared/components/icon'
 import { useTranslation } from 'react-i18next'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 function BackToProjectsButton() {
   const { t } = useTranslation()
+
   return (
-    <a className="toolbar-header-back-projects" href="/project">
-      <Icon
-        type="level-up"
-        fw
-        accessibilityLabel={t('back_to_your_projects')}
-      />
-    </a>
+    <OverlayTrigger
+      placement="right"
+      overlay={
+        <Tooltip id="back-to-projects-tooltip">
+          {t('back_to_your_projects')}
+        </Tooltip>
+      }
+    >
+      <a className="toolbar-header-back-projects" href="/project">
+        <Icon type="home" fw accessibilityLabel={t('back_to_your_projects')} />
+      </a>
+    </OverlayTrigger>
   )
 }
 
