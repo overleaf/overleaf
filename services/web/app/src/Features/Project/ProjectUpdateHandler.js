12bd1d49e7d34140caac2de1b8fed2fd162c4847
Brian Gough <briangough@users.noreply.github.com>
2022-07-11 09:11:52 +0100

    Merge pull request #8802 from overleaf/bg-reset-last-updated-in-migration
    [web] reset last updated date in history migration
    
    GitOrigin-RevId: 3d06eadf7d1dfb9b92a529d68de68f59370ab5de

--- services/web/app/src/Features/Project/ProjectUpdateHandler.js
+++ services/web/app/src/Features/Project/ProjectUpdateHandler.js
@@ -13,8 +13,9 @@
  */
 const { Project } = require('../../models/Project')
 const logger = require('@overleaf/logger')
+const { promisifyAll } = require('../../util/promises')
 
-module.exports = {
+const ProjectUpdateHandler = {
   markAsUpdated(projectId, lastUpdatedAt, lastUpdatedBy, callback) {
     if (callback == null) {
       callback = function () {}
@@ -26,6 +27,26 @@ module.exports = {
     const conditions = {
       _id: projectId,
       lastUpdated: { $lt: lastUpdatedAt },
+    }
+
+    const update = {
+      lastUpdated: lastUpdatedAt || new Date().getTime(),
+      lastUpdatedBy,
+    }
+    return Project.updateOne(conditions, update, {}, callback)
+  },
+
+  // like markAsUpdated but allows lastUpdatedAt to be reset to earlier time
+  resetUpdated(projectId, lastUpdatedAt, lastUpdatedBy, callback) {
+    if (callback == null) {
+      callback = function () {}
+    }
+    if (lastUpdatedAt == null) {
+      lastUpdatedAt = new Date()
+    }
+
+    const conditions = {
+      _id: projectId,
     }
 
     const update = {
@@ -65,3 +86,6 @@ module.exports = {
     })
   },
 }
+
+ProjectUpdateHandler.promises = promisifyAll(ProjectUpdateHandler)
+module.exports = ProjectUpdateHandler
