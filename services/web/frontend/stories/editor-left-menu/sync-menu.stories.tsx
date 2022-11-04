a2c232643fb69ceb35335939f9b1a0d1bc680472
M Fahru <m.fahru@overleaf.com>
2022-11-04 16:26:34 -0700

    Add sync menu storybook components
    GitOrigin-RevId: 8fb5979e88ba601ef1a95852550a62c295b43f76

---
+++ services/web/frontend/stories/editor-left-menu/sync-menu.stories.tsx
@@ -0,0 +1,35 @@
+import SyncMenu from '../../js/features/editor-left-menu/components/sync-menu'
+import { ScopeDecorator } from '../decorators/scope'
+import { useScope } from '../hooks/use-scope'
+
+export default {
+  title: 'Editor / Left Menu / Sync Menu',
+  component: SyncMenu,
+  decorators: [ScopeDecorator],
+}
+
+export const WriteAccess = () => {
+  window.metaAttributesCache.set('ol-anonymous', false)
+  useScope({
+    permissionsLevel: 'owner',
+  })
+
+  return (
+    <div id="left-menu" className="shown">
+      <SyncMenu />
+    </div>
+  )
+}
+
+export const ReadOnlyAccess = () => {
+  window.metaAttributesCache.set('ol-anonymous', false)
+  useScope({
+    permissionsLevel: 'readOnly',
+  })
+
+  return (
+    <div id="left-menu" className="shown">
+      <SyncMenu />
+    </div>
+  )
+}
