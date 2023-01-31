b42c4e57a7a9d6cb0ee079353e60bf6614fd609f
Mathias Jakobsen <mathias.jakobsen@overleaf.com>
2023-01-31 14:43:23 +0000

    Merge pull request #11526 from overleaf/mj-ae-selection-changes
    [cm6+rt] Integration of selection changes
    
    GitOrigin-RevId: cd2574ca75eab0f8f1202d3d4b6a009f14fa33b5

--- services/web/frontend/stories/decorators/scope.tsx
+++ services/web/frontend/stories/decorators/scope.tsx
@@ -98,6 +98,7 @@ const initialize = () => {
       findEntityByPath: () => null,
       getEntityPath: () => null,
       getRootDocDirname: () => undefined,
+      getPreviewUrlByPath: () => undefined,
     },
     editorManager: {
       getCurrentDocId: () => 'foo',
