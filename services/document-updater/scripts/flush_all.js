909bb99b9f2a73aefe8551d746c384cf33f7eed3
Miguel Serrano <mserranom@users.noreply.github.com>
2023-02-08 16:02:21 +0100

    [CE/SP] Flush redis on `docker stop` (#11714)
    * [CE/SP] Flush redis on `docker stop`
    
    * Extracted `limit` to variable
    
    Co-authored-by: Jakob Ackermann <jakob.ackermann@overleaf.com>
    
    * Increased logging
    
    ---------
    
    Co-authored-by: Jakob Ackermann <jakob.ackermann@overleaf.com>
    GitOrigin-RevId: 4bcb4e70988337369acca0e80f243609bd6323ed

---
+++ services/document-updater/scripts/flush_all.js
@@ -0,0 +1,28 @@
+const ProjectFlusher = require('../app/js/ProjectFlusher')
+
+async function main() {
+  console.log('Flushing all projects')
+  return new Promise((resolve, reject) => {
+    const options = {
+      limit: 100000,
+      concurrency: 5,
+    }
+    ProjectFlusher.flushAllProjects(options, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+main()
+  .then(() => {
+    console.log('Done flushing all projects')
+    process.exit(0)
+  })
+  .catch(error => {
+    console.error('There was an error flushing all projects', { error })
+    process.exit(1)
+  })
---
+++ services/track-changes/scripts/flush_all.js
@@ -0,0 +1,25 @@
+const UpdatesManager = require('../app/js/UpdatesManager')
+
+async function main() {
+  return new Promise((resolve, reject) => {
+    const limit = -1
+    console.log('Flushing all updates')
+    UpdatesManager.flushAll(limit, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+main()
+  .then(() => {
+    console.log('Done flushing all updates')
+    process.exit(0)
+  })
+  .catch(error => {
+    console.error('There was an error flushing updates', { error })
+    process.exit(1)
+  })
