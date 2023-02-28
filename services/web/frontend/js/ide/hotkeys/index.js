44b1f555f19fd059d47b68f4f2b7e8144af1a6b2
ilkin-overleaf <100852799+ilkin-overleaf@users.noreply.github.com>
2023-02-28 11:37:38 +0200

    Merge pull request #12001 from overleaf/ae-backspace
    Remove BackspaceHighjack
    
    GitOrigin-RevId: 7b20bff7d52b20152c437ba8bc773c4f37ad1fc3

--- services/web/frontend/js/ide/hotkeys/index.js
+++ services/web/frontend/js/ide/hotkeys/index.js
@@ -1,4 +1,3 @@
 // TODO: This file was created by bulk-decaffeinate.
 // Fix any style issues and re-enable lint.
-import './BackspaceHighjack'
 import '../../features/hotkeys-modal/controllers/hotkeys-modal-controller'
