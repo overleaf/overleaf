3f992f9453009dfebbeac9a5641fa32651512809
M Fahru <m.fahru@overleaf.com>
2023-04-12 10:40:42 -0700

    Ensure new data are fetched properly whenever history is opened (#12577)
    GitOrigin-RevId: e0ae14cdc90c95dbd44b12bd8332bc982ea34a10

--- services/web/frontend/js/features/history/components/history-root.tsx
+++ services/web/frontend/js/features/history/components/history-root.tsx
@@ -1,18 +1,15 @@
 import ChangeList from './change-list/change-list'
 import DiffView from './diff-view/diff-view'
-import { useLayoutContext } from '../../../shared/context/layout-context'
 import { HistoryProvider, useHistoryContext } from '../context/history-context'
 import { createPortal } from 'react-dom'
 import HistoryFileTree from './history-file-tree'
-import getMeta from '../../../utils/meta'
 
 const fileTreeContainer = document.getElementById('history-file-tree')
 
 function Main() {
-  const { view } = useLayoutContext()
   const { updates } = useHistoryContext()
 
-  if (view !== 'history' || updates.length === 0) {
+  if (updates.length === 0) {
     return null
   }
 
@@ -30,12 +27,6 @@ function Main() {
 }
 
 export default function HistoryRoot() {
-  const isReact = getMeta('ol-splitTestVariants')?.['history-view'] === 'react'
-
-  if (!isReact) {
-    return null
-  }
-
   return (
     <HistoryProvider>
       <Main />
