d2172e51790e6269d418b7133d9202f4e0bba533
Miguel Serrano <mserranom@users.noreply.github.com>
2023-04-27 12:22:29 +0200

    [web] Issue Oauth2 access tokens (#12788)
    * [web] Issue Oauth2 access tokens
    
    * [web] Add partial index for `oauthAccessTokens.user_id` for Personal Access Tokens
    
    * [web] script to create personal access tokens
    
    GitOrigin-RevId: 796e8d23a6799a87ac6096c686139c6290668b83

--- services/web/app/src/models/OauthAccessToken.js
+++ services/web/app/src/models/OauthAccessToken.js
@@ -6,13 +6,17 @@ const { ObjectId } = Schema
 const OauthAccessTokenSchema = new Schema(
   {
     accessToken: String,
+    accessTokenPartial: String,
+    type: String,
     accessTokenExpiresAt: Date,
     oauthApplication_id: { type: ObjectId, ref: 'OauthApplication' },
     refreshToken: String,
     refreshTokenExpiresAt: Date,
     scope: String,
     user_id: { type: ObjectId, ref: 'User' },
+    createdAt: { type: Date },
     expiresAt: Date,
+    lastUsedAt: Date,
   },
   {
     collection: 'oauthAccessTokens',
