7f5be3ed043e921802084cd3fed798185d11e33c
Rebeka <rebeka.dekany@overleaf.com>
2023-06-30 18:20:26 +0200

    Make left menu sidebar a Modal
    GitOrigin-RevId: 63630c2a98c3abb5c5ebbde0afe25c0aa981282b

--- services/web/frontend/js/features/editor-left-menu/components/editor-left-menu.tsx
+++ services/web/frontend/js/features/editor-left-menu/components/editor-left-menu.tsx
@@ -2,26 +2,40 @@ import DownloadMenu from './download-menu'
 import ActionsMenu from './actions-menu'
 import HelpMenu from './help-menu'
 import { useLayoutContext } from '../../../shared/context/layout-context'
-import classNames from 'classnames'
 import SyncMenu from './sync-menu'
 import SettingsMenu from './settings-menu'
 import LeftMenuMask from './left-menu-mask'
+import AccessibleModal from '../../../shared/components/accessible-modal'
+import { Modal } from 'react-bootstrap'
+import classNames from 'classnames'
 
 export default function EditorLeftMenu() {
-  const { leftMenuShown } = useLayoutContext()
+  const { leftMenuShown, setLeftMenuShown } = useLayoutContext()
+
+  const closeModal = () => {
+    setLeftMenuShown(false)
+  }
 
   return (
     <>
-      <aside
-        id="left-menu"
-        className={classNames('full-size', { shown: leftMenuShown })}
+      <AccessibleModal
+        backdropClassName="left-menu-modal-backdrop"
+        keyboard
+        onHide={closeModal}
+        id="left-menu-modal"
+        show={leftMenuShown}
       >
-        <DownloadMenu />
-        <ActionsMenu />
-        <SyncMenu />
-        <SettingsMenu />
-        <HelpMenu />
-      </aside>
+        <Modal.Body
+          className={classNames('full-size', { shown: leftMenuShown })}
+          id="left-menu"
+        >
+          <DownloadMenu />
+          <ActionsMenu />
+          <SyncMenu />
+          <SettingsMenu />
+          <HelpMenu />
+        </Modal.Body>
+      </AccessibleModal>
       {leftMenuShown && <LeftMenuMask />}
     </>
   )
