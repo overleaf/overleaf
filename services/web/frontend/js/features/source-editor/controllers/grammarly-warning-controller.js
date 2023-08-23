96195177a3df7968b06de6bff76c5b85edcc2b6b
Alf Eaton <alf.eaton@overleaf.com>
2023-08-23 13:43:40 +0100

    Remove Grammarly warning (#14423)
    GitOrigin-RevId: 623ecffabdce9fc15dfb62361822afb75bd1cfa8

--- services/web/frontend/js/features/source-editor/controllers/grammarly-warning-controller.js
+++
@@ -1,9 +0,0 @@
-import App from '../../../base'
-import { react2angular } from 'react2angular'
-import { rootContext } from '../../../shared/context/root-context'
-import GrammarlyWarning from '../components/grammarly-warning'
-
-App.component(
-  'grammarlyWarning',
-  react2angular(rootContext.use(GrammarlyWarning), ['delay'])
-)
