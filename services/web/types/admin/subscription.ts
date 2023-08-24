9787edcc8ea7373e9a191ebc05af5433eb224578
Alexandre Bourdin <abourdin@users.noreply.github.com>
2023-08-24 12:29:29 +0200

    Merge pull request #14453 from overleaf/ab-admin-flag-managed
    [web] Easily identify managed users and groups in admin and Front
    
    GitOrigin-RevId: 9137a680f902a92adcc434aacd6c35726a2f5dbe

---
+++ services/web/types/admin/subscription.ts
@@ -0,0 +1,11 @@
+import { GroupPolicy } from '../subscription/dashboard/subscription'
+import { TeamInvite } from './team-invite'
+
+export type Subscription = {
+  _id: string
+  teamInvites: TeamInvite[]
+  groupPolicy: GroupPolicy
+  admin_id: string
+  groupPlan: boolean
+  customAccount: boolean
+}
---
+++ services/web/types/admin/user.ts
@@ -0,0 +1,4 @@
+export type User = {
+  _id: string
+  email: string
+}
