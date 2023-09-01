e320473d05cdc81ff6dbcbc8173deedf61cd667e
Alf Eaton <alf.eaton@overleaf.com>
2023-09-01 12:04:32 +0100

    Allow compile to be triggered with Ctrl/Cmd-s when Caps Lock is enabled (#14547)
    GitOrigin-RevId: f78bd433e6862a49f353e3f994c90024a06e2b3f

--- services/web/frontend/js/features/pdf-preview/hooks/use-compile-triggers.js
+++ services/web/frontend/js/features/pdf-preview/hooks/use-compile-triggers.js
@@ -12,9 +12,19 @@ export const startCompileKeypress = event => {
     if (event.key === 's' || event.key === 'Enter' || event.key === '.') {
       return true
     }
+
+    // Ctrl+s with Caps-Lock on
+    if (event.key === 'S' && !event.shiftKey) {
+      return true
+    }
   } else if (event.metaKey) {
     // Cmd+s / Cmd+Enter
     if (event.key === 's' || event.key === 'Enter') {
+      return true
+    }
+
+    // Cmd+s with Caps-Lock on
+    if (event.key === 'S' && !event.shiftKey) {
       return true
     }
   }
