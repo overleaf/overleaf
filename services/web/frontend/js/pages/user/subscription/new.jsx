70e824e41acacab8fbadb10b0d89cd636abbfd18
Thomas <thomas-@users.noreply.github.com>
2023-10-02 16:56:27 +0200

    Merge pull request #15065 from overleaf/tm-fix-jsx-new-subscription
    Fix jsx in non-jsx file
    
    GitOrigin-RevId: c66dbbfdb5d874dfd5096b204198c2f24304d784

--- services/web/frontend/js/pages/user/subscription/new.jsx
+++
@@ -1,8 +0,0 @@
-import './base'
-import ReactDOM from 'react-dom'
-import Root from '../../../features/subscription/components/new/root'
-
-const element = document.getElementById('subscription-new-root')
-if (element) {
-  ReactDOM.render(<Root />, element)
-}
