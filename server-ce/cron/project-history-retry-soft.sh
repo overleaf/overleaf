28ac76a2ecfabc0c36c66504cf8fb63a2b964741
Miguel Serrano <mserranom@users.noreply.github.com>
2023-10-06 12:48:45 +0200

    Merge pull request #15156 from overleaf/msm-hotfix-4-1-3
    [CE/SP] Hotfix 4.1.3 / 3.5.13
    
    GitOrigin-RevId: 82d8db6164a197a0427f7b9aedd0a440ab2f71b1

--- server-ce/cron/project-history-retry-soft.sh
+++ server-ce/cron/project-history-retry-soft.sh
@@ -8,4 +8,4 @@ echo "-----------------------------------"
 
 PROJECT_HISTORY_URL='http://localhost:3054'
 
-curl -X POST "${PROJECT_HISTORY_URL}/retry/failures?failureType=hard&timeout=3600000&limit=10000"
+curl -X POST "${PROJECT_HISTORY_URL}/retry/failures?failureType=soft&timeout=3600000&limit=10000"
