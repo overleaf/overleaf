0e52c245ce235babbc967c093682e6ebd491c86b
Jakob Ackermann <jakob.ackermann@overleaf.com>
2023-10-17 10:40:28 +0200

    Merge pull request #15269 from overleaf/jpa-delete-dead-history-code
    [web] delete unused "history-file-tree-react" angular component
    
    GitOrigin-RevId: a3e54352dd54bf234b3e868d91eaffafd078c0f8

--- services/web/frontend/js/features/history/controllers/history-file-tree-controller.js
+++
@@ -1,9 +0,0 @@
-import App from '../../../base'
-import { react2angular } from 'react2angular'
-import { rootContext } from '../../../shared/context/root-context'
-import HistoryFileTree from '../components/history-file-tree'
-
-App.component(
-  'historyFileTreeReact',
-  react2angular(rootContext.use(HistoryFileTree))
-)
