23a35aa0d275a3a8ee6b9393f1c774db5fb6d194
Alf Eaton <alf.eaton@overleaf.com>
2023-12-05 09:38:07 +0000

    Unselect multiple file tree items when selecting the root folder (#16081)
    GitOrigin-RevId: 567034249f18f7ef23be9f4aee3cd271d89701a5

--- services/web/frontend/js/features/file-tree/components/file-tree-inner.tsx
+++ services/web/frontend/js/features/file-tree/components/file-tree-inner.tsx
@@ -6,11 +6,15 @@ type FileTreeInnerProps = {
 }
 
 function FileTreeInner({ children }: FileTreeInnerProps) {
-  const { setIsRootFolderSelected } = useFileTreeSelectable()
+  const { setIsRootFolderSelected, selectedEntityIds, select } =
+    useFileTreeSelectable()
 
   const handleFileTreeClick = useCallback(() => {
     setIsRootFolderSelected(true)
-  }, [setIsRootFolderSelected])
+    if (selectedEntityIds.size > 1) {
+      select([])
+    }
+  }, [select, selectedEntityIds.size, setIsRootFolderSelected])
 
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
