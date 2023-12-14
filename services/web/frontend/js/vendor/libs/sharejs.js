1da44a1e7cb926e2ec9615a3233b209511ceb333
Alf Eaton <alf.eaton@overleaf.com>
2023-12-14 13:59:49 +0000

    Merge pull request #16184 from overleaf/ae-sharejs-esm
    Convert vendored `sharejs.js` from RequireJS to ESM
    
    GitOrigin-RevId: 38f010ac0a740667a633eb491e3be75eab8163ef

--- services/web/frontend/js/vendor/libs/sharejs.js
+++ services/web/frontend/js/vendor/libs/sharejs.js
@@ -1,3 +1,6 @@
+import CryptoJSSHA1 from 'crypto-js/sha1'
+import { debugging, debugConsole } from '@/utils/debugging'
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
@@ -26,7 +29,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-define(['crypto-js/sha1', '@/utils/debugging'], function (CryptoJSSHA1, { debugging, debugConsole }) {
+export const { Doc } = (() => {
   var append = void 0,
       bootstrapTransform = void 0,
       exports = void 0,
@@ -1411,5 +1414,7 @@ define(['crypto-js/sha1', '@/utils/debugging'], function (CryptoJSSHA1, { debugg
 
   exports.Doc = Doc;
 
-  return window.sharejs;
-});
+  return exports;
+})()
+
+export default window.sharejs
