f97e64ab52f244f0a3146ac0dec5f8866f60dadb
Jakob Ackermann <jakob.ackermann@overleaf.com>
2024-01-10 09:58:24 +0000

    Merge pull request #16433 from overleaf/jpa-migrate-audit-log-dev-env
    [web] run audit log migration in SaaS/dev-env
    
    GitOrigin-RevId: 9e0c5917102cef018ae6078a826a145a2181163f

--- services/web/migrations/20220913125500_migrate_auditLog_to_collections.js
+++ services/web/migrations/20220913125500_migrate_auditLog_to_collections.js
@@ -1,6 +1,6 @@
 const runScript = require('../scripts/migrate_audit_logs.js')
 
-exports.tags = ['server-ce', 'server-pro']
+exports.tags = ['server-ce', 'server-pro', 'saas']
 
 exports.migrate = async () => {
   const options = {
