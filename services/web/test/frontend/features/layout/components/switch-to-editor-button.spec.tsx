b2fbc3993069b1a0ef00dd49364857675218ffe1
Alf Eaton <alf.eaton@overleaf.com>
2024-03-25 11:55:06 +0000

    Improve tests for layout components (#17518)
    GitOrigin-RevId: 1d08224a5282482dcf153a2ebcff69a08be2eb58

---
+++ services/web/test/frontend/features/layout/components/switch-to-editor-button.spec.tsx
@@ -0,0 +1,38 @@
+import { EditorProviders } from '../../../helpers/editor-providers'
+import SwitchToEditorButton from '@/features/pdf-preview/components/switch-to-editor-button'
+
+describe('<SwitchToEditorButton />', function () {
+  it('shows button in full screen pdf layout', function () {
+    cy.mount(
+      <EditorProviders ui={{ view: 'pdf', pdfLayout: 'flat', chatOpen: false }}>
+        <SwitchToEditorButton />
+      </EditorProviders>
+    )
+
+    cy.findByRole('button', { name: 'Switch to editor' })
+  })
+
+  it('does not show button in split screen layout', function () {
+    cy.mount(
+      <EditorProviders
+        ui={{ view: 'pdf', pdfLayout: 'sideBySide', chatOpen: false }}
+      >
+        <SwitchToEditorButton />
+      </EditorProviders>
+    )
+
+    cy.findByRole('button', { name: 'Switch to editor' }).should('not.exist')
+  })
+
+  it('does not show button when detached', function () {
+    window.metaAttributesCache.set('ol-detachRole', 'detacher')
+
+    cy.mount(
+      <EditorProviders ui={{ view: 'pdf', pdfLayout: 'flat', chatOpen: false }}>
+        <SwitchToEditorButton />
+      </EditorProviders>
+    )
+
+    cy.findByRole('button', { name: 'Switch to editor' }).should('not.exist')
+  })
+})
---
+++ services/web/test/frontend/features/layout/components/switch-to-pdf-button.spec.tsx
@@ -0,0 +1,42 @@
+import SwitchToPDFButton from '@/features/source-editor/components/switch-to-pdf-button'
+import { EditorProviders } from '../../../helpers/editor-providers'
+
+describe('<SwitchToPDFButton />', function () {
+  it('shows button in full screen editor layout', function () {
+    cy.mount(
+      <EditorProviders
+        ui={{ view: 'editor', pdfLayout: 'flat', chatOpen: false }}
+      >
+        <SwitchToPDFButton />
+      </EditorProviders>
+    )
+
+    cy.findByRole('button', { name: 'Switch to PDF' })
+  })
+
+  it('does not show button in split screen layout', function () {
+    cy.mount(
+      <EditorProviders
+        ui={{ view: 'editor', pdfLayout: 'sideBySide', chatOpen: false }}
+      >
+        <SwitchToPDFButton />
+      </EditorProviders>
+    )
+
+    cy.findByRole('button', { name: 'Switch to PDF' }).should('not.exist')
+  })
+
+  it('does not show button when detached', function () {
+    window.metaAttributesCache.set('ol-detachRole', 'detacher')
+
+    cy.mount(
+      <EditorProviders
+        ui={{ view: 'editor', pdfLayout: 'flat', chatOpen: false }}
+      >
+        <SwitchToPDFButton />
+      </EditorProviders>
+    )
+
+    cy.findByRole('button', { name: 'Switch to PDF' }).should('not.exist')
+  })
+})
