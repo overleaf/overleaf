e8fe5abc8275dd8d2dbde10efb9ab6c59fcfc21d
Gernot Schulz <gernot.schulz@overleaf.com>
2024-05-17 09:52:09 +0200

    Merge pull request #18312 from overleaf/gs-git-bridge-bash
    Install bash in git-bridge Docker image
    
    GitOrigin-RevId: 2ed599c3c2a3a1c3e66845d4d17d6ac36d925d70

--- services/git-bridge/start.sh
+++ services/git-bridge/start.sh
@@ -4,11 +4,11 @@
 
 VERSION=$(date +%y%m%d%H%M%S)
 
-if [ "x$GIT_BRIDGE_JVM_ARGS" == "x" ]; then
+if [ -z "$GIT_BRIDGE_JVM_ARGS" ]; then
   GIT_BRIDGE_JVM_ARGS="-XX:+UseContainerSupport -XX:MaxRAMPercentage=50.0"
 fi
 
-if [ "$ENABLE_PROFILE_AGENT" == "true" ]; then
+if [ "$ENABLE_PROFILE_AGENT" = "true" ]; then
   GIT_BRIDGE_JVM_ARGS="-agentpath:/opt/cprof/profiler_java_agent.so=-cprof_service=git-bridge,-cprof_service_version=${VERSION},-cprof_enable_heap_sampling=true ${GIT_BRIDGE_JVM_ARGS}"
 fi
 
