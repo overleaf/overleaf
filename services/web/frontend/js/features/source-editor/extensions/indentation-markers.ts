17074473a7cfbea4a5e1ddb4b303f8326e237e63
Jimmy Domagala-Tang <Jimmy.Domagala-Tang@overleaf.com>
2024-05-30 12:17:23 -0400

    Merge pull request #18525 from overleaf/jdt-disable-indenttaion-safari
    [Web] Disable indentation markers on Safari
    
    GitOrigin-RevId: 7a62382e6d4a4ecf0bd144ab937f6e197d173bdb

--- services/web/frontend/js/features/source-editor/extensions/indentation-markers.ts
+++ services/web/frontend/js/features/source-editor/extensions/indentation-markers.ts
@@ -1,12 +1,17 @@
 import { Extension } from '@codemirror/state'
 import { indentationMarkers as markers } from '@replit/codemirror-indentation-markers'
 import { sourceOnly } from './visual/visual'
+import browser from './browser'
 
 /**
  * A third-party extension which adds markers to show the indentation level.
  * Configured to omit markers in the first column and to keep the same style for markers in the active block.
  */
-export const indentationMarkers = (visual: boolean): Extension =>
-  sourceOnly(visual, [
-    markers({ hideFirstIndent: true, highlightActiveBlock: false }),
-  ])
+export const indentationMarkers = (visual: boolean): Extension => {
+  // disable indentation markers in Safari due to flicker, ref to git issue: 18263
+  return browser.safari
+    ? []
+    : sourceOnly(visual, [
+        markers({ hideFirstIndent: true, highlightActiveBlock: false }),
+      ])
+}
