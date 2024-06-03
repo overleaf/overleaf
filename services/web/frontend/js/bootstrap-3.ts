9f1272d46d7ee3a1207c0ce80b5ac62aae3d07c2
Tim Down <158919+timdown@users.noreply.github.com>
2024-06-03 16:11:59 +0100

    Merge pull request #18536 from overleaf/td-bs5-remove-bookmarkable-tab
    Move bookmarkable tabs to Bootstrap 3 only
    
    GitOrigin-RevId: 456599e96f6b0ee3862004f74e4b19c5d4d94c93

--- services/web/frontend/js/bootstrap-3.ts
+++ services/web/frontend/js/bootstrap-3.ts
@@ -1,6 +1,7 @@
 import 'jquery'
 import 'bootstrap'
 import './features/contact-form'
+import './features/bookmarkable-tab'
 
 $('[data-ol-lang-selector-tooltip]').tooltip({ trigger: 'hover' })
 $('[data-toggle="tooltip"]').tooltip()
