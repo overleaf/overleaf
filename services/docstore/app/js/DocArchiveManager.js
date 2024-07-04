907195931dacd56e25e8840d824e131571fce278
Alf Eaton <alf.eaton@overleaf.com>
2024-07-04 17:25:58 +0100

    Fix BSON usage
    GitOrigin-RevId: ce495bca0a82c9bb973d5b755abdd22894840290

--- services/docstore/app/js/DocArchiveManager.js
+++ services/docstore/app/js/DocArchiveManager.js
@@ -8,9 +8,7 @@ const { ReadableString } = require('@overleaf/stream-utils')
 const RangeManager = require('./RangeManager')
 const PersistorManager = require('./PersistorManager')
 const pMap = require('p-map')
-
-const Bson = require('bson')
-const BSON = new Bson()
+const { BSON } = require('bson')
 
 const PARALLEL_JOBS = Settings.parallelArchiveJobs
 const UN_ARCHIVE_BATCH_SIZE = Settings.unArchiveBatchSize
