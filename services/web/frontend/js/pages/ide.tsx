8821a6d2f43203de925bab56137820e3d66d585b
Alf Eaton <alf.eaton@overleaf.com>
2024-07-29 13:30:32 +0100

    Work around Safari 15's incomplete support for dvh units (#19567)
    GitOrigin-RevId: ff6429d6a918413857dd8116611c9857958f68ed

--- services/web/frontend/js/pages/ide.tsx
+++ services/web/frontend/js/pages/ide.tsx
@@ -4,3 +4,19 @@ import ReactDOM from 'react-dom'
 import IdeRoot from '@/features/ide-react/components/ide-root'
 
 ReactDOM.render(<IdeRoot />, document.getElementById('ide-root'))
+
+// work around Safari 15's incomplete support for dvh units
+// https://github.com/overleaf/internal/issues/18109
+try {
+  if (
+    document.body.parentElement &&
+    document.body.parentElement?.clientHeight < document.body.clientHeight
+  ) {
+    const rootElement = document.querySelector<HTMLDivElement>('#ide-root')
+    if (rootElement) {
+      rootElement.style.height = '100vh'
+    }
+  }
+} catch {
+  // ignore errors
+}
