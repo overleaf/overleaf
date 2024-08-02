5cd5c1bffc5a3751e6a3dd70c9b4cd05b620cd32
andrew rumble <andrew.rumble@overleaf.com>
2024-08-02 17:31:02 +0100

    Use new ObjectId instead of ObjectId()
    GitOrigin-RevId: bfbf9f1d6b84a13f32fce127f01a49b1deaee6fe

--- services/web/test/acceptance/src/MongoHelper.js
+++ services/web/test/acceptance/src/MongoHelper.js
@@ -130,11 +130,15 @@ describe('MongoTests', function () {
         expectInQueryWithNativeObjectIds(query)
       })
       it('should transform all Mongoose ObjectIds to native ObjectIds', function () {
-        const query = normalizeMultiQuery(userIds.map(MongooseObjectId))
+        const query = normalizeMultiQuery(
+          userIds.map(userId => new NativeObjectId(userId))
+        )
         expectInQueryWithNativeObjectIds(query)
       })
       it('should leave all native Objects as native ObjectIds', function () {
-        const query = normalizeMultiQuery(userIds.map(NativeObjectId))
+        const query = normalizeMultiQuery(
+          userIds.map(userId => new NativeObjectId(userId))
+        )
         expectInQueryWithNativeObjectIds(query)
       })
 
@@ -143,11 +147,15 @@ describe('MongoTests', function () {
         await expectToFindTheThreeUsers(query)
       })
       it('should find the three users from Mongoose ObjectIds', async function () {
-        const query = normalizeMultiQuery(userIds.map(MongooseObjectId))
+        const query = normalizeMultiQuery(
+          userIds.map(userId => new NativeObjectId(userId))
+        )
         await expectToFindTheThreeUsers(query)
       })
       it('should find the three users from native ObjectIds', async function () {
-        const query = normalizeMultiQuery(userIds.map(NativeObjectId))
+        const query = normalizeMultiQuery(
+          userIds.map(userId => new NativeObjectId(userId))
+        )
         await expectToFindTheThreeUsers(query)
       })
     })
