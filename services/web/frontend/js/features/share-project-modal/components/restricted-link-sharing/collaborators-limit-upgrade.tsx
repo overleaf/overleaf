3e06c0086e0f351c71091a8b432468221ab423f4
Thomas <thomas-@users.noreply.github.com>
2024-08-26 15:01:14 +0200

    Merge pull request #20090 from overleaf/tm-link-sharing-enforcement-add-more-editors
    Update the notification shown when at max collaborators for users in the link-sharing-enforcement stage
    
    GitOrigin-RevId: 7699491b217e963bcbd919b3763d8c8d31b5361f

--- services/web/frontend/js/features/share-project-modal/components/restricted-link-sharing/collaborators-limit-upgrade.tsx
+++ services/web/frontend/js/features/share-project-modal/components/restricted-link-sharing/collaborators-limit-upgrade.tsx
@@ -5,47 +5,92 @@ import { upgradePlan } from '@/main/account-upgrade'
 import { useProjectContext } from '@/shared/context/project-context'
 import { useUserContext } from '@/shared/context/user-context'
 import StartFreeTrialButton from '@/shared/components/start-free-trial-button'
+import getMeta from '@/utils/meta'
 
 export default function CollaboratorsLimitUpgrade() {
   const { t } = useTranslation()
   const { features } = useProjectContext()
   const user = useUserContext()
+  const linkSharingEnforcement = getMeta('ol-linkSharingEnforcement')
 
   return (
     <div className="invite-warning">
-      <Notification
-        type="info"
-        customIcon={<div />}
-        title={t('upgrade_to_add_more_editors')}
-        content={
-          <p>
-            {t('you_can_only_add_n_people_to_edit_a_project', {
-              count: features.collaborators,
-            })}
-          </p>
-        }
-        action={
-          user.allowedFreeTrial ? (
-            <StartFreeTrialButton
-              buttonProps={{ variant: 'secondary', size: 'small' }}
-              source="project-sharing"
-              variant="limit"
-            >
-              {t('upgrade')}
-            </StartFreeTrialButton>
-          ) : (
-            <Button
-              bsSize="sm"
-              className="btn-secondary"
-              onClick={() => {
-                upgradePlan('project-sharing')
-              }}
-            >
-              {t('upgrade')}
-            </Button>
-          )
-        }
-      />
+      {linkSharingEnforcement ? (
+        <Notification
+          type="info"
+          customIcon={
+            <img
+              src="/img/share-modal/add-more-editors.svg"
+              alt=""
+              aria-hidden="true"
+            />
+          }
+          title={t('add_more_editors')}
+          content={
+            <p>
+              {t(
+                'upgrade_to_add_more_editors_and_access_collaboration_features'
+              )}
+            </p>
+          }
+          isActionBelowContent
+          action={
+            user.allowedFreeTrial ? (
+              <StartFreeTrialButton
+                buttonProps={{ variant: 'premium', size: 'default' }}
+                source="project-sharing"
+                variant="limit"
+              >
+                {t('upgrade')}
+              </StartFreeTrialButton>
+            ) : (
+              <Button
+                bsSize="medium"
+                className="btn-premium"
+                onClick={() => {
+                  upgradePlan('project-sharing')
+                }}
+              >
+                {t('upgrade')}
+              </Button>
+            )
+          }
+        />
+      ) : (
+        <Notification
+          type="info"
+          customIcon={<div />}
+          title={t('upgrade_to_add_more_editors')}
+          content={
+            <p>
+              {t('you_can_only_add_n_people_to_edit_a_project', {
+                count: features.collaborators,
+              })}
+            </p>
+          }
+          action={
+            user.allowedFreeTrial ? (
+              <StartFreeTrialButton
+                buttonProps={{ variant: 'secondary', size: 'small' }}
+                source="project-sharing"
+                variant="limit"
+              >
+                {t('upgrade')}
+              </StartFreeTrialButton>
+            ) : (
+              <Button
+                bsSize="sm"
+                className="btn-secondary"
+                onClick={() => {
+                  upgradePlan('project-sharing')
+                }}
+              >
+                {t('upgrade')}
+              </Button>
+            )
+          }
+        />
+      )}
     </div>
   )
 }
