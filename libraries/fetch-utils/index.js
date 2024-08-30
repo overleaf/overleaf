496a63cf23c2bc0ef2e5612adc926a0b9bd47a42
Mathias Jakobsen <mathias.jakobsen@overleaf.com>
2024-08-30 09:11:46 +0100

    Merge pull request #20058 from overleaf/mj-promisify-tpr
    [third-party-references] Promisify third-party-references
    
    GitOrigin-RevId: cfc462abaca95ed10798b3d1756950f2997a5947

--- libraries/fetch-utils/index.js
+++ libraries/fetch-utils/index.js
@@ -21,7 +21,7 @@ async function fetchJson(url, opts = {}) {
 async function fetchJsonWithResponse(url, opts = {}) {
   const { fetchOpts } = parseOpts(opts)
   fetchOpts.headers = fetchOpts.headers ?? {}
-  fetchOpts.headers.Accept = 'application/json'
+  fetchOpts.headers.Accept = fetchOpts.headers.Accept ?? 'application/json'
 
   const response = await performRequest(url, fetchOpts)
   if (!response.ok) {
