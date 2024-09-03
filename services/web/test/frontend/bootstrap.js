4f37ef0b1a3a325e205a815289f53ce6c8923c6b
Eric Mc Sween <5454374+emcsween@users.noreply.github.com>
2024-09-03 08:22:06 -0400

    Merge pull request #20189 from overleaf/em-github-file-rejections
    Show a detailed error message when GitHub rejects oversized files
    
    GitOrigin-RevId: 6ae37c228f232a00f4d872623cb6e85a3b65f258

--- services/web/test/frontend/bootstrap.js
+++ services/web/test/frontend/bootstrap.js
@@ -85,9 +85,9 @@ globalThis.fetch =
   window.fetch =
     (url, ...options) => fetch(new URL(url, 'http://127.0.0.1'), ...options)
 
-// ignore CSS files
+// ignore CSS/LESS files
 const { addHook } = require('pirates')
-addHook(() => '', { exts: ['.css'], ignoreNodeModules: false })
+addHook(() => '', { exts: ['.css', '.less'], ignoreNodeModules: false })
 
 globalThis.HTMLElement.prototype.scrollIntoView = () => {}
 
