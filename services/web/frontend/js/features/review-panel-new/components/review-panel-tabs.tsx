7002d1e4bfa5cfc5d6956adfad530239a35c8eb5
David <33458145+davidmcpowell@users.noreply.github.com>
2024-09-24 09:54:32 +0100

    Merge pull request #20511 from overleaf/dp-review-panel-footer
    Update UI for new review panel footer
    
    GitOrigin-RevId: 8beb554f78fa5876d5349981d96b44a16ab5567c

--- services/web/frontend/js/features/review-panel-new/components/review-panel-tabs.tsx
+++ services/web/frontend/js/features/review-panel-new/components/review-panel-tabs.tsx
@@ -1,28 +1,34 @@
 import { Dispatch, FC, memo, SetStateAction } from 'react'
 import classnames from 'classnames'
 import { SubView } from '../components/review-panel'
+import MaterialIcon from '@/shared/components/material-icon'
+import { useTranslation } from 'react-i18next'
 
 const ReviewPanelTabs: FC<{
   subView: SubView
   setSubView: Dispatch<SetStateAction<SubView>>
 }> = ({ subView, setSubView }) => {
+  const { t } = useTranslation()
+
   return (
     <>
       <button
-        className={classnames('rp-nav-item', {
-          'rp-nav-item-active': subView === 'cur_file',
+        className={classnames('review-panel-tab', {
+          'review-panel-tab-active': subView === 'cur_file',
         })}
         onClick={() => setSubView('cur_file')}
       >
-        Current file
+        <MaterialIcon type="description" />
+        {t('current_file')}
       </button>
       <button
-        className={classnames('rp-nav-item', {
-          'rp-nav-item-active': subView === 'overview',
+        className={classnames('review-panel-tab', {
+          'review-panel-tab-active': subView === 'overview',
         })}
         onClick={() => setSubView('overview')}
       >
-        Overview
+        <MaterialIcon type="list" />
+        {t('overview')}
       </button>
     </>
   )
