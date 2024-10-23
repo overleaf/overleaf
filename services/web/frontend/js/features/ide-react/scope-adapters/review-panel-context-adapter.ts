c4b23b4d3936058f30c34cb5271e8abb57152c17
Domagoj Kriskovic <dom.kriskovic@overleaf.com>
2024-10-23 10:38:16 +0200

    Add "usingNewReviewPanel" flag in store (#21279)
    * Revert "Add isUsingNewReviewPanel flag for writefull (#21111)"
    
    This reverts commit 3491e93ca70fa1db0c751872e233dfebdbe81423.
    
    * add usingNewReviewPanel in store
    
    * fix lint error
    
    GitOrigin-RevId: 56d90a428a2cc45708b8b483dc60a62aae233a85

--- services/web/frontend/js/features/ide-react/scope-adapters/review-panel-context-adapter.ts
+++ services/web/frontend/js/features/ide-react/scope-adapters/review-panel-context-adapter.ts
@@ -1,7 +1,9 @@
 import { ReactScopeValueStore } from '@/features/ide-react/scope-value-store/react-scope-value-store'
+import { isSplitTestEnabled } from '@/utils/splitTestUtils'
 
 export default function populateReviewPanelScope(store: ReactScopeValueStore) {
   store.set('loadingThreads', true)
   store.set('users', {})
   store.set('addNewComment', () => {})
+  store.set('usingNewReviewPanel', isSplitTestEnabled('review-panel-redesign'))
 }
