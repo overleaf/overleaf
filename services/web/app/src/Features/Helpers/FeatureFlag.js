6c83f85d5d99a5a6488259281812437b48179567
M Fahru <m.fahru@overleaf.com>
2024-10-24 11:44:19 -0700

    Merge pull request #21232 from overleaf/mf-remove-use-advanced-search
    [web] Remove advance gallery search
    
    GitOrigin-RevId: af39f63ac0483fe2540ba737f0239d0b503f87fe

--- services/web/app/src/Features/Helpers/FeatureFlag.js
+++
@@ -1,9 +0,0 @@
-function shouldDisplayFeature(req, name, variantFlag) {
-  if (req.query && req.query[name]) {
-    return req.query[name] === 'true'
-  } else {
-    return variantFlag === true
-  }
-}
-
-module.exports = { shouldDisplayFeature }
