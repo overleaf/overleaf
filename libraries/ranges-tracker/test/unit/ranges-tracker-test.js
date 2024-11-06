d95981cb0c9155832c51446c471496040ef84762
Eric Mc Sween <5454374+emcsween@users.noreply.github.com>
2024-11-06 09:33:49 -0500

    Merge pull request #21682 from overleaf/em-revert-ranges-tracker-changes
    Revert RangesTracker fix for multiple tracked deletes at same position
    
    GitOrigin-RevId: ee6ade406a86319f5551863e18ed40d508128ecb

--- libraries/ranges-tracker/test/unit/ranges-tracker-test.js
+++ libraries/ranges-tracker/test/unit/ranges-tracker-test.js
@@ -4,7 +4,6 @@ const RangesTracker = require('../..')
 describe('RangesTracker', function () {
   describe('with duplicate change ids', function () {
     beforeEach(function () {
-      this.comments = []
       this.changes = [
         { id: 'id1', op: { p: 1, i: 'hello' } },
         { id: 'id2', op: { p: 10, i: 'world' } },
@@ -25,47 +24,6 @@ describe('RangesTracker', function () {
     it('removeChangeIds() removes all changes with the given ids', function () {
       this.rangesTracker.removeChangeIds(['id1', 'id2'])
       expect(this.rangesTracker.changes).to.deep.equal([this.changes[2]])
-    })
-  })
-
-  describe('with multiple tracked deletes at the same position', function () {
-    beforeEach(function () {
-      this.comments = []
-      this.changes = [
-        { id: 'id1', op: { p: 33, d: 'before' } },
-        { id: 'id2', op: { p: 50, d: 'right before' } },
-        { id: 'id3', op: { p: 50, d: 'this one' } },
-        { id: 'id4', op: { p: 50, d: 'right after' } },
-        { id: 'id5', op: { p: 75, d: 'long after' } },
-      ]
-      this.rangesTracker = new RangesTracker(this.changes, this.comments)
-    })
-
-    it('preserves the text order when rejecting changes', function () {
-      this.rangesTracker.applyOp(
-        { p: 50, i: 'this one', u: true },
-        { user_id: 'user-id' }
-      )
-      expect(this.rangesTracker.changes).to.deep.equal([
-        { id: 'id1', op: { p: 33, d: 'before' } },
-        { id: 'id2', op: { p: 50, d: 'right before' } },
-        { id: 'id4', op: { p: 58, d: 'right after' } },
-        { id: 'id5', op: { p: 83, d: 'long after' } },
-      ])
-    })
-
-    it('moves all tracked deletes after the insert if not rejecting changes', function () {
-      this.rangesTracker.applyOp(
-        { p: 50, i: 'some other text', u: true },
-        { user_id: 'user-id' }
-      )
-      expect(this.rangesTracker.changes).to.deep.equal([
-        { id: 'id1', op: { p: 33, d: 'before' } },
-        { id: 'id2', op: { p: 65, d: 'right before' } },
-        { id: 'id3', op: { p: 65, d: 'this one' } },
-        { id: 'id4', op: { p: 65, d: 'right after' } },
-        { id: 'id5', op: { p: 90, d: 'long after' } },
-      ])
     })
   })
 })
