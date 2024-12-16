bb8bdd04afa067c39f397c1e4ae03f297a8d4080
Antoine Clausse <antoine.clausse@overleaf.com>
2024-12-16 10:30:39 +0100

    [web] Use icons from `@phosphor-icons/react` for the project-list (DS-Nav) (#22517)
    * Use icons from `@phosphor-icons/react`
    
    https://phosphoricons.com/
    
    * Add the phosphor Plus icon to the tag list
    
    * Add SignOut icon
    
    * Use Phosphor icon for tags
    
    * Fix icon color when active
    
    * Add offset to dropdown menus so they're positioned like in Figma
    
    ---------
    
    Co-authored-by: Tim Down <158919+timdown@users.noreply.github.com>
    GitOrigin-RevId: 039e98063d775d0348d227202d5ecd6a0bfb7b65

--- services/web/frontend/js/features/project-list/components/sidebar/sidebar-ds-nav.tsx
+++ services/web/frontend/js/features/project-list/components/sidebar/sidebar-ds-nav.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import classnames from 'classnames'
+import { Question, User } from '@phosphor-icons/react'
 import NewProjectButton from '../new-project-button'
 import SidebarFilters from './sidebar-filters'
 import AddAffiliation, { useAddAffiliation } from '../add-affiliation'
 import SurveyWidget from '../survey-widget'
-import { usePersistedResize } from '../../../../shared/hooks/use-resize'
-import MaterialIcon from '@/shared/components/material-icon'
+import { usePersistedResize } from '@/shared/hooks/use-resize'
 import { Dropdown } from 'react-bootstrap-5'
 import getMeta from '@/utils/meta'
 import OLTooltip from '@/features/ui/components/ol/ol-tooltip'
@@ -77,11 +77,18 @@ function SidebarDsNav() {
                   }}
                 >
                   <div>
-                    <MaterialIcon type="help" size="2x" />
+                    <Question size={24} />
                   </div>
                 </OLTooltip>
               </Dropdown.Toggle>
-              <Dropdown.Menu as="ul" role="menu" align="end">
+              <Dropdown.Menu
+                as="ul"
+                role="menu"
+                align="end"
+                popperConfig={{
+                  modifiers: [{ name: 'offset', options: { offset: [0, 5] } }],
+                }}
+              >
                 <NavDropdownMenuItems
                   dropdown={helpItem.dropdown}
                   showContactUsModal={showContactUsModal}
@@ -105,11 +112,20 @@ function SidebarDsNav() {
                     }}
                   >
                     <div>
-                      <MaterialIcon type="person" size="2x" />
+                      <User size={24} />
                     </div>
                   </OLTooltip>
                 </Dropdown.Toggle>
-                <Dropdown.Menu as="ul" role="menu" align="end">
+                <Dropdown.Menu
+                  as="ul"
+                  role="menu"
+                  align="end"
+                  popperConfig={{
+                    modifiers: [
+                      { name: 'offset', options: { offset: [-50, 5] } },
+                    ],
+                  }}
+                >
                   <AccountMenuItems
                     sessionUser={sessionUser}
                     showSubscriptionLink={showSubscriptionLink}
