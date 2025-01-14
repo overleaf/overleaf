292308d546d20473958d061dddcf3d0369fd278b
M Fahru <m.fahru@overleaf.com>
2025-01-14 08:43:59 -0700

    Merge pull request #22540 from overleaf/mf-remove-plans-page-default-variant-code
    [web] Remove dead backend (include pug template) code after `website-redesign-plans` test has been concluded and `new-design` variant is fully activated
    
    GitOrigin-RevId: 143129da71d43b88535f971b13e9e72c98bad798

--- services/web/app/src/Features/Subscription/SubscriptionController.js
+++ services/web/app/src/Features/Subscription/SubscriptionController.js
@@ -9,8 +9,6 @@ const Settings = require('@overleaf/settings')
 const logger = require('@overleaf/logger')
 const GeoIpLookup = require('../../infrastructure/GeoIpLookup')
 const FeaturesUpdater = require('./FeaturesUpdater')
-const plansConfig = require('./plansConfig')
-const interstitialPaymentConfig = require('./interstitialPaymentConfig')
 const GroupPlansData = require('./GroupPlansData')
 const V1SubscriptionManager = require('./V1SubscriptionManager')
 const AnalyticsManager = require('../Analytics/AnalyticsManager')
@@ -19,13 +17,10 @@ const { expressify } = require('@overleaf/promise-utils')
 const OError = require('@overleaf/o-error')
 const { DuplicateAddOnError, AddOnNotPresentError } = require('./Errors')
 const SplitTestHandler = require('../SplitTests/SplitTestHandler')
-const SubscriptionHelper = require('./SubscriptionHelper')
 const AuthorizationManager = require('../Authorization/AuthorizationManager')
 const Modules = require('../../infrastructure/Modules')
 const async = require('async')
-const { formatCurrency } = require('../../util/currency')
 const HttpErrorHandler = require('../Errors/HttpErrorHandler')
-const { URLSearchParams } = require('url')
 const RecurlyClient = require('./RecurlyClient')
 const { AI_ADD_ON_CODE } = require('./RecurlyEntities')
 const PlansLocator = require('./PlansLocator')
@@ -38,154 +33,6 @@ const PlansLocator = require('./PlansLocator')
  */
 
 const groupPlanModalOptions = Settings.groupPlanModalOptions
-const validGroupPlanModalOptions = {
-  plan_code: groupPlanModalOptions.plan_codes.map(item => item.code),
-  currency: groupPlanModalOptions.currencies.map(item => item.code),
-  size: groupPlanModalOptions.sizes,
-  usage: groupPlanModalOptions.usages.map(item => item.code),
-}
-
-function _getGroupPlanModalDefaults(req, currency) {
-  function getDefault(param, category, defaultValue) {
-    const v = req.query && req.query[param]
-    if (v && validGroupPlanModalOptions[category].includes(v)) {
-      return v
-    }
-    return defaultValue
-  }
-
-  let defaultGroupPlanModalCurrency = 'USD'
-  if (validGroupPlanModalOptions.currency.includes(currency)) {
-    defaultGroupPlanModalCurrency = currency
-  }
-
-  return {
-    plan_code: getDefault('plan', 'plan_code', 'collaborator'),
-    size: getDefault('number', 'size', '2'),
-    currency: getDefault('currency', 'currency', defaultGroupPlanModalCurrency),
-    usage: getDefault('usage', 'usage', 'enterprise'),
-  }
-}
-
-function _plansBanners(countryCode) {
-  const showLATAMBanner = ['MX', 'CO', 'CL', 'PE'].includes(countryCode)
-  const showInrGeoBanner = countryCode === 'IN'
-  const showBrlGeoBanner = countryCode === 'BR'
-  return { showLATAMBanner, showInrGeoBanner, showBrlGeoBanner }
-}
-
-async function plansPage(req, res) {
-  const websiteRedesignPlansAssignment =
-    await SplitTestHandler.promises.getAssignment(
-      req,
-      res,
-      'website-redesign-plans'
-    )
-  if (websiteRedesignPlansAssignment.variant !== 'default') {
-    const queryParamString = new URLSearchParams(req.query)?.toString()
-    const queryParamForRedirect = queryParamString ? '?' + queryParamString : ''
-
-    if (websiteRedesignPlansAssignment.variant === 'new-design') {
-      return res.redirect(
-        302,
-        '/user/subscription/plans-2' + queryParamForRedirect
-      )
-    } else if (websiteRedesignPlansAssignment.variant === 'light-design') {
-      return res.redirect(
-        302,
-        '/user/subscription/plans-3' + queryParamForRedirect
-      )
-    }
-  }
-
-  const language = req.i18n.language || 'en'
-
-  const plans = SubscriptionViewModelBuilder.buildPlansList()
-
-  const { currency, countryCode } = await _getRecommendedCurrency(req, res)
-
-  const latamCountryBannerDetails = await getLatamCountryBannerDetails(req, res)
-  const groupPlanModalDefaults = _getGroupPlanModalDefaults(req, currency)
-
-  const currentView = 'annual'
-
-  const { showLATAMBanner, showInrGeoBanner, showBrlGeoBanner } =
-    _plansBanners(countryCode)
-
-  const shouldLoadHotjar = await getShouldLoadHotjar(req, res)
-
-  res.render('subscriptions/plans', {
-    title: 'plans_and_pricing',
-    currentView,
-    plans,
-    itm_content: req.query?.itm_content,
-    itm_referrer: req.query?.itm_referrer,
-    itm_campaign: 'plans',
-    language,
-    formatCurrency,
-    recommendedCurrency: currency,
-    plansConfig,
-    groupPlans: GroupPlansData,
-    groupPlanModalOptions,
-    groupPlanModalDefaults,
-    initialLocalizedGroupPrice:
-      SubscriptionHelper.generateInitialLocalizedGroupPrice(
-        currency ?? 'USD',
-        language
-      ),
-    showInrGeoBanner,
-    showBrlGeoBanner,
-    showLATAMBanner,
-    latamCountryBannerDetails,
-    countryCode,
-    websiteRedesignPlansVariant: 'default',
-    shouldLoadHotjar,
-  })
-}
-
-async function plansPageLightDesign(req, res) {
-  const { currency, countryCode } = await _getRecommendedCurrency(req, res)
-
-  const language = req.i18n.language || 'en'
-  const currentView = 'annual'
-  const plans = SubscriptionViewModelBuilder.buildPlansList()
-  const groupPlanModalDefaults = _getGroupPlanModalDefaults(req, currency)
-
-  const { showLATAMBanner, showInrGeoBanner, showBrlGeoBanner } =
-    _plansBanners(countryCode)
-
-  const latamCountryBannerDetails = await getLatamCountryBannerDetails(req, res)
-
-  const shouldLoadHotjar = await getShouldLoadHotjar(req, res)
-
-  res.render('subscriptions/plans-light-design', {
-    title: 'plans_and_pricing',
-    currentView,
-    plans,
-    itm_content: req.query?.itm_content,
-    itm_referrer: req.query?.itm_referrer,
-    itm_campaign: 'plans',
-    language,
-    formatCurrency,
-    recommendedCurrency: currency,
-    plansConfig,
-    groupPlans: GroupPlansData,
-    groupPlanModalOptions,
-    groupPlanModalDefaults,
-    initialLocalizedGroupPrice:
-      SubscriptionHelper.generateInitialLocalizedGroupPrice(
-        currency ?? 'USD',
-        language
-      ),
-    showLATAMBanner,
-    showInrGeoBanner,
-    showBrlGeoBanner,
-    latamCountryBannerDetails,
-    countryCode,
-    websiteRedesignPlansVariant: 'light-design',
-    shouldLoadHotjar,
-  })
-}
 
 function formatGroupPlansDataForDash() {
   return {
@@ -293,68 +140,6 @@ async function userSubscriptionPage(req, res) {
     userRestrictions: Array.from(req.userRestrictions || []),
   }
   res.render('subscriptions/dashboard-react', data)
-}
-
-async function interstitialPaymentPage(req, res) {
-  const websiteRedesignPlansAssignment =
-    await SplitTestHandler.promises.getAssignment(
-      req,
-      res,
-      'website-redesign-plans'
-    )
-
-  let template = 'subscriptions/interstitial-payment'
-
-  if (websiteRedesignPlansAssignment.variant === 'new-design') {
-    return await Modules.promises.hooks.fire(
-      'interstitialPaymentPageNewDesign',
-      req,
-      res
-    )
-  } else if (websiteRedesignPlansAssignment.variant === 'light-design') {
-    template = 'subscriptions/interstitial-payment-light-design'
-  }
-
-  const user = SessionManager.getSessionUser(req.session)
-  const { recommendedCurrency, countryCode } = await _getRecommendedCurrency(
-    req,
-    res
-  )
-
-  const latamCountryBannerDetails = await getLatamCountryBannerDetails(req, res)
-
-  const { hasSubscription } =
-    await LimitationsManager.promises.userHasSubscription(user)
-  const showSkipLink = req.query?.skipLink === 'true'
-
-  if (hasSubscription) {
-    res.redirect('/user/subscription?hasSubscription=true')
-  } else {
-    const { showLATAMBanner, showInrGeoBanner, showBrlGeoBanner } =
-      _plansBanners(countryCode)
-
-    const shouldLoadHotjar = await getShouldLoadHotjar(req, res)
-
-    res.render(template, {
-      title: 'subscribe',
-      itm_content: req.query?.itm_content,
-      itm_campaign: req.query?.itm_campaign,
-      itm_referrer: req.query?.itm_referrer,
-      recommendedCurrency,
-      interstitialPaymentConfig,
-      showSkipLink,
-      formatCurrency,
-      showCurrencyAndPaymentMethods: true, // TODO: remove hardcode
-      showInrGeoBanner,
-      showBrlGeoBanner,
-      showLATAMBanner,
-      latamCountryBannerDetails,
-      skipLinkTarget: req.session?.postCheckoutRedirect || '/project',
-      websiteRedesignPlansVariant: websiteRedesignPlansAssignment.variant,
-      countryCode,
-      shouldLoadHotjar,
-    })
-  }
 }
 
 async function successfulSubscription(req, res) {
@@ -828,15 +613,6 @@ async function getLatamCountryBannerDetails(req, res) {
   return latamCountryBannerDetails
 }
 
-async function getShouldLoadHotjar(req, res) {
-  const assignment = await SplitTestHandler.promises.getAssignment(
-    req,
-    res,
-    'hotjar-plans'
-  )
-  return assignment?.variant === 'enabled'
-}
-
 /**
  * Build a subscription change preview for display purposes
  *
@@ -886,10 +662,7 @@ function makeChangePreview(
 }
 
 module.exports = {
-  plansPage: expressify(plansPage),
-  plansPageLightDesign: expressify(plansPageLightDesign),
   userSubscriptionPage: expressify(userSubscriptionPage),
-  interstitialPaymentPage: expressify(interstitialPaymentPage),
   successfulSubscription: expressify(successfulSubscription),
   cancelSubscription,
   canceledSubscription: expressify(canceledSubscription),
@@ -904,7 +677,6 @@ module.exports = {
   recurlyNotificationParser,
   refreshUserFeatures: expressify(refreshUserFeatures),
   redirectToHostedPage: expressify(redirectToHostedPage),
-  plansBanners: _plansBanners,
   previewAddonPurchase: expressify(previewAddonPurchase),
   purchaseAddon,
   removeAddon,
