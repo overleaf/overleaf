68dcacea93a28471302c819460870e7c22d5d9cd
Miguel Serrano <mserranom@users.noreply.github.com>
2025-01-15 13:56:56 +0100

    Merge pull request #22890 from overleaf/msm-fix-migrations
    Fix module import in migrations
    
    GitOrigin-RevId: 462eb54c68942118f3f76b166c3f27d923227e6c

--- services/web/migrations/20230817081910_back_fill_gitBridge_feature_server_pro.mjs
+++ services/web/migrations/20230817081910_back_fill_gitBridge_feature_server_pro.mjs
@@ -1,9 +1,10 @@
-import runScript from '../modules/server-ce-scripts/scripts/upgrade-user-features.mjs'
-
 const tags = ['server-ce', 'server-pro']
 
 const migrate = async () => {
   // Run-time import as SaaS does not ship with the server-ce-scripts module
+  const { default: runScript } = await import(
+    '../modules/server-ce-scripts/scripts/upgrade-user-features.mjs'
+  )
   await runScript(false, {
     gitBridge: 1,
   })
