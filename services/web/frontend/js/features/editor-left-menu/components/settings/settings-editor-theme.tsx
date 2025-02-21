816410f2d4e4c10cd5e4826287083951366a644a
Alf Eaton <alf.eaton@overleaf.com>
2025-02-21 13:22:06 +0000

    Remove aria-hidden from theme menu divider option (#23704)
    GitOrigin-RevId: c6df23ad5869aaff41beb767ecd5d1d86910a267

--- services/web/frontend/js/features/editor-left-menu/components/settings/settings-editor-theme.tsx
+++ services/web/frontend/js/features/editor-left-menu/components/settings/settings-editor-theme.tsx
@@ -21,7 +21,6 @@ export default function SettingsEditorTheme() {
     const dividerOption: Option = {
       value: '-',
       label: '—————————————————',
-      ariaHidden: 'true',
       disabled: true,
     }
 
