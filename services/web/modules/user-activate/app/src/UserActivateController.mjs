1f339b37bd2340b206858281f6c9ccae1cdf3a9a
Tim Down <158919+timdown@users.noreply.github.com>
2025-03-04 09:21:46 +0000

    Merge pull request #23763 from overleaf/td-bs5-user-activation
    Migrate user activation page to BS5
    
    GitOrigin-RevId: 3f4c9173cf480aafc5d874bfe81bbc8503a2e5be

--- services/web/modules/user-activate/app/src/UserActivateController.mjs
+++ services/web/modules/user-activate/app/src/UserActivateController.mjs
@@ -3,68 +3,67 @@ import { fileURLToPath } from 'node:url'
 import UserGetter from '../../../../app/src/Features/User/UserGetter.js'
 import UserRegistrationHandler from '../../../../app/src/Features/User/UserRegistrationHandler.js'
 import ErrorController from '../../../../app/src/Features/Errors/ErrorController.js'
+import { expressify } from '@overleaf/promise-utils'
 
 const __dirname = Path.dirname(fileURLToPath(import.meta.url))
 
+function registerNewUser(req, res, next) {
+  res.render(Path.resolve(__dirname, '../views/user/register'))
+}
+
+async function register(req, res, next) {
+  const { email } = req.body
+  if (email == null || email === '') {
+    return res.sendStatus(422) // Unprocessable Entity
+  }
+  const { user, setNewPasswordUrl } =
+    await UserRegistrationHandler.promises.registerNewUserAndSendActivationEmail(
+      email
+    )
+  res.json({
+    email: user.email,
+    setNewPasswordUrl,
+  })
+}
+
+async function activateAccountPage(req, res, next) {
+  // An 'activation' is actually just a password reset on an account that
+  // was set with a random password originally.
+  if (req.query.user_id == null || req.query.token == null) {
+    return ErrorController.notFound(req, res)
+  }
+
+  if (typeof req.query.user_id !== 'string') {
+    return ErrorController.forbidden(req, res)
+  }
+
+  const user = await UserGetter.promises.getUser(req.query.user_id, {
+    email: 1,
+    loginCount: 1,
+  })
+
+  if (!user) {
+    return ErrorController.notFound(req, res)
+  }
+
+  if (user.loginCount > 0) {
+    // Already seen this user, so account must be activated.
+    // This lets users keep clicking the 'activate' link in their email
+    // as a way to log in which, if I know our users, they will.
+    return res.redirect(`/login`)
+  }
+
+  req.session.doLoginAfterPasswordReset = true
+
+  res.render(Path.resolve(__dirname, '../views/user/activate'), {
+    title: 'activate_account',
+    email: user.email,
+    token: req.query.token,
+  })
+}
+
 export default {
-  registerNewUser(req, res, next) {
-    res.render(Path.resolve(__dirname, '../views/user/register'))
-  },
-
-  register(req, res, next) {
-    const { email } = req.body
-    if (email == null || email === '') {
-      return res.sendStatus(422) // Unprocessable Entity
-    }
-    UserRegistrationHandler.registerNewUserAndSendActivationEmail(
-      email,
-      (error, user, setNewPasswordUrl) => {
-        if (error != null) {
-          return next(error)
-        }
-        res.json({
-          email: user.email,
-          setNewPasswordUrl,
-        })
-      }
-    )
-  },
-
-  activateAccountPage(req, res, next) {
-    // An 'activation' is actually just a password reset on an account that
-    // was set with a random password originally.
-    if (req.query.user_id == null || req.query.token == null) {
-      return ErrorController.notFound(req, res)
-    }
-
-    if (typeof req.query.user_id !== 'string') {
-      return ErrorController.forbidden(req, res)
-    }
-
-    UserGetter.getUser(
-      req.query.user_id,
-      { email: 1, loginCount: 1 },
-      (error, user) => {
-        if (error != null) {
-          return next(error)
-        }
-        if (!user) {
-          return ErrorController.notFound(req, res)
-        }
-        if (user.loginCount > 0) {
-          // Already seen this user, so account must be activate
-          // This lets users keep clicking the 'activate' link in their email
-          // as a way to log in which, if I know our users, they will.
-          res.redirect(`/login`)
-        } else {
-          req.session.doLoginAfterPasswordReset = true
-          res.render(Path.resolve(__dirname, '../views/user/activate'), {
-            title: 'activate_account',
-            email: user.email,
-            token: req.query.token,
-          })
-        }
-      }
-    )
-  },
+  registerNewUser,
+  register: expressify(register),
+  activateAccountPage: expressify(activateAccountPage),
 }
--- services/web/modules/user-activate/test/unit/src/UserActivateControllerTests.mjs
+++ services/web/modules/user-activate/test/unit/src/UserActivateControllerTests.mjs
@@ -17,15 +17,26 @@ describe('UserActivateController', function () {
       email: 'joe@example.com',
     }
 
-    this.UserGetter = { getUser: sinon.stub() }
-    this.UserRegistrationHandler = {}
+    this.UserGetter = {
+      promises: {
+        getUser: sinon.stub(),
+      },
+    }
+    this.UserRegistrationHandler = { promises: {} }
     this.ErrorController = { notFound: sinon.stub() }
+    this.SplitTestHandler = {
+      promises: {
+        getAssignment: sinon.stub().resolves({ variant: 'default' }),
+      },
+    }
     this.UserActivateController = await esmock(MODULE_PATH, {
       '../../../../../app/src/Features/User/UserGetter.js': this.UserGetter,
       '../../../../../app/src/Features/User/UserRegistrationHandler.js':
         this.UserRegistrationHandler,
       '../../../../../app/src/Features/Errors/ErrorController.js':
         this.ErrorController,
+      '../../../../../app/src/Features/SplitTests/SplitTestHandler':
+        this.SplitTestHandler,
     })
     this.req = {
       body: {},
@@ -41,12 +52,12 @@ describe('UserActivateController', function () {
 
   describe('activateAccountPage', function () {
     beforeEach(function () {
-      this.UserGetter.getUser = sinon.stub().callsArgWith(2, null, this.user)
+      this.UserGetter.promises.getUser = sinon.stub().resolves(this.user)
       this.req.query.user_id = this.user_id
       this.req.query.token = this.token = 'mock-token-123'
     })
 
-    it('should 404 without a user_id', function (done) {
+    it('should 404 without a user_id', async function (done) {
       delete this.req.query.user_id
       this.ErrorController.notFound = () => done()
       this.UserActivateController.activateAccountPage(this.req, this.res)
@@ -59,7 +70,7 @@ describe('UserActivateController', function () {
     })
 
     it('should 404 without a valid user_id', function (done) {
-      this.UserGetter.getUser = sinon.stub().callsArgWith(2, null, null)
+      this.UserGetter.promises.getUser = sinon.stub().resolves(null)
       this.ErrorController.notFound = () => done()
       this.UserActivateController.activateAccountPage(this.req, this.res)
     })
@@ -73,7 +84,7 @@ describe('UserActivateController', function () {
     it('should redirect activated users to login', function (done) {
       this.user.loginCount = 1
       this.res.redirect = url => {
-        this.UserGetter.getUser.calledWith(this.user_id).should.equal(true)
+        sinon.assert.calledWith(this.UserGetter.promises.getUser, this.user_id)
         url.should.equal('/login')
         return done()
       }
@@ -93,17 +104,20 @@ describe('UserActivateController', function () {
   })
 
   describe('register', function () {
-    beforeEach(function () {
-      this.UserRegistrationHandler.registerNewUserAndSendActivationEmail = sinon
-        .stub()
-        .callsArgWith(1, null, this.user, (this.url = 'mock/url'))
+    beforeEach(async function () {
+      this.UserRegistrationHandler.promises.registerNewUserAndSendActivationEmail =
+        sinon.stub().resolves({
+          user: this.user,
+          setNewPasswordUrl: (this.url = 'mock/url'),
+        })
       this.req.body.email = this.user.email = this.email = 'email@example.com'
-      this.UserActivateController.register(this.req, this.res)
+      await this.UserActivateController.register(this.req, this.res)
     })
 
     it('should register the user and send them an email', function () {
       sinon.assert.calledWith(
-        this.UserRegistrationHandler.registerNewUserAndSendActivationEmail,
+        this.UserRegistrationHandler.promises
+          .registerNewUserAndSendActivationEmail,
         this.email
       )
     })
