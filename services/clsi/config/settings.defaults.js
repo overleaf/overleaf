13b4e6333cdc64bc67a1237313e38ef79d0609f2
Andrew Rumble <andrew.rumble@overleaf.com>
2025-03-06 09:17:56 +0000

    Merge pull request #23874 from overleaf/ar-use-gcp-pre-emptible-signal
    [clsi] Use GCP pre-emptible metadata instead of hostname
    
    GitOrigin-RevId: 2df305e68f2999c9d3bde051dbb533025478800f

--- services/clsi/config/settings.defaults.js
+++ services/clsi/config/settings.defaults.js
@@ -1,11 +1,10 @@
 const Path = require('node:path')
-const os = require('node:os')
 const http = require('node:http')
 const https = require('node:https')
 
 http.globalAgent.keepAlive = false
 https.globalAgent.keepAlive = false
-const isPreEmptible = os.hostname().includes('pre-emp')
+const isPreEmptible = process.env.PREEMPTIBLE === 'TRUE'
 
 module.exports = {
   compileSizeLimit: process.env.COMPILE_SIZE_LIMIT || '7mb',
