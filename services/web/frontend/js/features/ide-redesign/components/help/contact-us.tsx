f11ad912493b8ea5f10de1a12a13fe1d166e8270
Mathias Jakobsen <mathias.jakobsen@overleaf.com>
2025-03-10 10:39:47 +0000

    Merge pull request #24201 from overleaf/mj-fix-ce-contact
    [web] Import contact us modal via macro
    
    GitOrigin-RevId: 6d8a14c33fb158a38413d32036b17dd4b3debdd9

--- services/web/frontend/js/features/ide-redesign/components/help/contact-us.tsx
+++ services/web/frontend/js/features/ide-redesign/components/help/contact-us.tsx
@@ -1,11 +1,21 @@
-import { FC, useCallback } from 'react'
-import ContactUsModal from '../../../../../../modules/support/frontend/js/components/contact-us-modal'
+import { FC, JSXElementConstructor, useCallback } from 'react'
+import importOverleafModules from '../../../../../macros/import-overleaf-module.macro'
 import { useRailContext } from '../../contexts/rail-context'
 import getMeta from '@/utils/meta'
+
+const [contactUsModalModules] = importOverleafModules('contactUsModal')
+const ContactUsModal: JSXElementConstructor<{
+  show: boolean
+  handleHide: () => void
+  autofillProjectUrl: boolean
+}> = contactUsModalModules?.import.default
 
 export const RailHelpContactUsModal: FC<{ show: boolean }> = ({ show }) => {
   const { setActiveModal } = useRailContext()
   const handleHide = useCallback(() => setActiveModal(null), [setActiveModal])
+  if (!ContactUsModal) {
+    return null
+  }
   const showSupport = getMeta('ol-showSupport')
   if (!showSupport) {
     return null
