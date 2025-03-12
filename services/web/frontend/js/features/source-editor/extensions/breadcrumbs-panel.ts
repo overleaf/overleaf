ea33c7d896cd3a8ff4f1c13a435f407d2314301b
David <33458145+davidmcpowell@users.noreply.github.com>
2025-03-12 09:32:21 +0000

    Merge pull request #24233 from overleaf/dp-dark-breadcrumbs
    Add dark mode for breadcrumbs
    
    GitOrigin-RevId: f1425a1da514f74854fd7afe176e27e2b62dae9c

--- services/web/frontend/js/features/source-editor/extensions/breadcrumbs-panel.ts
+++ services/web/frontend/js/features/source-editor/extensions/breadcrumbs-panel.ts
@@ -1,11 +1,41 @@
 import { isSplitTestEnabled } from '@/utils/splitTestUtils'
-import { showPanel } from '@codemirror/view'
+import { EditorView, showPanel } from '@codemirror/view'
 
 export function createBreadcrumbsPanel() {
   const dom = document.createElement('div')
   dom.classList.add('ol-cm-breadcrumbs-portal')
   return { dom, top: true }
 }
+
+const breadcrumbsTheme = EditorView.baseTheme({
+  '.ol-cm-breadcrumbs': {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 'var(--spacing-01)',
+    fontSize: 'var(--font-size-01)',
+    padding: 'var(--spacing-02)',
+    overflow: 'auto',
+    scrollbarWidth: 'thin',
+    '& > *': {
+      flexShrink: '0',
+    },
+  },
+  '&light .ol-cm-breadcrumbs': {
+    color: 'var(--content-secondary)',
+    backgroundColor: 'var(--bg-light-primary)',
+    borderBottom: '1px solid #ddd',
+  },
+  '&light .ol-cm-breadcrumb-chevron': {
+    color: 'var(--neutral-30)',
+  },
+  '&dark .ol-cm-breadcrumbs': {
+    color: 'var(--content-secondary-dark)',
+    backgroundColor: 'var(--bg-dark-primary)',
+  },
+  '&dark .ol-cm-breadcrumb-chevron': {
+    color: 'var(--neutral-50)',
+  },
+})
 
 /**
  * A panel which contains the editor breadcrumbs
@@ -15,5 +45,5 @@ export const breadcrumbPanel = () => {
     return []
   }
 
-  return [showPanel.of(createBreadcrumbsPanel)]
+  return [showPanel.of(createBreadcrumbsPanel), breadcrumbsTheme]
 }
