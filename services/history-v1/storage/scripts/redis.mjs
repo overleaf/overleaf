4ba0e97b95713a2c34fb77f8cb3a32e46d762a80
Brian Gough <briangough@users.noreply.github.com>
2025-04-09 13:51:55 +0100

    Merge pull request #24749 from overleaf/bg-history-redis-buffer
    add history redis to history-v1
    
    GitOrigin-RevId: 70dc1aee809ad17902c93c020f3804c0f1429238

--- services/history-v1/storage/scripts/redis.mjs
+++ services/history-v1/storage/scripts/redis.mjs
@@ -1,11 +1,28 @@
 import redis from '@overleaf/redis-wrapper'
 import config from 'config'
 
-const redisOptions = config.get('redis.queue')
+// Get allowed Redis dbs from config
+const redisConfig = config.get('redis')
+const allowedDbs = Object.keys(redisConfig)
 
+// Get the Redis db from command line argument or use the first available db as default
+const db = process.argv[2]
+
+// Validate redis db
+if (!allowedDbs.includes(db)) {
+  if (db) {
+    console.error('Invalid redis db:', db)
+  }
+  console.error(`Usage: node redis.mjs [${allowedDbs.join('|')}]`)
+  process.exit(1)
+}
+
+// Get redis options based on command line argument
+const redisOptions = config.get(`redis.${db}`)
+console.log('Using redis db:', db)
 console.log('REDIS CONFIG', {
   ...redisOptions,
-  password: '*'.repeat(redisOptions.password.length),
+  password: '*'.repeat(redisOptions.password?.length),
 })
 const rclient = redis.createClient(redisOptions)
 
