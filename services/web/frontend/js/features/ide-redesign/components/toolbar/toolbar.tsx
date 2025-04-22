9d858dcf0f9f1ffebde5fca8cf4ff153996179ab
David <33458145+davidmcpowell@users.noreply.github.com>
2025-04-22 09:58:16 +0100

    Merge pull request #24961 from overleaf/dp-back-to-editor
    Update toolbar in history view to add BackToEditorButton
    
    GitOrigin-RevId: 4c260126a373b1b3a7c31f9f4b44f6ae7fba6f36

--- services/web/frontend/js/features/ide-redesign/components/toolbar/toolbar.tsx
+++ services/web/frontend/js/features/ide-redesign/components/toolbar/toolbar.tsx
@@ -7,8 +7,31 @@ import ShareProjectButton from './share-project-button'
 import ChangeLayoutButton from './change-layout-button'
 import ShowHistoryButton from './show-history-button'
 import { LabsActions } from './labs-actions'
+import { useLayoutContext } from '@/shared/context/layout-context'
+import BackToEditorButton from '@/features/editor-navigation-toolbar/components/back-to-editor-button'
+import { useCallback } from 'react'
+import * as eventTracking from '../../../../infrastructure/event-tracking'
 
 export const Toolbar = () => {
+  const { view, setView } = useLayoutContext()
+
+  const handleBackToEditorClick = useCallback(() => {
+    eventTracking.sendMB('navigation-clicked-history', { action: 'close' })
+    setView('editor')
+  }, [setView])
+
+  if (view === 'history') {
+    return (
+      <div className="ide-redesign-toolbar">
+        <div className="d-flex align-items-center">
+          <BackToEditorButton onClick={handleBackToEditorClick} />
+        </div>
+        <ToolbarProjectTitle />
+        <div /> {/* Empty div used for spacing */}
+      </div>
+    )
+  }
+
   return (
     <div className="ide-redesign-toolbar">
       <ToolbarMenus />
