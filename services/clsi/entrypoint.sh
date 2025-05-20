efc42481da79ba5a8a7985e5552c3d199b839d5a
Jakob Ackermann <jakob.ackermann@overleaf.com>
2025-05-20 08:20:00 +0100

    [clsi] avoid creating legacy db folder (#25760)
    GitOrigin-RevId: eaa97813f0e20d14d7d6dafefe7b8b4d002fa41c

--- services/clsi/entrypoint.sh
+++ services/clsi/entrypoint.sh
@@ -8,7 +8,6 @@ usermod -aG dockeronhost node
 # compatibility: initial volume setup
 mkdir -p /overleaf/services/clsi/cache && chown node:node /overleaf/services/clsi/cache
 mkdir -p /overleaf/services/clsi/compiles && chown node:node /overleaf/services/clsi/compiles
-mkdir -p /overleaf/services/clsi/db && chown node:node /overleaf/services/clsi/db
 mkdir -p /overleaf/services/clsi/output && chown node:node /overleaf/services/clsi/output
 
 exec runuser -u node -- "$@"
