290bf71659b6ee9a39e3ee666e346037ebfb31fb
Tim Down <158919+timdown@users.noreply.github.com>
2025-05-22 11:12:34 +0100

    Merge pull request #25788 from overleaf/td-layout-react
    Use correct layout for React pages
    
    GitOrigin-RevId: 0dbf3146273c0ac2f1549f67be374595e1b8403e

--- services/web/frontend/js/pages/sharing-updates.tsx
+++ services/web/frontend/js/pages/sharing-updates.tsx
@@ -1,6 +1,7 @@
 import './../utils/meta'
 import '../utils/webpack-public-path'
 import './../infrastructure/error-reporter'
+import './../features/header-footer-react'
 import '@/i18n'
 import { createRoot } from 'react-dom/client'
 import SharingUpdatesRoot from '../features/token-access/components/sharing-updates-root'
