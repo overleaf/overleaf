50b5aa33b12b7d79acc4d687bf688dd87079df8b
roo hutton <roo.hutton@overleaf.com>
2025-06-24 10:30:17 +0100

    Merge pull request #26495 from overleaf/rh-prevent-pause-cancel-last-term
    Redirect to support if trying to cancel in last month of pause
    
    GitOrigin-RevId: d92f84c79482bb7c409f4e3070337b0ac958756a

--- services/web/frontend/js/features/subscription/components/dashboard/states/active/cancel-subscription-button.tsx
+++ services/web/frontend/js/features/subscription/components/dashboard/states/active/cancel-subscription-button.tsx
@@ -4,6 +4,7 @@ import { useSubscriptionDashboardContext } from '../../../../context/subscriptio
 import OLButton from '@/features/ui/components/ol/ol-button'
 import { PaidSubscription } from '../../../../../../../../types/subscription/dashboard/subscription'
 import { useFeatureFlag } from '@/shared/context/split-test-context'
+import { useLocation } from '@/shared/hooks/use-location'
 
 export function CancelSubscriptionButton() {
   const { t } = useTranslation()
@@ -13,6 +14,7 @@ export function CancelSubscriptionButton() {
     setModalIdShown,
     setShowCancellation,
   } = useSubscriptionDashboardContext()
+  const location = useLocation()
 
   const subscription = personalSubscription as PaidSubscription
   const isInTrial =
@@ -29,14 +31,22 @@ export function CancelSubscriptionButton() {
     useFeatureFlag('pause-subscription') &&
     !hasPendingOrActivePause &&
     planIsEligibleForPause
+  const shouldContactSupport =
+    subscription.payment.state === 'paused' &&
+    subscription.payment.remainingPauseCycles === 0
 
   function handleCancelSubscriptionClick() {
     eventTracking.sendMB('subscription-page-cancel-button-click', {
       plan_code: subscription?.planCode,
       is_trial: isInTrial,
     })
-    if (enablePause) setModalIdShown('pause-subscription')
-    else setShowCancellation(true)
+    if (shouldContactSupport) {
+      location.assign('/contact')
+    } else if (enablePause) {
+      setModalIdShown('pause-subscription')
+    } else {
+      setShowCancellation(true)
+    }
   }
 
   if (recurlyLoadError) return null
