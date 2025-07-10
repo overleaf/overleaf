14afc82acf71d721b84d95be0a9175e35b6804b5
David <33458145+davidmcpowell@users.noreply.github.com>
2025-07-10 10:24:06 +0100

    Merge pull request #27003 from overleaf/dp-modal-opacity
    Change settings modal appearance tab to have transparent backdrop
    
    GitOrigin-RevId: 9cbf36164a8d82ec5945d06c7107a3b73b334cfe

--- services/web/frontend/js/features/ide-redesign/components/settings/settings-modal.tsx
+++ services/web/frontend/js/features/ide-redesign/components/settings/settings-modal.tsx
@@ -5,24 +5,70 @@ import OLModal, {
 } from '@/features/ui/components/ol/ol-modal'
 import { useLayoutContext } from '@/shared/context/layout-context'
 import { useTranslation } from 'react-i18next'
-import { SettingsModalBody } from './settings-modal-body'
+import { SettingsEntry, SettingsModalBody } from './settings-modal-body'
+
+import AppearanceSettings from './appearance-settings/appearance-settings'
+import CompilerSettings from './compiler-settings/compiler-settings'
+import EditorSettings from './editor-settings/editor-settings'
+import { useMemo, useState } from 'react'
 
 const SettingsModal = () => {
   // TODO ide-redesign-cleanup: Either rename the field, or introduce a separate
   // one
   const { leftMenuShown, setLeftMenuShown } = useLayoutContext()
   const { t } = useTranslation()
+  const settingsTabs: SettingsEntry[] = useMemo(
+    () => [
+      {
+        key: 'editor',
+        title: t('editor'),
+        icon: 'code',
+        component: <EditorSettings />,
+      },
+      {
+        key: 'compiler',
+        title: t('compiler'),
+        icon: 'picture_as_pdf',
+        component: <CompilerSettings />,
+      },
+      {
+        key: 'appearance',
+        title: t('appearance'),
+        icon: 'brush',
+        component: <AppearanceSettings />,
+      },
+      {
+        key: 'account_settings',
+        title: t('account_settings'),
+        icon: 'settings',
+        href: '/user/settings',
+      },
+    ],
+    [t]
+  )
+  const [activeTab, setActiveTab] = useState<string | null | undefined>(
+    settingsTabs[0]?.key
+  )
   return (
     <OLModal
       show={leftMenuShown}
       onHide={() => setLeftMenuShown(false)}
       size="lg"
+      backdropClassName={
+        activeTab === 'appearance'
+          ? 'ide-settings-modal-transparent-backdrop'
+          : undefined
+      }
     >
       <OLModalHeader closeButton>
         <OLModalTitle>{t('settings')}</OLModalTitle>
       </OLModalHeader>
       <OLModalBody className="ide-settings-modal-body">
-        <SettingsModalBody />
+        <SettingsModalBody
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          settingsTabs={settingsTabs}
+        />
       </OLModalBody>
     </OLModal>
   )
