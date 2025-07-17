5d79cf18c0b880e39a2679b58c66721c93bf25c5
Andrew Rumble <andrew.rumble@overleaf.com>
2025-07-17 14:25:48 +0100

    Define all initial roles
    GitOrigin-RevId: ad613bad4d8a47e327281e90b5475e989a3ccec4

--- services/web/types/admin-capabilities.ts
+++ services/web/types/admin-capabilities.ts
@@ -1,3 +1,10 @@
 export type AdminCapability = 'modify-user-email' | 'view-project'
 
-export type AdminRole = 'engineering'
+export type AdminRole =
+  | 'engagement'
+  | 'engineering'
+  | 'finance'
+  | 'product'
+  | 'sales'
+  | 'support'
+  | 'support_tier_1'
