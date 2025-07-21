6daf877c909b72a982f96f84d3b5ddb09645ff59
Andrew Rumble <andrew.rumble@overleaf.com>
2025-07-21 14:06:55 +0100

    Add view-session, clear-session capabilities
    GitOrigin-RevId: 10134ab400939c388c5e563ac216af122bffaeb6

--- services/web/types/admin-capabilities.ts
+++ services/web/types/admin-capabilities.ts
@@ -1,8 +1,10 @@
 export type AdminCapability =
   | 'clear-saml-data'
+  | 'clear-session'
   | 'modify-user-email'
   | 'modify-user-name'
   | 'view-project'
+  | 'view-session'
 
 export type AdminRole =
   | 'engagement'
