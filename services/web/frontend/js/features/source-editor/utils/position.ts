d3668bddec2a2bbdc2b60ba191cce5dd999073ae
Alf Eaton <alf.eaton@overleaf.com>
2025-07-29 11:31:32 +0100

    Clamp minimum valid doc position to 0 (#27461)
    GitOrigin-RevId: d531e2b5351b41217ccc8385f8f317cce91a840b

--- services/web/frontend/js/features/source-editor/utils/position.ts
+++ services/web/frontend/js/features/source-editor/utils/position.ts
@@ -5,6 +5,10 @@ export const findValidPosition = (
   lineNumber: number, // 1-indexed
   columnNumber = 0 // 0-indexed
 ): number => {
+  if (lineNumber < 1) {
+    return 0
+  }
+
   const lines = doc.lines
 
   if (lineNumber > lines) {
