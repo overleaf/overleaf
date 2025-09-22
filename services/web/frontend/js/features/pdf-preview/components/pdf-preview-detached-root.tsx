f58c213edae4e98b4d8b025ce8cc2c68f31ce3d4
David <33458145+davidmcpowell@users.noreply.github.com>
2025-09-22 10:05:56 +0100

    Merge pull request #28608 from overleaf/mj-fix-redesign-css-in-detached-pdf
    [web] Use wrapping div for detached PDF when using redesigned editor
    
    GitOrigin-RevId: f8f7b395dbe5a627804da10218f4de9d5ac76348

--- services/web/frontend/js/features/pdf-preview/components/pdf-preview-detached-root.tsx
+++ services/web/frontend/js/features/pdf-preview/components/pdf-preview-detached-root.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import PdfPreview from './pdf-preview'
 import useWaitForI18n from '../../../shared/hooks/use-wait-for-i18n'
 import { ReactContextRoot } from '@/features/ide-react/context/react-context-root'
+import { useIsNewEditorEnabled } from '@/features/ide-redesign/utils/new-editor-utils'
 
 function PdfPreviewDetachedRoot() {
   const { isReady } = useWaitForI18n()
@@ -12,9 +13,21 @@ function PdfPreviewDetachedRoot() {
 
   return (
     <ReactContextRoot>
-      <PdfPreview />
+      <EditorRedesignWrapper>
+        <PdfPreview />
+      </EditorRedesignWrapper>
     </ReactContextRoot>
   )
+}
+
+function EditorRedesignWrapper({ children }: { children: React.ReactNode }) {
+  const newEditorEnabled = useIsNewEditorEnabled()
+  if (!newEditorEnabled) {
+    return <>{children}</>
+  }
+
+  // TODO ide-redesign-cleanup: this wrapper should not be required
+  return <div className="ide-redesign-main">{children}</div>
 }
 
 export default PdfPreviewDetachedRoot // for testing
