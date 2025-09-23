19f1f6f702d2c735d299fbe5b310f84c168e767d
Andrew Rumble <andrew.rumble@overleaf.com>
2025-09-23 14:11:04 +0100

    Remove Joi and celebrate
    GitOrigin-RevId: 12cf2e0266a2e57d674d13a2e6fe8368c980d2a1

--- services/web/app/src/infrastructure/Validation.js
+++ services/web/app/src/infrastructure/Validation.js
@@ -1,7 +1,5 @@
 // @ts-check
 
-const { Joi: CelebrateJoi, celebrate, errors } = require('celebrate')
-const { ObjectId } = require('mongodb-legacy')
 const { NotFoundError } = require('../Features/Errors/Errors')
 const {
   validateReq,
@@ -15,29 +13,7 @@ const { isZodErrorLike, fromError } = require('zod-validation-error')
  * @typedef {import('express').ErrorRequestHandler} ErrorRequestHandler
  */
 
-const objectIdValidator = {
-  type: 'objectId',
-  base: CelebrateJoi.any(),
-  messages: {
-    'objectId.invalid': 'needs to be a valid ObjectId',
-  },
-  coerce(value) {
-    return {
-      value: typeof value === typeof ObjectId ? value : new ObjectId(value),
-    }
-  },
-  prepare(value, helpers) {
-    if (!ObjectId.isValid(value)) {
-      return {
-        errors: helpers.error('objectId.invalid'),
-      }
-    }
-  },
-}
-
-const Joi = CelebrateJoi.extend(objectIdValidator)
 const errorMiddleware = [
-  errors(),
   /** @type {ErrorRequestHandler} */
   (err, req, res, next) => {
     if (!isZodErrorLike(err)) {
@@ -47,14 +23,6 @@ const errorMiddleware = [
     res.status(400).json({ ...fromError(err), statusCode: 400 })
   },
 ]
-
-/**
- * Validation middleware
- * @deprecated Please use Zod schemas and `validateReq` instead
- */
-function validate(schema) {
-  return celebrate(schema, { allowUnknown: true })
-}
 
 const validateReqWeb = (req, schema) => {
   try {
@@ -69,8 +37,6 @@ const validateReqWeb = (req, schema) => {
 }
 
 module.exports = {
-  Joi,
-  validate,
   errorMiddleware,
   validateReq: validateReqWeb,
   z,
