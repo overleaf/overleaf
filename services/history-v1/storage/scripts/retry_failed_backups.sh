2cae9c46356ab2a4413fe38f0a74bee9c299eedc
Brian Gough <briangough@users.noreply.github.com>
2025-10-15 09:54:44 +0100

    Merge pull request #29097 from overleaf/bg-reduce-retry-backup-script-concurrency
    reduce concurrency for retrying failed backups
    
    GitOrigin-RevId: aeb1f05b7ed26b80c00f12fe07a5f30a14786c30

--- services/history-v1/storage/scripts/retry_failed_backups.sh
+++ services/history-v1/storage/scripts/retry_failed_backups.sh
@@ -4,5 +4,5 @@ RETRY_TIMEOUT="${RETRY_TIMEOUT:-10000}"
 ids=$(node storage/scripts/backup_scheduler.mjs --show-pending="$RETRY_TIMEOUT" | grep '^{' | jq -r '.projectId' | sort -u)
 # Retry backups for each project ID with a concurrency of 5 blob uploads at a time
 for project_id in $ids ; do
-  LOG_LEVEL=debug node storage/scripts/backup.mjs --projectId="$project_id" -c 5 ;
+  LOG_LEVEL=debug node storage/scripts/backup.mjs --projectId="$project_id" -c 2 ;
 done
