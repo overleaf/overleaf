0fdd87f345f7d6a219af53599b1032b0c32b712c
Andrew Rumble <andrew.rumble@overleaf.com>
2025-10-20 10:55:35 +0100

    Allow sandboxed-module for model mockery
    GitOrigin-RevId: 79dfe75088b760968fdc1b6e5dce4278f44a0dae

--- services/web/test/unit/vitest_bootstrap.mjs
+++ services/web/test/unit/vitest_bootstrap.mjs
@@ -4,6 +4,35 @@ import sinon from 'sinon'
 import logger from '@overleaf/logger'
 import sinonChai from 'sinon-chai'
 import chaiAsPromised from 'chai-as-promised'
+import SandboxedModule from 'sandboxed-module'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
+SandboxedModule.configure({
+  ignoreMissing: true,
+  requires: {
+    // This is already imported the same way in the mocha bootstrap
+    // eslint-disable-next-line import/no-extraneous-dependencies
+    sshpk: require('sshpk'),
+  },
+  globals: {
+    AbortController,
+    AbortSignal,
+    Buffer,
+    Promise,
+    console,
+    process,
+    URL,
+    TextEncoder,
+    TextDecoder,
+  },
+  sourceTransformers: {
+    removeNodePrefix: function (source) {
+      return source.replace(/require\(['"]node:/g, "require('")
+    },
+  },
+})
 
 /*
  * Chai configuration
