c2e0c40808b7f1e78eb3c35f8c401daede2b28d5
roo hutton <roo.hutton@overleaf.com>
2025-10-29 11:15:46 +0000

    Merge pull request #29193 from overleaf/rh-stripe-addon-error
    Redirect to plans page if trying to add add-on to non-existing subscription
    
    GitOrigin-RevId: 65e0a88c32beca00d700292b14b2e7aa6e4dad20

--- services/web/app/src/Features/Subscription/SubscriptionController.mjs
+++ services/web/app/src/Features/Subscription/SubscriptionController.mjs
@@ -523,6 +523,12 @@ async function previewAddonPurchase(req, res) {
         '/user/subscription?redirect-reason=ai-assist-unavailable'
       )
     }
+    if (
+      err instanceof Error &&
+      err.constructor.name === 'PaymentServiceResourceNotFoundError'
+    ) {
+      return res.redirect('/user/subscription/plans#ai-assist')
+    }
     throw err
   }
 
@@ -542,6 +548,12 @@ async function previewAddonPurchase(req, res) {
   } catch (err) {
     if (err instanceof DuplicateAddOnError) {
       return res.redirect('/user/subscription?redirect-reason=double-buy')
+    }
+    if (
+      err instanceof Error &&
+      err.constructor.name === 'PaymentServiceResourceNotFoundError'
+    ) {
+      return res.redirect('/user/subscription/plans#ai-assist')
     }
     throw err
   }
