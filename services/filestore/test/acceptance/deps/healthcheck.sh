9408d253da46c5aeb9a694bcfe93ace20d3d9d67
John Lees-Miller <jdleesmiller@gmail.com>
2025-11-13 16:02:50 +0000

    Remove apparently unused files
    GitOrigin-RevId: 60a4427c9c8a58ae700244f06d80c574002e923c

--- services/filestore/test/acceptance/deps/healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# health check to allow 404 status code as valid
-STATUSCODE=$(curl --silent --output /dev/null --write-out "%{http_code}" "$1")
-# will be 000 on non-http error (e.g. connection failure)
-if test "$STATUSCODE" -ge 500 || test "$STATUSCODE" -lt 200; then
-  exit 1
-fi
-exit 0
