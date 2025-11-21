00f6a1e0f97a913c4bba00e5428f572e4de07b23
Tim Down <158919+timdown@users.noreply.github.com>
2025-11-21 12:55:04 +0000

    CIAM registration form buttons, inputs and fixes (#29740)
    * Many fixes to CIAM registration form, including Phosphor icons
    
    * Unify layout between Pug and React, fixes for spacing and mobile screen sizes
    
    * Pug lint fix
    
    * Make CIAM footer links underlined
    
    * Add CIAM error notification styling
    
    * Merge duplicate style rules
    
    * Remove outdated comment
    
    * Fix ordering of en.json
    
    * Move aria-label to buttons
    
    * Move full stop into translation string
    
    * Remove dummy password strength indicator
    
    * CIAM spacing and label fixes
    
    * Header logo fixes from review
    
    * Add aria-hidden to error icon
    
    GitOrigin-RevId: 87c8181566f0878256b8010f95f115ec25c7ceb9

---
+++ services/web/frontend/js/features/form-helpers/form-phosphor-icons.ts
@@ -0,0 +1,4 @@
+// These are used in the CIAM registration form
+import '@phosphor-icons/webcomponents/PhBank'
+import '@phosphor-icons/webcomponents/PhEye'
+import '@phosphor-icons/webcomponents/PhEyeSlash'
