e590543bc5d3954c9866535373d690604b2c2162
Alf Eaton <alf.eaton@overleaf.com>
2025-11-25 10:10:11 +0000

    Store PDF scroll position when the viewer unmounts (#29872)
    GitOrigin-RevId: fc82c8db3d8cdf6a4857a4d2f3f62b181e40e067

--- services/web/frontend/js/features/pdf-preview/components/pdf-js-viewer.tsx
+++ services/web/frontend/js/features/pdf-preview/components/pdf-js-viewer.tsx
@@ -238,6 +238,7 @@ function PdfJsViewer({ url, pdfFile }: PdfJsViewerProps) {
           window.clearTimeout(storePositionTimer)
         }
         storePosition.cancel()
+        setPosition(pdfJsWrapper.currentPosition)
       }
     }
   }, [setPosition, pdfJsWrapper, initialised])
