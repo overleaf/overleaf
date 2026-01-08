5e49f421c2ef2352740bbc84d3f1182c48343d5c
Mathias Jakobsen <mathias.jakobsen@overleaf.com>
2026-01-08 09:28:08 +0000

    Merge pull request #30405 from overleaf/dp-cleanup-editor-redesign-opt-out-2
    (Re)Cleanup editor-redesign-opt-out feature flag
    
    GitOrigin-RevId: afefced1f77de2b8b27e1333ffa647ac41561e31

--- services/web/app/src/Features/Project/UserSettingsHelper.mjs
+++ services/web/app/src/Features/Project/UserSettingsHelper.mjs
@@ -41,13 +41,6 @@ async function buildUserSettings(req, res, user) {
   const enableNewEditorLegacy =
     user.ace.enableNewEditor ?? defaultLegacyEnableNewEditor
 
-  const assignment = await SplitTestHandler.promises.getAssignment(
-    req,
-    res,
-    'editor-redesign-opt-out'
-  )
-  const isOptOutEnabled = assignment.variant === 'enabled'
-
   return {
     mode: user.ace.mode,
     editorTheme: user.ace.theme,
@@ -64,9 +57,7 @@ async function buildUserSettings(req, res, user) {
     mathPreview: user.ace.mathPreview,
     breadcrumbs: user.ace.breadcrumbs,
     referencesSearchMode: user.ace.referencesSearchMode,
-    enableNewEditor: isOptOutEnabled
-      ? enableNewEditorStageFour
-      : enableNewEditorLegacy,
+    enableNewEditor: enableNewEditorStageFour,
     enableNewEditorLegacy,
     darkModePdf: user.ace.darkModePdf ?? false,
   }
--- services/web/frontend/js/features/ide-redesign/utils/new-editor-utils.ts
+++ services/web/frontend/js/features/ide-redesign/utils/new-editor-utils.ts
@@ -1,67 +1,16 @@
 import { useUserSettingsContext } from '@/shared/context/user-settings-context'
 import getMeta from '@/utils/meta'
-import { isSplitTestEnabled, getSplitTestVariant } from '@/utils/splitTestUtils'
 
-const ignoringUserCutoffDate =
-  new URLSearchParams(window.location.search).get('skip-new-user-check') ===
-  'true'
-
-// For E2E tests, allow forcing a user to be treated as an existing user
-const existingUserOverride =
-  new URLSearchParams(window.location.search).get('existing-user-override') ===
+// For e2e tests purposes, allow overriding to old editor
+export const oldEditorOverride =
+  new URLSearchParams(window.location.search).get('old-editor-override') ===
   'true'
 
 // We don't want to enable the new editor on server-pro/CE until we have fully rolled it out on SaaS
 const { isOverleaf } = getMeta('ol-ExposedSettings')
 
-const SPLIT_TEST_USER_CUTOFF_DATE = new Date(Date.UTC(2025, 8, 23, 13, 0, 0)) // 2pm British Summer Time on September 23, 2025
-const NEW_USER_CUTOFF_DATE = new Date(Date.UTC(2025, 10, 12, 12, 0, 0)) // 12pm GMT on November 12, 2025
-
-export const isNewUser = () => {
-  if (existingUserOverride) return false
-
-  if (ignoringUserCutoffDate) return true
-  const user = getMeta('ol-user')
-
-  if (!user.signUpDate) return false
-
-  const createdAt = new Date(user.signUpDate)
-  return createdAt > NEW_USER_CUTOFF_DATE
-}
-
-export const isSplitTestUser = () => {
-  if (existingUserOverride) return false
-
-  const user = getMeta('ol-user')
-  if (!user.signUpDate) return false
-
-  const createdAt = new Date(user.signUpDate)
-  return (
-    createdAt > SPLIT_TEST_USER_CUTOFF_DATE && createdAt <= NEW_USER_CUTOFF_DATE
-  )
-}
-
-export const canUseNewEditorAsExistingUser = () => {
-  return !canUseNewEditorAsNewUser() && isSplitTestEnabled('editor-redesign')
-}
-
-export const canUseNewEditorAsNewUser = () => {
-  const newUserTestVariant = getSplitTestVariant('editor-redesign-new-users')
-  return (
-    isOverleaf &&
-    (isNewUser() || (isSplitTestUser() && newUserTestVariant !== 'default'))
-  )
-}
-
 export const canUseNewEditor = () => {
-  return canUseNewEditorAsExistingUser() || canUseNewEditorAsNewUser()
-}
-
-export const useIsNewEditorEnabledAsExistingUser = () => {
-  const { userSettings } = useUserSettingsContext()
-  const hasAccess = canUseNewEditorAsExistingUser()
-  const enabled = userSettings.enableNewEditor
-  return hasAccess && enabled
+  return isOverleaf && !oldEditorOverride
 }
 
 export const useIsNewEditorEnabled = () => {
