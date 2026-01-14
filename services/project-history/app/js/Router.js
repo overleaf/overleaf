da9da896c246dfdab65733a2c9ba2b762b37276f
Domagoj Kriskovic <dom.kriskovic@overleaf.com>
2026-01-14 11:26:29 +0100

    Revert "Revert "[project-history] migrate from joi to zod (#30187)" (#30681)"
    This reverts commit 8d66aa15a29216736755c3f86954a4d40aa20a6c.
    
    GitOrigin-RevId: 9d56dafff3da5202204ead896bed30182cc2aceb

--- services/project-history/app/js/Router.js
+++ services/project-history/app/js/Router.js
@@ -1,19 +1,7 @@
 import OError from '@overleaf/o-error'
 import * as HttpController from './HttpController.js'
-import { Joi, validate } from './Validation.js'
 
 export function initialize(app) {
-  app.use(
-    validate({
-      params: Joi.object({
-        project_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-        user_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-        label_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-        version: Joi.number().integer(),
-      }),
-    })
-  )
-
   // use an extended timeout on all endpoints, to allow for long requests to history-v1
   app.use(longerTimeout)
 
@@ -23,129 +11,36 @@ export function initialize(app) {
 
   app.get('/project/:project_id/snapshot', HttpController.getLatestSnapshot)
 
-  app.get(
-    '/project/:project_id/diff',
-    validate({
-      query: {
-        pathname: Joi.string().required(),
-        from: Joi.number().integer().required(),
-        to: Joi.number().integer().required(),
-      },
-    }),
-    HttpController.getDiff
-  )
+  app.get('/project/:project_id/diff', HttpController.getDiff)
 
-  app.get(
-    '/project/:project_id/filetree/diff',
-    validate({
-      query: {
-        from: Joi.number().integer().required(),
-        to: Joi.number().integer().required(),
-      },
-    }),
-    HttpController.getFileTreeDiff
-  )
+  app.get('/project/:project_id/filetree/diff', HttpController.getFileTreeDiff)
 
-  app.get(
-    '/project/:project_id/updates',
-    validate({
-      query: {
-        before: Joi.number().integer(),
-        min_count: Joi.number().integer(),
-      },
-    }),
-    HttpController.getUpdates
-  )
+  app.get('/project/:project_id/updates', HttpController.getUpdates)
 
   app.get(
     '/project/:project_id/changes-in-chunk',
-    validate({
-      query: {
-        since: Joi.number().integer().min(0),
-      },
-    }),
     HttpController.getChangesInChunkSince
   )
 
   app.get('/project/:project_id/version', HttpController.latestVersion)
 
-  app.post(
-    '/project/:project_id/flush',
-    validate({
-      query: {
-        debug: Joi.boolean().default(false),
-        bisect: Joi.boolean().default(false),
-      },
-    }),
-    HttpController.flushProject
-  )
+  app.post('/project/:project_id/flush', HttpController.flushProject)
 
-  app.post(
-    '/project/:project_id/resync',
-    validate({
-      query: {
-        force: Joi.boolean().default(false),
-      },
-      body: {
-        force: Joi.boolean().default(false),
-        origin: Joi.object({
-          kind: Joi.string().required(),
-        }),
-        historyRangesMigration: Joi.string()
-          .optional()
-          .valid('forwards', 'backwards'),
-      },
-    }),
-    HttpController.resyncProject
-  )
+  app.post('/project/:project_id/resync', HttpController.resyncProject)
 
-  app.get(
-    '/project/:project_id/dump',
-    validate({
-      query: {
-        count: Joi.number().integer(),
-      },
-    }),
-    HttpController.dumpProject
-  )
+  app.get('/project/:project_id/dump', HttpController.dumpProject)
 
   app.get('/project/:project_id/labels', HttpController.getLabels)
 
-  app.post(
-    '/project/:project_id/labels',
-    validate({
-      body: {
-        version: Joi.number().integer().required(),
-        comment: Joi.string().required(),
-        created_at: Joi.string(),
-        validate_exists: Joi.boolean().default(true),
-        user_id: Joi.string().allow(null),
-      },
-    }),
-
-    HttpController.createLabel
-  )
+  app.post('/project/:project_id/labels', HttpController.createLabel)
 
   app.delete(
     '/project/:project_id/user/:user_id/labels/:label_id',
-    validate({
-      params: Joi.object({
-        project_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-        user_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-        label_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-      }),
-    }),
     HttpController.deleteLabelForUser
   )
 
   app.delete(
     '/project/:project_id/labels/:label_id',
-    validate({
-      params: Joi.object({
-        project_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-        label_id: Joi.string().regex(/^[0-9a-f]{24}$/),
-      }),
-    }),
     HttpController.deleteLabel
   )
 
@@ -179,15 +74,7 @@ export function initialize(app) {
     HttpController.getPathsAtVersion
   )
 
-  app.post(
-    '/project/:project_id/force',
-    validate({
-      query: {
-        clear: Joi.boolean().default(false),
-      },
-    }),
-    HttpController.forceDebugProject
-  )
+  app.post('/project/:project_id/force', HttpController.forceDebugProject)
 
   app.get('/project/:history_id/blob/:hash', HttpController.getProjectBlob)
 
@@ -195,43 +82,9 @@ export function initialize(app) {
 
   app.get('/status/queue', HttpController.getQueueCounts)
 
-  app.post(
-    '/retry/failures',
-    validate({
-      query: {
-        failureType: Joi.string().valid('soft', 'hard'),
-        // bail out after this time limit
-        timeout: Joi.number().integer().default(300),
-        // maximum number of projects to check
-        limit: Joi.number().integer().default(100),
-        callbackUrl: Joi.string(),
-      },
-    }),
-    HttpController.retryFailures
-  )
+  app.post('/retry/failures', HttpController.retryFailures)
 
-  app.post(
-    '/flush/old',
-    validate({
-      query: {
-        // flush projects with queued ops older than this
-        maxAge: Joi.number()
-          .integer()
-          .default(6 * 3600),
-        // pause this amount of time between checking queues
-        queueDelay: Joi.number().integer().default(100),
-        // maximum number of queues to check
-        limit: Joi.number().integer().default(1000),
-        //  maximum amount of time allowed
-        timeout: Joi.number()
-          .integer()
-          .default(60 * 1000),
-        // whether to run in the background
-        background: Joi.boolean().falsy('0').truthy('1').default(false),
-      },
-    }),
-    HttpController.flushOld
-  )
+  app.post('/flush/old', HttpController.flushOld)
 
   app.get('/status', (req, res, next) => res.send('project-history is up'))
 
