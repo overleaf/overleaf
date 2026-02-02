9028ec7b295e0f97a6c3410c690f183cb419f9ad
Domagoj Kriskovic <dom.kriskovic@overleaf.com>
2026-02-02 11:07:08 +0100

    Use labs experiments for visual bibtex editor
    GitOrigin-RevId: e616d42af0d54f9acdf40b24fbfcbed06f349266

--- services/web/frontend/js/utils/labs-utils.ts
+++ services/web/frontend/js/utils/labs-utils.ts
@@ -2,7 +2,10 @@ import getMeta from './meta'
 
 // Should be `never` when no experiments are active. Otherwise it should be a
 // union of active experiment names e.g. `'experiment1' | 'experiment2'`
-export type ActiveExperiment = 'monthly-texlive' | 'ai-workbench'
+export type ActiveExperiment =
+  | 'monthly-texlive'
+  | 'ai-workbench'
+  | 'bibtex-visual-editor'
 
 export const isInExperiment = (experiment: ActiveExperiment): boolean => {
   const experiments = getMeta('ol-labsExperiments')
