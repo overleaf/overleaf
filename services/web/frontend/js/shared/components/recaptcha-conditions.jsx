b345bf94850cfda5672701f05ff3a1c7a24765e8
Olzhas Askar <olzhas.askar@overleaf.com>
2026-04-13 07:41:39 -0700

    [web] Update captcha notice (#32286)
    * feat: remove translations
    
    * feat: remove scss
    
    * fix: remove the last translation
    
    * feat: remove pug
    
    * fix: forgotten conditions
    
    * feat: also remove from react
    
    * Revert changes
    
    * Remove links from `recaptcha_conditions`
    
    * Center short reCAPTCHA notice on CIAM screens
    
    * Re-add the Chinese translation
    
    ---------
    
    Co-authored-by: Antoine Clausse <antoine.clausse@overleaf.com>
    GitOrigin-RevId: 64e7b8a38c2a4b4f7e60d5c9df6740c35b8763d8

--- services/web/frontend/js/shared/components/recaptcha-conditions.jsx
+++ services/web/frontend/js/shared/components/recaptcha-conditions.jsx
@@ -1,32 +1,6 @@
-import { Trans } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 export default function RecaptchaConditions() {
-  // the component link children below will be overwritten by the translation string
-  return (
-    <div className="recaptcha-branding">
-      <Trans
-        i18nKey="recaptcha_conditions"
-        components={{
-          1: (
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://policies.google.com/privacy"
-            >
-              Privacy Policy
-            </a>
-          ),
-          2: (
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://policies.google.com/terms"
-            >
-              Terms of Service
-            </a>
-          ),
-        }}
-      />
-    </div>
-  )
+  const { t } = useTranslation()
+  return <div className="recaptcha-branding">{t('recaptcha_conditions')}</div>
 }
