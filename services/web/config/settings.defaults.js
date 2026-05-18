99148d5956b86b13ea8c53a4ef7703a69e86c939
Jakob Ackermann <jakob.ackermann@overleaf.com>
2026-05-18 10:30:32 +0200

    [web] silence customer.io integration when not configured in dev/CI (#33731)
    GitOrigin-RevId: c9498f57f0dacb3d18cd7617388df11d5cf029de

--- services/web/config/settings.defaults.js
+++ services/web/config/settings.defaults.js
@@ -330,6 +330,7 @@ module.exports = {
 
   isCodeSpace: process.env.IS_CODE_SPACE === 'true',
   isDevEnv: process.env.NODE_ENV === 'development',
+  isCI: process.env.NODE_ENV === 'test',
 
   lockManager: {
     lockTestInterval: intFromEnv('LOCK_MANAGER_LOCK_TEST_INTERVAL', 50),
