f4565a677bb158f5ffa73e3b77488dd380eb6004
Daniel Kontšek <daniel@kontsek.sk>
2026-05-22 16:05:53 +0200

    Merge pull request #33922 from overleaf/dn0-gke02-rt-23-staging
    [gke-02] Migrate staging real-time pool to n4d-highcpu-2
    
    GitOrigin-RevId: 5dcdc8d5074cc49e2027ba06e86a0751eab71bf4

--- services/contacts/app/js/Errors.js
+++
@@ -1,6 +0,0 @@
-export class NotFoundError extends Error {
-  constructor(message) {
-    super(message)
-    this.name = 'NotFoundError'
-  }
-}
--- services/contacts/test/mocha-multi-reporters.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reporterEnabled: 'spec, mocha-junit-reporter',
-  mochaJunitReporterReporterOptions: {
-    mochaFile: `reports/junit-mocha-${process.env.MOCHA_GREP}.xml`,
-    includePending: true,
-    jenkinsMode: true,
-    output: true,
-  },
-}
