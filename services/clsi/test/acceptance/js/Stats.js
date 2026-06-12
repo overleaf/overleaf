9743c7006ece2c9ebd2efd823921ea7e361fb7ea
Jakob Ackermann <jakob.ackermann@overleaf.com>
2026-06-12 12:10:28 +0200

    [clsi] tests: remove verbose after hook for printing metrics (#34532)
    This was useful when iterating on the pdf-caching feature.
    PDF caching has been stable for years. We can remove the logging now.
    
    GitOrigin-RevId: 826c91c1e45d3dae1bb3ce5aeb7b6a3f9283392c

--- services/clsi/test/acceptance/js/Stats.js
+++
@@ -1,8 +0,0 @@
-import { fetchString } from '@overleaf/fetch-utils'
-import Settings from '@overleaf/settings'
-after(async function () {
-  const metrics = await fetchString(`${Settings.apis.clsi.url}/metrics`)
-  console.error('-- metrics --')
-  console.error(metrics)
-  console.error('-- metrics --')
-})
