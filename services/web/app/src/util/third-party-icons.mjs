3f26afcb8becabb5a5e26072a160230f4747b0c7
Tim Down <158919+timdown@users.noreply.github.com>
2026-07-02 09:21:47 +0100

    Merge pull request #35124 from overleaf/td-dropbox-logo-rename
    Rename Dropbox icon file to refresh cache in staging and production
    
    GitOrigin-RevId: 4538892d8db227af3861e88da8e021936db0447d

--- services/web/app/src/util/third-party-icons.mjs
+++ services/web/app/src/util/third-party-icons.mjs
@@ -6,7 +6,7 @@
 const thirdPartyIcons = [
   { file: 'git.svg', label: 'Git' },
   { file: 'github.svg', label: 'GitHub' },
-  { file: 'dropbox.svg', label: 'Dropbox' },
+  { file: 'dropbox-transparent.svg', label: 'Dropbox' },
   { file: 'zotero_64x64x32.png', label: 'Zotero' },
   { file: 'mendeley.svg', label: 'Mendeley' },
   { file: 'papers.svg', label: 'Papers' },
