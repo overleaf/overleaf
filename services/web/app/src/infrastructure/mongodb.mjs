c4104f3bd729d59bb72b79fab418695ee5d4d0d2
Eric Mc Sween <5454374+emcsween@users.noreply.github.com>
2026-07-09 15:01:06 -0400

    Merge pull request #35235 from overleaf/em-library-size-limits
    Add size and count limits to the reference library
    
    GitOrigin-RevId: ec4fb333bbd02043a929f91a702bdfd3fd285785

--- services/web/app/src/infrastructure/mongodb.mjs
+++ services/web/app/src/infrastructure/mongodb.mjs
@@ -54,6 +54,7 @@ export const db = {
   groupAuditLogEntries: internalDb.collection('groupAuditLogEntries'),
   institutions: internalDb.collection('institutions'),
   libraryReferences: internalDb.collection('libraryReferences'),
+  librarySizes: internalDb.collection('librarySizes'),
   librarySyncStates: internalDb.collection('librarySyncStates'),
   messages: internalDb.collection('messages'),
   migrations: internalDb.collection('migrations'),
