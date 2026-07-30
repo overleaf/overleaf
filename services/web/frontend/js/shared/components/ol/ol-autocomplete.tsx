0fe77a8068a56d4d16b62dea52e799c0fd1eb394
David <33458145+davidmcpowell@users.noreply.github.com>
2026-07-30 10:12:00 +0100

    Merge pull request #35964 from overleaf/dp-dark-library-autocomplete
    Add dark theme support to the library auto-complete dropdown
    
    GitOrigin-RevId: be975d7c379e689ed61706bf027e488b7177eb73

--- services/web/frontend/js/shared/components/ol/ol-autocomplete.tsx
+++ services/web/frontend/js/shared/components/ol/ol-autocomplete.tsx
@@ -216,7 +216,9 @@ function OLAutocompleteInternal({
             'dropdown-item-highlighted': highlightedIndex === index,
           })}
         >
-          <span className="text-muted">{createOptionPrefix} </span>
+          <span className="ol-autocomplete-create-prefix">
+            {createOptionPrefix}{' '}
+          </span>
           <strong>'{internalInputValue}'</strong>
         </OLButton>
       </li>
@@ -245,6 +247,7 @@ function OLAutocompleteInternal({
           {...getInputProps({
             ref: inputRef,
           })}
+          type="text"
           placeholder={placeholder}
           disabled={disabled}
         />
