c8616286848e33f45b408320ecac3501f24c6e1d
Alexandre Bourdin <abourdin@users.noreply.github.com>
2026-08-14 15:23:27 +0200

    Merge pull request #36208 from overleaf/ab-labs-description-headings-code
    [web] Support headings and code in labs experiment descriptions
    
    GitOrigin-RevId: 2a85f92fb5f0ce0696c1056087108ef2599547de

--- services/web/frontend/js/shared/components/labs/labs-description.tsx
+++ services/web/frontend/js/shared/components/labs/labs-description.tsx
@@ -3,7 +3,21 @@ import { micromark } from 'micromark'
 import DOMPurify from 'dompurify'
 
 const PURIFY_CONFIG = {
-  ALLOWED_TAGS: ['#text', 'p', 'em', 'strong', 'a'],
+  ALLOWED_TAGS: [
+    '#text',
+    'p',
+    'em',
+    'strong',
+    'a',
+    'code',
+    'pre',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+  ],
   ALLOWED_ATTR: ['href'],
 }
 
@@ -27,12 +41,19 @@ function sanitizeDescription(description: string) {
 
 /**
  * Renders a labs experiment description from markdown to sanitized HTML.
- * Only bold, italic, and links are supported.
+ * Only bold, italic, links, headings, inline code and code blocks are
+ * supported. Note that the language of a fenced code block is discarded, as
+ * the `class` attribute micromark puts it in is not allowed through.
  */
 export const LabsDescription: FC<{ description: string }> = ({
   description,
 }) => {
   const html = useMemo(() => sanitizeDescription(description), [description])
 
-  return <div dangerouslySetInnerHTML={{ __html: html }} />
+  return (
+    <div
+      className="labs-description"
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  )
 }
